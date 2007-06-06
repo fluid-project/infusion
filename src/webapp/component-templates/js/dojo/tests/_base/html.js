f294c152491eec1d405b928a9e266b2033599562
Antranig Basman <antranig.basman@colorado.edu>
2007-06-06 18:38:14 +0000

    Reverting to Dojo 0.9M2
    svn path=/utoronto/fluid/components/; revision=4175

--- src/webapp/component-templates/js/dojo/tests/_base/html.js
+++
@@ -1,5 +0,0 @@
-dojo.provide("tests._base.html");
-if(dojo.isBrowser){
-	doh.registerUrl("tests._base.html", dojo.moduleUrl("tests", "_base/html.html"));
-	doh.registerUrl("tests._base.html_quirks", dojo.moduleUrl("tests", "_base/html_quirks.html"));
-}
