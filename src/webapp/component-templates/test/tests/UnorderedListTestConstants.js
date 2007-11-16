24237963461cf23e222f5b93a4c5cd00473b9725
Joseph Scheuhammer <clown@alum.mit.edu>
2007-11-16 21:43:40 +0000

    FLUID-102:  Added embedded licence to JavaScript files. [JS]
    svn path=/fluid/components/trunk/; revision=4464

--- src/webapp/component-templates/test/tests/UnorderedListTestConstants.js
+++ src/webapp/component-templates/test/tests/UnorderedListTestConstants.js
@@ -1,3 +1,14 @@
+/*
+Copyright 2007 University of Toronto
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://source.fluidproject.org/svn/LICENSE.txt
+*/
+
 /**
  * This file contains test constants and setup and teardown functions that are used when testing with the data in the unordered-list.html file.
  */
