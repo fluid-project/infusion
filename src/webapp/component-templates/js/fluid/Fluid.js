56de3535f7d75a735817fb218fc42fda9dfef452
Michelle D'Souza <michelled33@gmail.com>
2007-11-22 21:21:45 +0000

    FLUID-27: jsLintified and removed fluid.declare
    svn path=/fluid/components/trunk/; revision=4488

--- src/webapp/component-templates/js/fluid/Fluid.js
+++ src/webapp/component-templates/js/fluid/Fluid.js
@@ -11,106 +11,99 @@ https://source.fluidproject.org/svn/LICENSE.txt
 */
 
 if (typeof(fluid) == "undefined") {
-	fluid = {};
+    fluid = {};
+}
+
+fluid.deriveLightboxCellBase = function (namebase, index) {
+    return namebase + "lightbox-cell:" + index + ":";
 };
+    
+// Client-level initialisation for the lightbox, allowing parameterisation for
+// different templates.
+fluid.initLightbox = function (namebase, messageNamebase) {
+    var reorderform = fluid.Utilities.findForm (document.getElementById (namebase));
+        
+    // Remove the anchors from the taborder - camel case 'tabIndex' needed for IE7 support
+    jQuery ("a", reorderform).attr ("tabIndex", "-1");
+    
+    // An <input> tag nested within our root namebase tag, which has an id which 
+    // begins with the  namebase:lightbox-cell:: prefix, and ends with "reorder-index" trail.
+    // Very hard to imagine any perversity which may lead to this picking any stray stuff :P
+    
+    // An approach based on the "sourceIndex" DOM property would be much more efficient,
+    // but this is only supported in IE. 
+    var orderChangedCallback = function() {
+        var inputs = fluid.Utilities.seekNodesById(
+            reorderform, 
+            "input", 
+            "^" + fluid.deriveLightboxCellBase (namebase, "[^:]*") + "reorder-index$");
+        
+        for (var i = 0; i < inputs.length; ++ i) {
+            inputs[i].value = i;
+        }
 
-fluid.declare = function(target, args) {
-    for (arg in args) {
-      target[arg] = args[arg];
+        if (reorderform && reorderform.action) {
+            dojo.xhrPost({
+                url: reorderform.action,
+                form: reorderform,
+                load: function (type, data, evt) { /* No-op response */ }
+            });
+        }
+    };
+    
+    // This orderable finder knows that the lightbox thumbnails are 'div' elements
+    var lightboxCellNamePattern = "^" + fluid.deriveLightboxCellBase (namebase, "[0-9]+") +"$";
+    var lightboxOrderableFinder = function (containerEl) {
+        return fluid.Utilities.seekNodesById (containerEl, "div", lightboxCellNamePattern);
+    };
+    
+    var lightbox = new fluid.Reorderer (namebase, {
+            messageNamebase : messageNamebase,
+            orderChangedCallback: orderChangedCallback,
+            layoutHandler: new fluid.GridLayoutHandler (lightboxOrderableFinder),
+            orderableFinder: lightboxOrderableFinder
+        }
+    );
+};
+  
+
+
+/*
+ * Utilities object for providing various general convenience functions
+ */
+fluid.Utilities = {};
+
+// Custom query method seeks all tags descended from a given root with a 
+// particular tag name, whose id matches a regex. The Dojo query parser
+// is broken http://trac.dojotoolkit.org/ticket/3520#preview, this is all
+// it might do anyway, and this will be plenty fast.
+fluid.Utilities.seekNodesById = function (rootnode, tagname, idmatch) {
+    var inputs = rootnode.getElementsByTagName (tagname);
+    var togo = [];
+    for (var i = 0; i < inputs.length; ++ i) {
+        var input = inputs[i];
+        var id = input.id;
+        if (id && id.match (idmatch)) {
+            togo.push (input);
+        }
     }
-  };
+    return togo;
+};
+      
+fluid.Utilities.escapeSelector = function(id) {
+    return id.replace (/\:/g,"\\:");
+};
+  
+fluid.Utilities.findForm = function (element) {
+    while(element) {
+        if (element.nodeName.toLowerCase() == "form") {
+            return element;
+        }
+        element = element.parentNode;
+    }
+}
+    
+    
 
-fluid.declare(fluid, {
-	
-	deriveLightboxCellBase: function(namebase, index) {
-		return namebase + "lightbox-cell:"+index+":";
-	},
-	
-	// Client-level initialisation for the lightbox, allowing parameterisation for
-	// different templates.
-	initLightbox: function(namebase, messageNamebase) {
-		var reorderform = fluid.Utilities.findForm(document.getElementById(namebase));
-		
-		// Remove the anchors from the taborder - camel case 'tabIndex' needed for IE7 support
-		jQuery ("a", reorderform).attr ("tabIndex", "-1");
-		
-		// An <input> tag nested within our root namebase tag, which has an id which 
-		// begins with the  namebase:lightbox-cell:: prefix, and ends with "reorder-index" trail.
-		// Very hard to imagine any perversity which may lead to this picking any stray stuff :P
-		
-		// An approach based on the "sourceIndex" DOM property would be much more efficient,
-		// but this is only supported in IE. 
-		var orderChangedCallback = function() {
-			var inputs = fluid.Utilities.seekNodesById(
-				reorderform, 
-				"input", 
-				"^"+fluid.deriveLightboxCellBase(namebase, "[^:]*") + "reorder-index$"
-			);
-			
-			for (var i = 0; i < inputs.length; ++ i) {
-				inputs[i].value = i;
-			}
-
-			if (reorderform && reorderform.action) {
-				dojo.xhrPost({
-					url: reorderform.action,
-					form: reorderform,
-					load: function(type, data, evt){ /* No-op response */ }
-				});
-			}
-		};
-		
-		// This orderable finder assumes that the lightbox thumbnails are 'div' elements
-		var lightboxCellNamePattern = "^" + fluid.deriveLightboxCellBase (namebase, "[0-9]+") +"$";
-		var lightboxOrderableFinder = function (containerEl) {
-			return fluid.Utilities.seekNodesById (containerEl, "div", lightboxCellNamePattern);
-		};
-		
-		var lightbox = new fluid.Reorderer (namebase, {
-				messageNamebase : messageNamebase,
-				orderChangedCallback: orderChangedCallback,
-				layoutHandler: new fluid.GridLayoutHandler (lightboxOrderableFinder),
-                orderableFinder: lightboxOrderableFinder
-			}
-		);
-	}
-});  
-
-fluid.declare(fluid, {
-	/*
-	 * Utilities object for providing various general convenience functions
-	 */
-    Utilities: {
-
-	// Custom query method seeks all tags descended from a given root with a 
-	// particular tag name, whose id matches a regex. The Dojo query parser
-	// is broken http://trac.dojotoolkit.org/ticket/3520#preview, this is all
-	// it might do anyway, and this will be plenty fast.
-	seekNodesById: function (rootnode, tagname, idmatch) {
-		var inputs = rootnode.getElementsByTagName (tagname);
-		var togo = new Array();
-		for (var i = 0; i < inputs.length; ++ i) {
-			var input = inputs[i];
-			var id = input.id;
-			if (id && id.match (idmatch)) {
-				togo.push (input);
-			}
-		}
-		return togo;
-	},
-		  
-	escapeSelector: function(id) {
-		return id.replace(/\:/g,"\\:");
-	},
-	  
-	findForm: function (element) {
-		while(element) {
-			if (element.nodeName.toLowerCase() == "form") return element;
-				element = element.parentNode;
-			}
-		}
-	}
-	
-});
 
 
