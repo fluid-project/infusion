b7db29c5023d47790b08fba44501f826402c25d8
Colin Clark <colinbdclark@gmail.com>
2007-11-22 00:13:59 +0000

    FLUID-87: Wrote a sample orderable finder function and a partial order changed callback for the tutorial. Still need to finish the callback and write a sample layout handler.
    svn path=/fluid/components/trunk/; revision=4479

---
+++ src/webapp/component-templates/test/markupExamples/reorderer/concrete/scheduler/Scheduler.js
@@ -0,0 +1,41 @@
+if (typeof(fluid) == "undefined") {
+	fluid = {};
+}
+
+fluid.Scheduler = {};
+
+fluid.Scheduler.initScheduler = function (containerId) {
+	var orderableFinder = fluid.Scheduler.createCSSOrderableFinderForClass ("movableTopic");
+	
+	new fluid.Reorderer (containerId, {
+		orderChangedCallback: fluid.Scheduler.createJSONOrderChangedCallback (orderableFinder),
+		orderableFinder: orderableFinder,
+		layoutHandler: new fluid.GridLayoutHandler (orderableFinder)
+	});
+};
+
+fluid.Scheduler.fixedElementLayoutHandler = {
+	
+};
+
+fluid.Scheduler.createJSONOrderChangedCallback = function (orderableFinder) {
+	var orderables = orderableFinder();
+		
+	// Create a simple data structure keyed by element id and with the ordinal number as value.
+	var orderMap = {};
+	orderables.each(function (index) {
+		orderMap[this.id] = index;
+	});
+	
+	// Then serialize it to a JSON string.
+	var orderMapJSONString = JSON.stringify(orderMap);
+	
+	// Then POST it back to the server via XHR.
+};
+
+fluid.Scheduler.createCSSOrderableFinderForClass = function (className) {
+	return function (containerElement) {
+		var orderableSelector = "." + className;
+		return jQuery(orderableSelector, containerElement);
+	};
+};
