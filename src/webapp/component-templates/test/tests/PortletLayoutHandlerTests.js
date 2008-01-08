5e75c9deb134b938924c0e83156786b073395a10
Anastasia Cheetham <ac@anastasiacheetham.ca>
2008-01-08 19:56:20 +0000

    FLUID-49: Merged work from the branch into trunk: Added PortletLayoutHandler and associated tests; Added portlets.html test/demo file; Added necessary styles to markup-examples.css.  Some refactoring is still necessary. [AC, CC]
    svn path=/fluid/components/trunk/; revision=4580

---
+++ src/webapp/component-templates/test/tests/PortletLayoutHandlerTests.js
@@ -0,0 +1,150 @@
+
+function testIsFirstInColumn() {
+	assertFalse(col1portlet4id+" should not be first in column",
+		portletHandler._isFirstInColumn(jQuery ("#" + col1portlet4id)[0]));
+	assertFalse(col1portlet3id+" should not be first in column",
+		portletHandler._isFirstInColumn(jQuery ("#" + col1portlet3id)[0]));
+	assertTrue(col1portlet1id+" should be first in column",
+		portletHandler._isFirstInColumn(jQuery ("#" + col1portlet1id)[0]));
+	assertFalse(col2portlet2id+" should not be first in column",
+		portletHandler._isFirstInColumn(jQuery ("#" + col2portlet2id)[0]));
+	assertTrue(col2portlet1id+" should be first in column",
+		portletHandler._isFirstInColumn(jQuery ("#" + col2portlet1id)[0]));
+	assertFalse(col3portlet3id+" should not be first in column",
+		portletHandler._isFirstInColumn(jQuery ("#" + col3portlet3id)[0]));
+	assertTrue(col3portlet1id+" should be first in column",
+		portletHandler._isFirstInColumn(jQuery ("#" + col3portlet1id)[0]));
+ }
+
+function testIsLastInColumn() {
+	assertFalse(col1portlet1id+" should not be last in column",
+		portletHandler._isLastInColumn(jQuery ("#" + col1portlet1id)[0]));
+	assertFalse(col1portlet3id+" should not be last in column",
+		portletHandler._isLastInColumn(jQuery ("#" + col1portlet3id)[0]));
+	assertTrue(col1portlet4id+" should be last in column",
+		portletHandler._isLastInColumn(jQuery ("#" + col1portlet4id)[0]));
+	assertFalse(col2portlet1id+" should not be last in column",
+		portletHandler._isLastInColumn(jQuery ("#" + col2portlet1id)[0]));
+	assertTrue(col2portlet2id+" should be last in column",
+		portletHandler._isLastInColumn(jQuery ("#" + col2portlet2id)[0]));
+	assertFalse(col3portlet1id+" should not be last in column",
+		portletHandler._isLastInColumn(jQuery ("#" + col3portlet1id)[0]));
+	assertTrue(col3portlet3id+" should be last in column",
+		portletHandler._isLastInColumn(jQuery ("#" + col3portlet3id)[0]));
+ }
+
+function testGetItemAbove() {
+    var itemAbove = portletHandler.getItemAbove(jQuery ("#" + col3portlet3id)[0]);
+    assertEquals(col3portlet2id+" should be above "+col3portlet3id,
+        jQuery ("#" + col3portlet2id)[0], itemAbove);
+
+	itemAbove = portletHandler.getItemAbove(jQuery ("#" + col1portlet1id)[0]);
+	assertEquals(col1portlet1id.id+" is at the top of the column, so nothing is 'above' it",
+		jQuery ("#" + col1portlet1id)[0], itemAbove);
+
+	itemAbove = portletHandler.getItemAbove(jQuery ("#" + col3portlet1id)[0]);
+	assertEquals(col3portlet1id.id+" is at the top of the column, so nothing is 'above' it",
+		jQuery ("#" + col3portlet1id)[0], itemAbove);
+
+    itemAbove = portletHandler.getItemAbove(jQuery ("#" + col1portlet4id)[0]);
+    assertEquals(col1portlet3id+" should be above "+col1portlet4id,
+        jQuery ("#" + col1portlet3id)[0], itemAbove);
+
+    itemAbove = portletHandler.getItemAbove(jQuery ("#" + col3portlet2id)[0]);
+    assertEquals(col3portlet1id+" should be above "+col3portlet2id,
+        jQuery ("#" + col3portlet1id)[0], itemAbove);
+
+    itemAbove = portletHandler.getItemAbove(jQuery ("#" + col3portlet3id)[0]);
+    assertEquals(col3portlet2id+" should be above "+col3portlet3id,
+        jQuery ("#" + col3portlet2id)[0], itemAbove);
+}
+
+function testGetItemBelow() {
+	var itemBelow = portletHandler.getItemBelow(jQuery ("#" + col1portlet3id)[0]);
+	assertEquals(col1portlet4id+" should be below "+col1portlet3id,
+		jQuery ("#" + col1portlet4id)[0], itemBelow);
+
+	itemBelow = portletHandler.getItemBelow(jQuery ("#" + col3portlet1id)[0]);
+	assertEquals(col3portlet2id+" should be below "+col3portlet1id,
+		jQuery ("#" + col3portlet2id)[0], itemBelow);
+
+	itemBelow = portletHandler.getItemBelow(jQuery ("#" + col3portlet2id)[0]);
+	assertEquals(col3portlet3id+" should be below "+col3portlet2id,
+		jQuery ("#" + col3portlet3id)[0], itemBelow);
+
+	itemBelow = portletHandler.getItemBelow(jQuery ("#" + col1portlet4id)[0]);
+	assertEquals(col1portlet4id.id+" is at the bottom of the column, so nothing is 'below' it",
+		jQuery ("#" + col1portlet4id)[0], itemBelow);
+
+	itemBelow = portletHandler.getItemBelow(jQuery ("#" + col3portlet3id)[0]);
+	assertEquals(col3portlet3id.id+" is at the bottom of the column, so nothing is 'below' it",
+		jQuery ("#" + col3portlet3id)[0], itemBelow);
+
+}
+
+function testIsInLeftmostColumn() {
+	assertTrue(col1portlet1id+" should be in leftmost column",
+		portletHandler._isInLeftmostColumn(jQuery ("#" + col1portlet1id)[0]));
+	assertTrue(col1portlet4id+" should be in leftmost column",
+		portletHandler._isInLeftmostColumn(jQuery ("#" + col1portlet4id)[0]));
+	assertFalse(col2portlet1id+" should not be in leftmost column",
+		portletHandler._isInLeftmostColumn(jQuery ("#" + col2portlet1id)[0]));
+	assertFalse(col3portlet2id+" should not be in leftmost column",
+		portletHandler._isInLeftmostColumn(jQuery ("#" + col3portlet2id)[0]));
+}
+
+function testIsInRightmostColumn() {
+	assertTrue(col3portlet1id+" should be in rightmost column",
+		portletHandler._isInRightmostColumn(jQuery ("#" + col3portlet1id)[0]));
+	assertTrue(col3portlet3id+" should be in rightmost column",
+		portletHandler._isInRightmostColumn(jQuery ("#" + col3portlet3id)[0]));
+	assertFalse(col2portlet1id+" should not be in rightmost column",
+		portletHandler._isInRightmostColumn(jQuery ("#" + col2portlet1id)[0]));
+	assertFalse(col1portlet2id+" should not be in rightmost column",
+		portletHandler._isInRightmostColumn(jQuery ("#" + col1portlet2id)[0]));
+}
+
+function testGetLeftSibling() {
+	var leftSibling = portletHandler.getLeftSibling(jQuery ("#" + col2portlet1id)[0]);
+	assertEquals(col1portlet3id+" should to the left of "+col2portlet1id,
+		jQuery ("#" + col1portlet3id)[0], leftSibling);
+
+	leftSibling = portletHandler.getLeftSibling(jQuery ("#" + col2portlet2id)[0]);
+	assertEquals(col1portlet3id+" should to the left of "+col2portlet2id,
+		jQuery ("#" + col1portlet3id)[0], leftSibling);
+
+	leftSibling = portletHandler.getLeftSibling(jQuery ("#" + col3portlet3id)[0]);
+	assertEquals(col2portlet2id+" should to the left of "+col3portlet3id,
+		jQuery ("#" + col2portlet2id)[0], leftSibling);
+
+	leftSibling = portletHandler.getLeftSibling(jQuery ("#" + col1portlet1id)[0]);
+	assertEquals(col1portlet1id+" is at the far left, so nothing is to the left",
+		jQuery ("#" + col1portlet1id)[0], leftSibling);
+
+	leftSibling = portletHandler.getLeftSibling(jQuery ("#" + col1portlet3id)[0]);
+	assertEquals(col1portlet3id+" is at the far left, so nothing is to the left",
+		jQuery ("#" + col1portlet3id)[0], leftSibling);
+}
+
+function testGetRightSibling() {
+	var rightSibling = portletHandler.getRightSibling(jQuery ("#" + col1portlet1id)[0]);
+	assertEquals(col2portlet2id+" should to the right of "+col1portlet1id,
+		jQuery ("#" + col2portlet2id)[0], rightSibling);
+
+	rightSibling = portletHandler.getRightSibling(jQuery ("#" + col1portlet4id)[0]);
+	assertEquals(col2portlet2id+" should to the right of "+col1portlet4id,
+		jQuery ("#" + col2portlet2id)[0], rightSibling);
+
+	rightSibling = portletHandler.getRightSibling(jQuery ("#" + col2portlet2id)[0]);
+	assertEquals(col3portlet1id+" should to the right of "+col2portlet2id,
+		jQuery ("#" + col3portlet1id)[0], rightSibling);
+
+	rightSibling = portletHandler.getRightSibling(jQuery ("#" + col3portlet1id)[0]);
+	assertEquals(col3portlet1id+" is at the far right, so nothing is to the right",
+		jQuery ("#" + col3portlet1id)[0], rightSibling);
+
+	rightSibling = portletHandler.getRightSibling(jQuery ("#" + col3portlet3id)[0]);
+	assertEquals(col3portlet3id+" is at the far right, so nothing is to the right",
+		jQuery ("#" + col3portlet3id)[0], rightSibling);
+}
+
---
+++ src/webapp/component-templates/test/tests/PortletTestConstants.js
@@ -0,0 +1,85 @@
+/**
+ * This file contains test constants and setup and teardown functions that are used when
+ * testing with the data in the portlets.html file.
+ */
+
+var col1portlet1id = "portlet1";
+var col1portlet2id = "portlet2";
+var col1portlet3id = "portlet3";
+var col1portlet4id = "portlet4";
+var col2portlet1id = "portlet5";
+var col2portlet2id = "portlet6";
+var col3portlet1id = "portlet7";
+var col3portlet2id = "portlet8";
+var col3portlet3id = "portlet9";
+
+// All the test function names for the JsUnit tests that test against the unordered-list.html file.  
+// This is needed for running JsUnit in IE and Safari.
+// It's a very brittle and annoying way of specifying test names and should be fixed. [FLUID-35]
+
+function exposeTestFunctionNames() {
+    return [
+        // PortletLayoutHandlerTests.js
+		"testIsFirstInColumn",
+		"testIsLastInColumn",
+		"testGetItemAbove",
+		"testGetItemBelow",
+		"testIsInLeftmostColumn",
+		"testIsInRightmostColumn",
+		"testGetLeftSibling",
+		"testGetRightSibling"
+    ]
+}
+
+var portletHandler;
+
+	function portletOrderableFinder (containerEl) {
+        return jQuery ("#portlet3,#portlet4,#portlet6,#portlet7,#portlet8,#portlet9");
+	}
+
+var layout = { id:"t2",
+			columns:[{ id:"c1", children:["portlet1","portlet2","portlet3","portlet4"]},
+					 { id:"c2", children:["portlet5","portlet6"]   },
+					 { id:"c3", children:["portlet7","portlet8","portlet9"]}
+					]
+				};
+	
+var dropTargets = {
+    portlet1:{portlet1:[1,1],portlet2:[1,1],portlet3:[1,1],portlet4:[1,1],
+    	   portlet5:[1,1],portlet6:[1,1],
+		   portlet7:[1,1],portlet8:[1,1],portlet9:[1,1]},
+	portlet2:{portlet1:[0,0],portlet2:[0,0],portlet3:[0,0],portlet4:[0,0],
+    	   portlet5:[0,0],portlet6:[0,0],
+		   portlet7:[0,0],portlet8:[0,0],portlet9:[0,0]},
+	portlet3:{portlet1:[0,0],portlet2:[0,0],portlet3:[0,0],portlet4:[0,0],
+    	   portlet5:[0,0],portlet6:[0,0],
+		   portlet7:[0,0],portlet8:[0,0],portlet9:[0,0]},
+	portlet4:{portlet1:[0,0],portlet2:[0,0],portlet3:[0,1],portlet4:[1,1],
+    	   portlet5:[0,1],portlet6:[1,1],
+		   portlet7:[0,1],portlet8:[1,1],portlet9:[1,1]},
+	portlet5:{portlet1:[0,0],portlet2:[0,0],portlet3:[0,0],portlet4:[0,0],
+    	   portlet5:[0,0],portlet6:[0,0],
+		   portlet7:[0,0],portlet8:[0,0],portlet9:[0,0]},
+	portlet6:{portlet1:[0,0],portlet2:[0,0],portlet3:[0,1],portlet4:[1,1],
+    	   portlet5:[0,1],portlet6:[1,1],
+		   portlet7:[0,1],portlet8:[1,1],portlet9:[1,1]},
+	portlet7:{portlet1:[0,0],portlet2:[0,1],portlet3:[1,1],portlet4:[1,1],
+    	   portlet5:[0,1],portlet6:[1,1],
+		   portlet7:[1,1],portlet8:[1,1],portlet9:[1,1]},
+	portlet8:{portlet1:[0,0],portlet2:[0,0],portlet3:[0,1],portlet4:[1,1],
+    	   portlet5:[0,1],portlet6:[1,1],
+		   portlet7:[0,1],portlet8:[1,1],portlet9:[1,1]},
+	portlet9:{portlet1:[0,0],portlet2:[0,0],portlet3:[0,1],portlet4:[1,1],
+    	   portlet5:[0,1],portlet6:[1,1],
+		   portlet7:[0,1],portlet8:[1,1],portlet9:[1,1]}
+};
+
+// This setUp will be called before each of the tests that are included in unordered-list.html 
+function setUp() {
+	portletHandler = new fluid.PortletLayoutHandler (portletOrderableFinder,
+          		layout, dropTargets);
+    var table = jQuery("#reorderer-root");
+    portletHandler.setReorderableContainer(table);
+}
+
+
