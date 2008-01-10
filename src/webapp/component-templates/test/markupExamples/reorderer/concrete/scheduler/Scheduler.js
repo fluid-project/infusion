42469f9a7f20f78e7ce871b0dbb19fd6d0cd5f34
Colin Clark <colinbdclark@gmail.com>
2008-01-10 22:44:30 +0000

    NOJIRA: Fixed buggy jQuery selector.
    svn path=/fluid/components/trunk/; revision=4610

--- src/webapp/component-templates/test/markupExamples/reorderer/concrete/scheduler/Scheduler.js
+++ src/webapp/component-templates/test/markupExamples/reorderer/concrete/scheduler/Scheduler.js
@@ -13,94 +13,94 @@ fluid = fluid || {};
 
 fluid.Scheduler = function () {
 	var fetchReordererContainer = function (id) {
-		return jQuery("#" + id);
+		return jQuery("[id=" + id + "]");
 	};
-	
+
 	return {
 		initScheduler: function (containerId) {
 		    var orderableFinder = fluid.Scheduler.createCSSOrderableFinderForClass ("movableTopic");
 		    var jsonCallback = fluid.Scheduler.createJSONOrderChangedCallback (orderableFinder);
-		    
+
 		    return new fluid.Reorderer (fetchReordererContainer (containerId), {
 		                                orderChangedCallback: jsonCallback,
 		                                orderableFinder: orderableFinder,
 		                                layoutHandler: new fluid.ListLayoutHandler (orderableFinder)
 		                                });
 		},
-		
+
 		initRSFScheduler: function (namebase, orderableTagName, orderableIdName, numOrderables) {
-		    var orderableFinder = fluid.Scheduler.createRSFOrderableFinder (namebase, 
-		                                                                    orderableTagName, 
-		                                                                    orderableIdName, 
+		    var orderableFinder = fluid.Scheduler.createRSFOrderableFinder (namebase,
+		                                                                    orderableTagName,
+		                                                                    orderableIdName,
 		                                                                    numOrderables);
 		    return new fluid.Reorderer (fetchReordererContainer (namebase), {
 		                                orderableFinder: orderableFinder,
 		                                layoutHandler: new fluid.ListLayoutHandler (orderableFinder)
 		                                });
 		},
-		
+
 		createJSONOrderChangedCallback: function (orderableFinder, urlToPostJSON) {
 		    return function () {
 		        var orderMapJSONString = fluid.Scheduler.generateJSONStringForOrderables(orderableFinder());
-		            
+
 		        // Then POST it back to the server via XHR.
 		        fluid.Scheduler.postOrder(orderMapJSONString, urlToPostJSON);
 		    };
 		},
-		
+
 		generateJSONStringForOrderables: function (orderables) {
 		    // Create a simple data structure keyed by element id and with the ordinal number as value.
 		    var orderMap = {};
 		    jQuery.each (orderables, function (index, element) {
 		        orderMap[jQuery(element).attr("id")] = index;
 		    });
-		    
+
 		    // Then serialize it to a JSON string.
 		    return JSON.stringify(orderMap);
 		},
-		
+
 		createCSSOrderableFinderForClass: function (className) {
 		    return function (containerElement) {
 		        var orderableSelector = "." + className;
 		        return jQuery (orderableSelector, containerElement);
 		    };
 		},
-		
+
 		createPortalSafeFinder: function (containerId, orderableName, numOrderables, delimiter) {
 		    return function (containerElement) {
 		        if (!delimiter) {
 		            delimiter = ":";
 		        }
-		        
+
 		        // Escape the selector, since delimiters tend to be punctuation that will confuse jQuery.
 		        delimiter = "\\" + delimiter;
 		        var orderablePrefix = containerId + delimiter + orderableName;
-		        
+
 		        var orderables = [];
-		        for (var idx = 0; idx < numOrderables; idx++) {         
+		        for (var idx = 0; idx < numOrderables; idx++) {
 		            var idSelector = "#" + orderablePrefix + idx;
-		            
-		            var foundElement = jQuery (idSelector, containerElement).get (0);   
+
+		            var foundElement = jQuery (idSelector, containerElement).get (0);
 		            if (foundElement) {
 		                orderables.push (foundElement);
 		            }
 		        }
-		    
+
 		        return orderables;
 		    };
 		},
-		
+
 		createRSFOrderableFinder: function (namebase, orderableTagName, orderableIdName, numOrderables) {
 		    return function (containerElement) {
 		        var orderablesRange = "[0-" + (numOrderables - 1) + "]";
 		        var idRegExp = "^" + namebase + orderableIdName + ":" + orderablesRange + ":" + "$";
-		        
-		        return fluid.Utilities.seekNodesById (containerElement, 
-		                                              orderableTagName, 
+
+		        return fluid.Utilities.seekNodesById (containerElement,
+		                                              orderableTagName,
 		                                              fluid.Utilities.escapeSelector(idRegExp));
 		    };
 		},
-		
+
 		postOrder: function (jsonString, urlToPostJSON) {
 		    /*
 		     * By default this does nothing since we don't have a server to respond to this POST request.
