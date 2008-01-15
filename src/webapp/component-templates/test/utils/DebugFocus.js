b15038aac68600464efdb849d39b1b2c96c37e4e
Colin Clark <colinbdclark@gmail.com>
2008-01-15 02:45:14 +0000

    NOJIRA: Added focus debugging test utility. This is helpful for tracking down problems with focus handlers.
    svn path=/fluid/components/trunk/; revision=4623

---
+++ src/webapp/component-templates/test/utils/DebugFocus.js
@@ -0,0 +1,42 @@
+fluid = fluid || {};
+
+fluid.debug = function () {
+    var printId = function (element) {
+        return (element) ? element.id : element;
+    };
+
+    var outputEventDetails = function (eventType, event, caughtBy) {
+        console.debug (eventType + " was called on target " + printId (event.target) + ", caught by " + printId (caughtBy));
+    };
+
+    var focusOutputter = function (evt) {
+        outputEventDetails ("Focus", evt, this);
+    };
+
+    var blurOutputter = function (evt) {
+        outputEventDetails ("Blur", evt, this);
+    };
+
+    var addFocusChangeListeners = function (jQueryElements) {
+        jQueryElements.focus (focusOutputter);
+        jQueryElements.blur (blurOutputter);
+    };
+
+    return {
+        listenForFocusEvents: function () {
+            var focussableElements  = [];
+
+            var everything = jQuery ("*");
+            everything.each (function () {
+               if (jQuery (this).hasTabindex ()) {
+                   focussableElements.push (this);
+               }
+            });
+
+            addFocusChangeListeners (jQuery (focussableElements));
+        }
+    }; // End of public return.
+} (); // End of fluid.debug namespace.
+
+// Call listenForFocusEvents when the document is ready.
+jQuery (document).ready (fluid.debug.listenForFocusEvents);
