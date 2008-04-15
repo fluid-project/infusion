8bf032b349dd94b39f3257d649c54eb4d9768b66
Michelle D'Souza <michelled33@gmail.com>
2008-04-15 19:41:54 +0000

    FLUID-474: First add of the cleaned up portlet reordering example. There is still some styling clean up outstanding. Once this example is cleaned up and contains all the functionality of the original (uportal3-newskin) example the original example will be removed.
    svn path=/fluid/components/trunk/; revision=4804

---
+++ src/webapp/sample-code/reorderer/concrete/portal/portal.js
@@ -0,0 +1,38 @@
+/*
+Copyright 2008 University of Toronto
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://source.fluidproject.org/svn/LICENSE.txt
+*/   
+
+var demo = demo || {};
+    
+demo.initPortletReorderer = function() {
+    var layout = { 
+        id:"portalPageBody",
+        columns:[
+            { id:"column_u15l1s9", children:["portlet_u15l1n10", "portlet_u15l1n11"]},
+            { id:"column_u15l1s12", children:["portlet_u15l1n13","portlet_u15l1n14","portlet_u15l1n15"]}
+        ]
+    };
+
+    var dropTargetPerms = [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1],
+        [0, 1, 1, 1, 1, 1, 1],
+        [0, 1, 1, 1, 1, 1, 1],
+        [0, 1, 1, 1, 1, 1, 1]
+    ];
+
+    var grabHandle = function (item) {        
+        // the handle is the toolbar. The toolbar id is the same as the portlet id, with the
+        // "portlet_" prefix replaced by "toolbar_".
+        return jQuery ("[id=toolbar_" + item.id.split ("_")[1] + "]");
+    };
+
+    return fluid.initPortalReorderer (layout, dropTargetPerms, grabHandle);
+};  
