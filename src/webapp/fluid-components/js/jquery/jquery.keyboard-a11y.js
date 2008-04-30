8c248fdc05ec7ca6e158fc2fb81c95114fa813ad
Anastasia Cheetham <ac@anastasiacheetham.ca>
2008-04-30 15:23:00 +0000

    FLUID-513: Merged the jQuery tabindex plugin code into the keyboard-a11y plugin code, to create a single keyboard-a11y plugin that includes tabindex functionality. This code could probably still use some refactoring, as there are a large number of unorganized functions in the private space, untested through unit tests.
    svn path=/fluid/components/trunk/; revision=4912

--- src/webapp/fluid-components/js/jquery/jquery.keyboard-a11y.js
+++ src/webapp/fluid-components/js/jquery/jquery.keyboard-a11y.js
@@ -49,6 +49,38 @@ https://source.fluidproject.org/svn/sandbox/tabindex/trunk/LICENSE.txt
     };
 
     // Private functions.
+	var normalizeTabindexName = function () {
+	    return $.browser.msie ? "tabIndex" : "tabindex";
+	};
+
+	var getValue = function (elements) {
+        if (elements.length <= 0) {
+            return undefined;
+        }
+
+		if (!elements.hasTabindexAttr ()) {
+		    return canHaveDefaultTabindex (elements) ? Number (0) : undefined;
+		}
+
+        // Get the attribute (.attr () doesn't work for tabIndex in IE) and return it as a number value.
+		var value = elements[0].getAttribute (normalizeTabindexName ());
+		return Number (value);
+	};
+
+	var setValue = function (elements, toIndex) {
+		return elements.each (function (i, item) {
+			$ (item).attr (normalizeTabindexName (), toIndex);
+		});
+	};
+
+	var canHaveDefaultTabindex = function (elements) {
+       if (elements.length <= 0) {
+           return false;
+       }
+
+	   return jQuery (elements[0]).is ("a, input, button, select, area, textarea, object");
+	};
+
     var unwrap = function (element) {
         return (element.jquery) ? element[0] : element; // Unwrap the element if it's a jQuery.
     };
@@ -334,6 +366,33 @@ https://source.fluidproject.org/svn/sandbox/tabindex/trunk/LICENSE.txt
     };
 
     // Public API.
+	$.fn.tabindex = function (toIndex) {
+		if (toIndex !== null && toIndex !== undefined) {
+			return setValue (this, toIndex);
+		} else {
+			return getValue (this);
+		}
+	};
+
+	$.fn.removeTabindex = function () {
+		return this.each(function (i, item) {
+			$ (item).removeAttr (normalizeTabindexName ());
+		});
+	};
+
+	$.fn.hasTabindexAttr = function () {
+	    if (this.length <= 0) {
+	        return false;
+	    }
+
+	    var attributeNode = this[0].getAttributeNode (normalizeTabindexName ());
+        return attributeNode ? attributeNode.specified : false;
+	};
+
+	$.fn.hasTabindex = function () {
+        return this.hasTabindexAttr () || canHaveDefaultTabindex (this);
+	};
+
     /**
      * Makes all matched elements available in the tab order by setting their tabindices to "0".
      */
