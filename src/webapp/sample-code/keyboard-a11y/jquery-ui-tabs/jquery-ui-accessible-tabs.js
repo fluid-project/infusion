ec88eb95044df30a3d93a05dee6efcd827defa68
Colin Clark <colinbdclark@gmail.com>
2008-06-07 02:25:58 +0000

    FLUID-527: Applied David Bolter's great ARIA patch. With David's help, added support for changing the activedescendent when either the mouse or keyboard is used.
    Reformatting and style cleanup.
    
    
    svn path=/fluid/components/trunk/; revision=5090

--- src/webapp/sample-code/keyboard-a11y/jquery-ui-tabs/jquery-ui-accessible-tabs.js
+++ src/webapp/sample-code/keyboard-a11y/jquery-ui-tabs/jquery-ui-accessible-tabs.js
@@ -11,53 +11,75 @@ https://source.fluidproject.org/svn/LICENSE.txt
 
 var fluid = fluid || {};
 
-fluid.accessibleTabs =  function () {
+(function ($) {
 
-    function activateTabHandler (tabsContainer, tabs) {
-        return function (elementToActivate) {
-            tabsContainer.tabs ('select', tabs.index (elementToActivate));
+    // Private functions.
+    var createActiveDescendentHandler = function (tablist) {
+        return function (event, ui) {
+            tablist.ariaState ("activedescendant", $(ui.tab).parent().attr("id"));
         };
-    }
-
-    return {
-        setupTabs: function (tabsId) {
-            jQuery("#" + tabsId).tabs ();
-        },
-
+    };
+    
+    var setupTabs = function (tabsId) {
+        $("#" + tabsId).tabs();
+    };
+    
+    // Public API.
+    fluid.accessibletabs =  {
         makeTabsKeyNavigable: function (tabsId) {
-            var tabsContainer = jQuery("#" + tabsId);
-            var tabs = tabsContainer.children("li");
+            var tablist = $("#" + tabsId);
+            var tabs = tablist.children("li");
 
-            // Put the tabs container in the tab focus order. Take each tab *out* of the tab order
+            // Put the tablist in the tab focus order. Take each tab *out* of the tab order
             // so that they can be navigated with the arrow keys instead of the tab key.
-            tabsContainer.tabbable ();
+            tablist.tabbable ();
 
+            var keyboardSelect = function (tabToSelect) {
+                tablist.tabs ('select', tabs.index(tabToSelect));
+            };
+            
             // Make the tabs selectable:
             //  * Pass in the container for the tabs (the <ul>)--the plugin binds keyboard handlers to this.
-            //  * null because we don't want to have any special callbacks during selection
+            //  * utilize option willSelect: to make the tab actually selected.
             //  * lastly, the options object allows to simply specify the direction (which defaults to vertical)
-            tabs.selectable (tabsContainer, null, {direction: jQuery.a11y.orientation.HORIZONTAL});
-
-            // Make the tabs activatable. Pass in a handler that just calls the standard tabs select function.
-            tabs.activatable (activateTabHandler (tabsContainer, tabs));
+            tabs.selectable (tablist, {willSelect: keyboardSelect}, {direction: $.a11y.orientation.HORIZONTAL});
         },
 
         addARIA: function (tabsId, panelsId) {
-            var tabsContainer = jQuery ("#" + tabsId);
-            var tabs = tabsContainer.children ("li");
-            var panels = jQuery ("#" + "panels > div");
+			var tablist = $("#" + tabsId).tabs();
+			var tabs = tablist.children("li");
+            var panels = $("#" + "panels > div");
 
-            tabsContainer.ariaRole ("tablist");
-            tabs.ariaRole ("tab");
-            panels.ariaRole ("tabpanel");
+            // Give the container a role of tablist
+            tablist.ariaRole("tablist");
+            
+            // Each tab should have a role of Tab, 
+            // and a "position in set" property describing its order within the tab list.
+            tabs.each (function(i, tab) {
+            	$(tab).ariaRole("tab").ariaState("posinset", i);
+            });
 
-            // need to add labelledby properties
+            // Give each panel a role of tabpanel
+            panels.ariaRole("tabpanel");
+            
+            // And associate each panel with its tab using the labelledby relation.
+			panels.each (function (i, panel) {
+				$(panel).ariaState("posinset", i).ariaState("labelledby", panel.id.split("Panel")[0] + "Tab");
+			});
+			
+            // Listen for tab selection and set the tab list's active descendent property.
+            tablist.bind("tabsselect", createActiveDescendentHandler(tablist));
         },
 
         initializeTabs: function (tabsId, panelsId) {
-            this.setupTabs (tabsId);
-            this.makeTabsKeyNavigable (tabsId);
-            this.addARIA (tabsId, panelsId);
+            setupTabs(tabsId);
+            this.makeTabsKeyNavigable(tabsId);
+            this.addARIA(tabsId, panelsId);
         }
     };
-} ();
+    
+    // When the document is loaded, initialize our tabs.
+    $(function(){
+        fluid.accessibletabs.initializeTabs("tabs", "panels");
+    });
+}) (jQuery);
