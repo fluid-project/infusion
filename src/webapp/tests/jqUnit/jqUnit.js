5d5a339edf1b316738be1ee8c9caa76a8bf6c1c8
Michelle D'Souza <michelled33@gmail.com>
2008-07-29 19:45:57 +0000

    NOJIRA: Adding licenses and copyrights.
    svn path=/fluid/components/trunk/; revision=5298

--- src/webapp/tests/jqUnit/jqUnit.js
+++ src/webapp/tests/jqUnit/jqUnit.js
@@ -1,3 +1,12 @@
+/*
+
+ Copyright 2008 University of Toronto
+
+ Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) 
+ and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+
+*/
+
 var jqUnit = jqUnit || {};
 
 (function ($) {
