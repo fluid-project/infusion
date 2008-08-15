5d03d94a8c2a210eb4f1005ba36475f9e5030fc7
Colin Clark <colinbdclark@gmail.com>
2008-08-15 22:46:32 +0000

    NOJIRA: More jQuery Tabs example cleanup in preparation for a tutorial and blog post.
    svn path=/fluid/components/trunk/; revision=5377

--- src/webapp/sample-code/keyboard-a11y/jquery-ui-tabs/jquery-ui-accessible-tabs.js
+++ src/webapp/sample-code/keyboard-a11y/jquery-ui-tabs/jquery-ui-accessible-tabs.js
@@ -14,77 +14,78 @@ https://source.fluidproject.org/svn/sandbox/tabindex/trunk/LICENSE.txt
 var fluid = fluid || {};
 
 (function ($) {
-    // Private functions.
-    var createActiveDescendentHandler = function (tablist) {
-        return function (event, ui) {
-            tablist.ariaState("activedescendant", $(ui.tab).parent().attr("id"));
-        };
-    };
     
-    var setupTabs = function (tabsId) {
-        $("#" + tabsId).tabs();
+    // Private functions.
+    var makeTabsSelectable = function (tablist) {
+        var tabs = tablist.children("li");
+
+        // Put the tablist in the tab focus order. Take each tab *out* of the tab order
+        // so that they can be navigated with the arrow keys instead of the tab key.
+        tablist.tabbable();
+
+        // Use the DHTML Style Guide interaction: as soon as a tab is focused, select it.
+        // Probably not th best approach for Ajax tabs. 
+        // In that case, consider using a separate activatable() handler.
+        var selectTab = function (tabToSelect) {
+            tablist.tabs('select', tabs.index(tabToSelect));
+        };
+        
+        // Make the tab list selectable with the arrow keys:
+        //  * Pass in the items you want to make selectable.
+        //  * Register your onSelect callback to make the tab actually selected.
+        //  * Specify the orientation of the tabs (the default is vertical)
+        tablist.selectable({ 
+            selectableElements: tabs,
+        	onSelect: selectTab,
+        	direction: $.a11y.orientation.HORIZONTAL
+        });
+    };
+
+    var addARIA = function (tablist, panelsId) {
+		var tabs = tablist.children("li");
+        var panels = $("#" + "panels" + " > div");
+
+        // Give the container a role of tablist
+        tablist.ariaRole("tablist");
+        
+        // Each tab should have a role of Tab, 
+        // and a "position in set" property describing its order within the tab list.
+        tabs.each (function(i, tab) {
+        	$(tab).ariaRole("tab");
+        });
+
+        // Give each panel a role of tabpanel
+        panels.ariaRole("tabpanel");
+        
+        // And associate each panel with its tab using the labelledby relation.
+		panels.each (function (i, panel) {
+			$(panel).ariaState("labelledby", panel.id.split("Panel")[0] + "Tab");
+		});
+        
+        // Listen for tab selection and set the tab list's active descendent property.
+        tablist.bind("tabsselect", function (event, ui) {
+            tablist.ariaState("activedescendant", $(ui.tab).parent().attr("id"));
+        });
     };
     
     // Public API.
-    fluid.accessibletabs =  {
-        makeTabsKeyNavigable: function (tabsId) {
-            var tablist = $("#" + tabsId);
-            var tabs = tablist.children("li");
+    fluid.accessibletabs = function (tabsId, panelsId) {
+        var tablist = $("#" + tabsId);
+        
+        // Remove the anchors in the list from the tab order.
+        tablist.find("a").tabindex(-1);
 
-            // Put the tablist in the tab focus order. Take each tab *out* of the tab order
-            // so that they can be navigated with the arrow keys instead of the tab key.
-            tablist.tabbable();
-
-            var keyboardSelect = function (tabToSelect) {
-                tablist.tabs('select', tabs.index(tabToSelect));
-            };
-            
-            // Make the tabs selectable:
-            //  * Pass in the container for the tabs (the <ul>)--the plugin binds keyboard handlers to this.
-            //  * utilize option willSelect: to make the tab actually selected.
-            //  * lastly, the options object allows to simply specify the direction (which defaults to vertical)
-            tablist.selectable({ 
-                selectableElements: tabs,
-            	onSelect: keyboardSelect,
-            	direction: $.a11y.orientation.HORIZONTAL
-            });
-        },
-
-        addARIA: function (tabsId, panelsId) {
-			var tablist = $("#" + tabsId).tabs();
-			var tabs = tablist.children("li");
-            var panels = $("#" + "panels > div");
-
-            // Give the container a role of tablist
-            tablist.ariaRole("tablist");
-            
-            // Each tab should have a role of Tab, 
-            // and a "position in set" property describing its order within the tab list.
-            tabs.each (function(i, tab) {
-            	$(tab).ariaRole("tab");
-            });
-
-            // Give each panel a role of tabpanel
-            panels.ariaRole("tabpanel");
-            
-            // And associate each panel with its tab using the labelledby relation.
-			panels.each (function (i, panel) {
-				$(panel).ariaState("labelledby", panel.id.split("Panel")[0] + "Tab");
-			});
-			
-            // Listen for tab selection and set the tab list's active descendent property.
-            tablist.bind("tabsselect", createActiveDescendentHandler(tablist));
-        },
-
-        initializeTabs: function (tabsId, panelsId) {
-            setupTabs(tabsId);
-            this.makeTabsKeyNavigable(tabsId);
-            this.addARIA(tabsId, panelsId);
-        }
+        // Turn the list into a jQuery UI tabs widget.
+        tablist.tabs();
+        
+        // Make them accessible.
+        makeTabsSelectable(tablist);
+        addARIA(tablist, panelsId);
     };
-    
+
     // When the document is loaded, initialize our tabs.
     $(function(){
-        fluid.accessibletabs.initializeTabs("tabs", "panels");
+        fluid.accessibletabs("tabs", "panels");
     });
+    
 }) (jQuery);
