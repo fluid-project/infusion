7f1f9cf597f30361473e49f5a439dcf636a1d65b
Colin Clark <colinbdclark@gmail.com>
2008-08-15 21:10:29 +0000

    NOJIRA: Code formatting, linting, and commenting in preparation for (hopefully) giving some of this code back to jQuery.
    svn path=/fluid/components/trunk/; revision=5376

--- src/webapp/tests/jqUnit/jqUnit.js
+++ src/webapp/tests/jqUnit/jqUnit.js
@@ -10,50 +10,71 @@
 var jqUnit = jqUnit || {};
 
 (function ($) {
+    
+    /************************
+     * Deep equality assert *
+     ************************/
+    
     function path(el) {
-      return el? "path " + el: "root path";
+        return el? "path " + el: "root path";
     }
+    
     function deepEqImpl(thing1, thing2, basename) {
-      basename = basename || "";
-      if (typeof(thing1) !== typeof(thing2)) {
-        return "Type mismatch at " + path(basename) + ": " + typeof(thing1) + " to " + typeof(thing2);
+        basename = basename || "";
+        
+        if (typeof(thing1) !== typeof(thing2)) {
+            return "Type mismatch at " + path(basename) + ": " + typeof(thing1) + " to " + typeof(thing2);
         }
-      if (thing1 === null ^ thing2 === null) {
-        return "Unexpected null value at " + path(basename);
+        
+        if (thing1 === null ^ thing2 === null) {
+            return "Unexpected null value at " + path(basename);
         }
-      if (typeof(thing1) !== 'object') {
-        if (thing1 !== thing2) {
-          return "Primitive mismatch at " + path(basename) + ": " + thing1 + " to " + thing2;
-          }
+        
+        if (typeof(thing1) !== 'object') {
+            if (thing1 !== thing2) {
+                return "Primitive mismatch at " + path(basename) + ": " + thing1 + " to " + thing2;
+            }
+        } else {
+            for (var name in thing1) {
+                var n1 = thing1[name];
+                var n2 = thing2[name];
+                var neq = deepEqDiag(n1, n2, (basename? basename + ".": "") + name);
+                if (neq) {
+                    return neq;
+                }
+            }
         }
-      else {
-        for (var name in thing1) {
-          var n1 = thing1[name];
-          var n2 = thing2[name];
-          var neq = deepEqDiag(n1, n2, (basename? basename + ".": "") + name);
-          if (neq) return neq;
-        }
-      }
-      return null;
+        
+        return null;
     }
 
     function deepEqDiag(thing1, thing2, basename) {
       var diag1 = deepEqImpl(thing1, thing2, basename);
-      if (diag1) return diag1;
-      var diag2 = deepEqImpl(thing2, thing1, basename);
-      if (diag2) return diag2;
-      return null;    
-      }
-    
-    jqUnit.deepEq = function(thing1, thing2) {
-      return !deepEqImpl(thing1, thing2) && !deepEqImpl(thing2, thing1);
+      if (diag1) {
+          return diag1;
       }
       
-    jqUnit.deepEqDiag = function(thing1, thing2) {
-      return deepEqDiag(thing1, thing2);
+      var diag2 = deepEqImpl(thing2, thing1, basename);
+      if (diag2) {
+          return diag2;
+      }
+      
+      return null;    
     }
+    
+    jqUnit.deepEq = function(thing1, thing2) {
+        return !deepEqImpl(thing1, thing2) && !deepEqImpl(thing2, thing1);
+    };
+      
+    jqUnit.deepEqDiag = function(thing1, thing2) {
+        return deepEqDiag(thing1, thing2);
+    };
 
 
+    /***********************
+     * xUnit Compatibility *
+     ***********************/
+    
     var jsUnitCompat = {
         assertEquals: function (msg, expected, actual) {
             jqUnit.equals (actual, expected, msg);
@@ -82,10 +103,12 @@ var jqUnit = jqUnit || {};
         assertNotNull: function (msg, expected) {
             jqUnit.ok (!(expected === null), msg);
         },
+        
         assertDeepEq: function (msg, expected, actual) {
           var diag = deepEqDiag(expected, actual);
           jqUnit.ok(diag === null, msg + (diag === null? "" : ": " + diag));
         },
+        
         assertDeepNeq: function(msg, unexpected, actual) {
           var diag = deepEqDiag(unexpected, actual);
           jqUnit.ok(diag !== null, msg);
@@ -95,6 +118,11 @@ var jqUnit = jqUnit || {};
     // Mix these compatibility functions into the jqUnit namespace.
     $.extend(jqUnit, jsUnitCompat);
 
+
+    /************************
+     * Other helpful checks *
+     ************************/
+    
     var testFns = {
         isVisible: function (msg, selector) {
             jqUnit.ok($(selector).is(':visible'), msg);
@@ -116,14 +144,18 @@ var jqUnit = jqUnit || {};
     // Mix these test functions into the jqUnit namespace.
     $.extend(jqUnit, testFns);
     
-    // TestCase object
+    
+    /*******************
+     * TestCase object *
+     *******************/
+    
     function TestCase (moduleName, setUpFn, tearDownFn) {
         this.moduleName = moduleName;
         this.setUp = setUpFn || null;
         this.tearDown = tearDownFn || null;
 
         jqUnit.module(this.moduleName);
-    };
+    }
 
     TestCase.prototype.test = function (string, testFn) {
         if (this.setUp) {
