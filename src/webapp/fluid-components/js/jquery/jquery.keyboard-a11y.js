80facb6c48b61f441ec38e2510b0b9b76ead5aca
Anastasia Cheetham <ac@anastasiacheetham.ca>
2008-08-18 15:44:05 +0000

    FLUID-832: Modify tests to work around a bug in jQuery.simulate (http://ui.jquery.com/bugs/ticket/3229).
    svn path=/fluid/components/trunk/; revision=5391

--- src/webapp/fluid-components/js/jquery/jquery.keyboard-a11y.js
+++ src/webapp/fluid-components/js/jquery/jquery.keyboard-a11y.js
@@ -172,7 +172,14 @@ https://source.fluidproject.org/svn/sandbox/tabindex/trunk/LICENSE.txt
 
     var activationHandler = function (binding) {
         return function (evt) {
-            if (evt.which === binding.key && binding.activateHandler && checkForModifier (binding, evt)) {
+// The following 'if' clause works in the real world, but there's a bug in the jQuery simulation
+// that causes keyboard simulation to fail in Safari, causing our tests to fail:
+//     http://ui.jquery.com/bugs/ticket/3229
+// The replacement 'if' clause works around this bug.
+// When this issue is resolved, we should revert to the original clause.
+//            if (evt.which === binding.key && binding.activateHandler && checkForModifier (binding, evt)) {
+            var code = (evt.which? evt.which : evt.keyCode);
+            if (code === binding.key && binding.activateHandler && checkForModifier (binding, evt)) {
                 binding.activateHandler (evt.target, evt);
                 evt.preventDefault ();
             }
