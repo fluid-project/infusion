c6722a8d30f6fe5860666fe66333412888a0d0b8
Jacob Farber <jacob.farber@utoronto.ca>
2008-10-16 21:08:53 +0000

    FLUID-1649: Experimenting with a semantic markup tree
    svn path=/fluid/components/trunk/; revision=5763

---
+++ src/webapp/fluid-components/js/markupMap.js
@@ -0,0 +1,39 @@
+/**
+ * @author Jacob
+ */
+/*
+  Map out all elements of interest on a page via selectors and a common name
+  
+ */
+var Markup = {
+    root: "body",
+    headers: {
+        h1: "h1",
+        h2: "h2",
+        h3: ".subheading",
+        h4: "h4",
+        h5: "h5",
+        h6: "h6"
+    },
+    navigation: {
+        tabset: {},
+        menu: {}
+    },
+    template: {
+        header: "",
+        footer: "",
+        column: ""
+    },
+    components: {
+        widgets: {
+            title: "",
+            options: "",
+            content: ""
+        },
+        datePicker: "",
+        slider: "",
+        button: "",
+        toolbar: "",
+        icon: ""
+    }
+}
