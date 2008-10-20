58816d506f37e07766fa835ec9396d0e5372eb74
Antranig Basman <antranig.basman@colorado.edu>
2008-10-20 20:16:54 +0000

    FLUID-1690: Fix draggable/mouseDown issues with dynamic patch to JQuery UI core
    svn path=/fluid/components/trunk/; revision=5775

--- src/webapp/fluid-components/js/fluid/Reorderer.js
+++ src/webapp/fluid-components/js/fluid/Reorderer.js
@@ -62,6 +62,16 @@ fluid_0_6 = fluid_0_6 || {};
         return selectables[0];
     }
     
+    if (!$.ui.mouse["PATCHED_FLUID_1690"]) {
+       var old = $.ui.mouse.mouseDown;
+       var replaced = function() {
+         old.apply(this, arguments);
+       };
+       $.ui.mouse.mouseDown = replaced;
+       $.ui.draggable.prototype.mouseDown = replaced;
+       $.ui.mouse["PATCHED_FLUID_1690"] = true;
+    }
+    
     function bindHandlersToContainer(container, keyDownHandler, keyUpHandler, mouseMoveHandler) {
         var actualKeyDown = keyDownHandler;
         var advancedPrevention = false;
