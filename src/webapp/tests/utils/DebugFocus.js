161304fdcc767a929245925415363df6f1c67a84
Antranig Basman <antranig.basman@colorado.edu>
2008-10-20 15:32:00 +0000

    NOJIRA: Updated DebugFocus for whitespace, also to use fluid.log, fluid.dumpEl, and removed restriction on dumped elements requiring to be focusable, pursuant to FLUID-1690 work
    svn path=/fluid/components/trunk/; revision=5770

--- src/webapp/tests/utils/DebugFocus.js
+++ src/webapp/tests/utils/DebugFocus.js
@@ -12,44 +12,40 @@ https://source.fluidproject.org/svn/LICENSE.txt
 fluid = fluid || {};
 
 fluid.debug = function () {
-    var printId = function (element) {
-        return (element) ? element.id : element;
-    };
 
     var outputEventDetails = function (eventType, event, caughtBy) {
-        if (typeof console !== 'undefined') {
-            console.debug (new Date() + " " + eventType + " was called on target " + printId (event.target) + ", caught by " + printId (caughtBy));
-        }
+        fluid.log(new Date() + " " + eventType + " was called on target " + fluid.dumpEl(event.target) + ", caught by " + fluid.dumpEl(caughtBy));
     };
 
     var focusOutputter = function (evt) {
-        outputEventDetails ("Focus", evt, this);
+        outputEventDetails("Focus", evt, this);
     };
 
     var blurOutputter = function (evt) {
-        outputEventDetails ("Blur", evt, this);
+        outputEventDetails("Blur", evt, this);
     };
 
     var addFocusChangeListeners = function (jQueryElements) {
-        jQueryElements.focus (focusOutputter);
-        jQueryElements.blur (blurOutputter);
+        jQueryElements.focus(focusOutputter);
+        jQueryElements.blur(blurOutputter);
     };
 
     return {
         listenForFocusEvents: function () {
+            fluid.setLogging(true);
             var focussableElements  = [];
 
             var everything = jQuery ("*");
-            everything.each (function () {
-               if (jQuery (this).hasTabindex ()) {
-                   focussableElements.push (this);
-               }
+            everything.each(function () {
+               //if (jQuery(this).hasTabindex()) {
+                   focussableElements.push(this);
+              // }
             });
 
-            addFocusChangeListeners (jQuery (focussableElements));
+            addFocusChangeListeners(jQuery(focussableElements));
         }
     }; // End of public return.
 } (); // End of fluid.debug namespace.
 
 // Call listenForFocusEvents when the document is ready.
-jQuery (document).ready (fluid.debug.listenForFocusEvents);
+jQuery(document).ready(fluid.debug.listenForFocusEvents);
