aae1a6ab69711bf045b672746ad2faba5ffd4354
Eli Cochran <eli@media.berkeley.edu>
2008-10-27 22:15:05 +0000

    NOJIRA: removed the formatFileSize test from FileQueueView since this is now in the Uploader test since the method is now part of uploader proper
    svn path=/fluid/components/trunk/; revision=5848

--- src/webapp/tests/fluid-tests/uploader2/js/FileQueueViewTests.js
+++ src/webapp/tests/fluid-tests/uploader2/js/FileQueueViewTests.js
@@ -63,40 +63,7 @@
         };
         
         var fileQueueViewTests = new jqUnit.TestCase("FileQueue Tests", setupFunction);
-             
-        fileQueueViewTests.test("formatFileSize()", function () {          
-            var testFileSize = function (testVal, expected) {
-                jqUnit.assertEquals("File size " + testVal + " bytes ", expected, 
-                                    fluid.uploader.formatFileSize(testVal));
-            };
-            
-            testFileSize(0, "0.0 KB");
-            testFileSize(1, "0.1 KB");
-            testFileSize(10, "0.1 KB");
-            testFileSize(50, "0.1 KB");
-            testFileSize(100, "0.1 KB");
-            testFileSize(150, "0.2 KB");
-            testFileSize(200, "0.2 KB");
-            testFileSize(400, "0.4 KB");
-            testFileSize(600, "0.6 KB");
-            testFileSize(800, "0.8 KB");
-            testFileSize(900, "0.9 KB");
-            testFileSize(910, "0.9 KB");
-            testFileSize(950, "1.0 KB");
-            testFileSize(999, "1.0 KB");
-            testFileSize(1023, "1.0 KB");
-            testFileSize(1024, "1.0 KB");
-            testFileSize(1025, "1.1 KB");
-            testFileSize(10000, "9.8 KB");
-            testFileSize(100000, "97.7 KB");
-            testFileSize(1000000, "976.6 KB");
-            testFileSize(10000000, "9.6 MB");
-            testFileSize(100000000, "95.4 MB");
-            testFileSize(10000000000, "9536.8 MB");
-            testFileSize(-1024, "");
-            testFileSize("string", "");
-        });
-        
+
         fileQueueViewTests.test("Scrollability", function () {
             var q = createFileQueue(qEl);
             
