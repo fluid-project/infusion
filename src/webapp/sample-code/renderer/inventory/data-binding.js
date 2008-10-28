c1e1e7fdc522dbc18319216651c21f3e532c0703
Anastasia Cheetham <ac@anastasiacheetham.ca>
2008-10-28 21:11:10 +0000

    FLUID-1695: Add bound and autobound examples. Need to expose the model to the viewer.
    svn path=/fluid/components/trunk/; revision=5874

--- src/webapp/sample-code/renderer/inventory/data-binding.js
+++ src/webapp/sample-code/renderer/inventory/data-binding.js
@@ -38,7 +38,7 @@ fluid.dataBindingExample = function () {
         
         fluid.selfRender(jQuery("#location"), locationTree, {debugMode: true});
 
-        var menuTree = {
+        var unboundTree = {
             children: [
                 {ID: "wine-checkboxes", optionlist: ["berg-rottland-riesline", "weissbergunder", "pinot-grigio", "gewurztraminer-turkheim"],
                                optionnames: ["Berg Rottland Riesling", "Weissbergunder", "Pinot Grigio", "Gewurztraminer Turkheim"],
@@ -58,10 +58,13 @@ fluid.dataBindingExample = function () {
                 {ID: "wine-row:", children: [
                     {ID: "wine", choiceindex: 3, parentRelativeID: "..::wine-checkboxes"},
                     {ID: "wine-label", choiceindex: 3, parentRelativeID: "..::wine-checkboxes"}
-                ]},
-                {ID: "cheese-checkboxes", optionlist: ["castelo-branco", "chevre-noir", "camembert", "la-sauvagine", "pastorella"],
-                               optionnames: ["Castelo Branco", "Chevre noir", "Camembert", "La Sauvagine", "Pastorella"],
-                               selection: []},
+                ]}
+        ]};
+        var boundTree = {
+            children: [
+                {ID: "cheese-checkboxes", optionlist: {valuebinding: "values"},
+                               optionnames: {valuebinding: "names"},
+                               selection: {valuebinding: "choice"}},
                 {ID: "cheese-row:", children: [
                     {ID: "cheese", choiceindex: 0, parentRelativeID: "..::cheese-checkboxes"},
                     {ID: "cheese-label", choiceindex: 0, parentRelativeID: "..::cheese-checkboxes"}
@@ -82,9 +85,52 @@ fluid.dataBindingExample = function () {
                     {ID: "cheese", choiceindex: 4, parentRelativeID: "..::cheese-checkboxes"},
                     {ID: "cheese-label", choiceindex: 4, parentRelativeID: "..::cheese-checkboxes"}
                 ]}
-        ]};
+            ]};
+        var cheeseModel = {
+            values: ["castelo-branco", "chevre-noir", "camembert", "la-sauvagine", "pastorella"],
+            names: ["Castelo Branco", "Chevre noir", "Camembert", "La Sauvagine", "Pastorella"],
+            choice: "chevre-noir"
+        };
 
-        fluid.selfRender(jQuery(".#menu"), menuTree, {debugMode: true});        
+        var autoBoundTree = {
+            children: [
+                {ID: "canape-checkboxes", optionlist: {valuebinding: "values"},
+                               optionnames: {valuebinding: "names"},
+                               selection: {valuebinding: "choice"}},
+                {ID: "canape-row:", children: [
+                    {ID: "canape", choiceindex: 0, parentRelativeID: "..::canape-checkboxes"},
+                    {ID: "canape-label", choiceindex: 0, parentRelativeID: "..::canape-checkboxes"}
+                ]},
+                {ID: "canape-row:", children: [
+                    {ID: "canape", choiceindex: 1, parentRelativeID: "..::canape-checkboxes"},
+                    {ID: "canape-label", choiceindex: 1, parentRelativeID: "..::canape-checkboxes"}
+                ]},
+                {ID: "canape-row:", children: [
+                    {ID: "canape", choiceindex: 2, parentRelativeID: "..::canape-checkboxes"},
+                    {ID: "canape-label", choiceindex: 2, parentRelativeID: "..::canape-checkboxes"}
+                ]},
+                {ID: "canape-row:", children: [
+                    {ID: "canape", choiceindex: 3, parentRelativeID: "..::canape-checkboxes"},
+                    {ID: "canape-label", choiceindex: 3, parentRelativeID: "..::canape-checkboxes"}
+                ]},
+                {ID: "canape-row:", children: [
+                    {ID: "canape", choiceindex: 4, parentRelativeID: "..::canape-checkboxes"},
+                    {ID: "canape-label", choiceindex: 4, parentRelativeID: "..::canape-checkboxes"}
+                ]},
+                {ID: "canape-row:", children: [
+                    {ID: "canape", choiceindex: 5, parentRelativeID: "..::canape-checkboxes"},
+                    {ID: "canape-label", choiceindex: 5, parentRelativeID: "..::canape-checkboxes"}
+                ]}
+            ]};
+        var canapeModel = {
+            values: ["asparagus", "chicken-ballotine", "shrimp-crostini", "beef-salsa-verde", "peppers-goat-cheese", "gorgonzola-figs"],
+            names: ["Filo Wrapped Asparagus", "Chicken Ballotine with Carrot Raita and Pomegranate Chutney", "Spicy Shrimp Crostini", "Broiled Beef Fillet Croutes with Salsa Verde", "Roasted Marinated Peppers with Goat Cheese", "Gorgonzola Stuffed Figs"],
+            choice: ["asparagus", "shrimp-crostini", "gorgonzola-figs"]
+        };
+
+        fluid.selfRender(jQuery("#wine-list"), unboundTree);
+        fluid.selfRender(jQuery("#cheese-list"), boundTree, {model: cheeseModel});
+        fluid.selfRender(jQuery("#canape-list"), autoBoundTree, {model: canapeModel, autoBind: true});
     };
     
     return {
