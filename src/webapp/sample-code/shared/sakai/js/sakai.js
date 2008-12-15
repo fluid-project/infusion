ae14e3c5e4d04f64770b2f02d79971e7f0c88206
Michelle D'Souza <michelled33@gmail.com>
2008-12-15 19:03:44 +0000

    FLUID-1963: Fixed the sakai example of UI Options so the component is only initialized once. Linted.
    svn path=/fluid/components/trunk/; revision=6222

--- src/webapp/sample-code/shared/sakai/js/sakai.js
+++ src/webapp/sample-code/shared/sakai/js/sakai.js
@@ -1,24 +1,40 @@
-            $(function () {
-                var initDialog = function () {
-                    // center dialog
-                    $('#dialog_container').css({
-                        left: ($(window).width() / 2) - ($('#dialog_container').width() / 2),
-                        top: ($(window).height() / 2) - ($('#dialog_container').height() / 2)
-                    });
-                    // bind close dialog button
-                    $("#close_dialog").click(function () {
-                        $("#dialog_container").css('display','none');
-                    });
-                
-                    // instantiate component
-                    fluid.uiOptions(".ui_options_container");
-                };
+/*
+Copyright 2008 University of Toronto
 
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
 
-                $('.skin').click(function () {
-                    // ajax call to pull the UI Options dialog into a container
-                    $('#dialog_content').load('../../../fluid-components/html/templates/UIOptions.html .ui_options_container', initDialog);
-                    // reveal dialog
-                    $("#dialog_container").css('display','block');                    
-                });
-            });   
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://source.fluidproject.org/svn/LICENSE.txt
+*/
+
+// Declare dependencies.
+/*global $, fluid*/
+
+$(function () {
+    var uiOptions;
+    var initDialog = function () {
+        // center dialog
+        $('#dialog_container').css({
+            left: ($(window).width() / 2) - ($('#dialog_container').width() / 2),
+            top: ($(window).height() / 2) - ($('#dialog_container').height() / 2)
+        });
+        // bind close dialog button
+        $("#close_dialog").click(function () {
+            $("#dialog_container").css('display', 'none');
+        });
+    
+        // instantiate component
+        uiOptions = fluid.uiOptions(".ui_options_container");
+    };
+
+    $('.skin').click(function () {
+        if (!uiOptions) {
+            // ajax call to pull the UI Options dialog into a container
+            $('#dialog_content').load('../../../fluid-components/html/templates/UIOptions.html .ui_options_container', initDialog);
+        }
+        
+        $("#dialog_container").css('display', 'block');                    
+    });
+});   
