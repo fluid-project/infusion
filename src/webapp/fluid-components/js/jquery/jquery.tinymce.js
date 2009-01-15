340b0ecba3c4bec5ce293a2e69ecbdd239d4ed9c
Michelle D'Souza <michelled33@gmail.com>
2009-01-15 17:17:06 +0000

    FLUID-2128: Adding missing copyrights.
    svn path=/fluid/components/tags/fluid-0.7/; revision=6319

--- src/webapp/fluid-components/js/jquery/jquery.tinymce.js
+++ src/webapp/fluid-components/js/jquery/jquery.tinymce.js
@@ -1,3 +1,14 @@
+/*
+Copyright 2008-2009 University of Cambridge
+Copyright 2008-2009 University of Toronto
+
+Licensed under the GNU General Public License or the MIT license.
+You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the GPL and MIT License at
+https://source.fluidproject.org/svn/sandbox/tabindex/trunk/LICENSE.txt
+*/
 // A super-simple jQuery TinyMCE plugin that actually works.
 // Written by Colin Clark
 
