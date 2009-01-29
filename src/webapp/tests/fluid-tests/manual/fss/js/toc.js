9a3ae8898577e329a4049330b2d8f0ba3dfe9f5b
Jacob Farber <jacob.farber@utoronto.ca>
2009-01-29 22:40:49 +0000

    FLUID-2170: Started test #3 - advanced layout class names
    svn path=/fluid/components/trunk/; revision=6381

--- src/webapp/tests/fluid-tests/manual/fss/js/toc.js
+++ src/webapp/tests/fluid-tests/manual/fss/js/toc.js
@@ -1,38 +1,32 @@
 $().ready(function(){
-    // parse the # from the filename
-    var uri = (""+window.location).split(".");
-    var currentTest = uri[uri.length-2];
-    
 	var FSSTestTOC = {
 		tests: [
-			{title: "FSS.Layout.Containers", id:1, note: "Each container should be as wide as the text inside says"},
-			{title: "FSS.Layout.Containers", id:2, note: "Each container should behave as the text inside says. May require adjusting the browser width."},
-			{title: "FSS.Layout.Advanced", id:1},
-			{title: "FSS.Layout.Advanced", id:2},
-			{title: "FSS.Layout.Advanced", id:3}
+			{title: "FSS.Layout.Containers", note: "A test for fixed width containers. Each container should be as wide as the text inside says"},
+			{title: "FSS.Layout.Containers", note: "A test for flexible and fixed width columns. Each container should behave as the text inside says. May require adjusting the browser width."},
+			{title: "FSS.Layout.Advanced"}
 		]
 	}
 
-	var groupTotal = 0, next = '#', prev = '#', thisTest = currentTest-1;
-	$.each(FSSTestTOC.tests, function(i){
-		groupTotal = (FSSTestTOC.tests[i].title === FSSTestTOC.tests[thisTest].title) ? groupTotal + 1 : groupTotal;
-	});
-	if (currentTest < FSSTestTOC.tests.length){
-		next = FSSTestTOC.tests[thisTest+1].title.toLowerCase() + '.' + (currentTest+1) + '.html';
-	}
-	if (currentTest > 1){
-		prev = FSSTestTOC.tests[thisTest-1].title.toLowerCase() + '.' + (currentTest-1) + '.html';
-	}
+    // parse the # from the filename
+    var uri = (""+window.location).split("fss/");
+    var currentTest = parseInt(uri[1].substr(0,1));
+	var next = '#', prev = '#', totalTests = FSSTestTOC.tests.length, thisTestIndex = currentTest-1, prevTestIndex = currentTest-2, nextTestIndex = currentTest;
+
+	next = (currentTest < totalTests) ? (currentTest+1) + "." + FSSTestTOC.tests[nextTestIndex].title.toLowerCase() + '.html' : next;
+	prev = (currentTest > 1) ? (currentTest-1) + "." + FSSTestTOC.tests[prevTestIndex].title.toLowerCase() + '.html' : prev;
 	
-	// Set links
+    
+	// LINKS
 	$('.options a[href=#prev]').attr('href',prev);
-	$('.options a[href=#next]').attr('href',next);
-	
-	// Set heading
-	$('.options h1').text(FSSTestTOC.tests[thisTest].title);
-    // Set Notes
-    $('#note').text(FSSTestTOC.tests[thisTest].note);
-	// Set test #
-    $('#page').text('Test ' + (currentTest) + ' of ' + FSSTestTOC.tests.length);
+	$('.options a[href=#next]').attr('href',next);	
+    
+	// HEADING
+	$('.options h1').text(FSSTestTOC.tests[thisTestIndex].title);
+    
+    // NOTES
+    $('#note').text(FSSTestTOC.tests[thisTestIndex].note);
+    
+	// TEST ID
+    $('#page').text('Test ' + (currentTest) + ' of ' + totalTests);
     
 });
