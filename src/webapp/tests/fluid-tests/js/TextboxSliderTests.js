a1dd72aa85874d4ba7d9b9062651704209f036ec
Michelle D'Souza <michelled33@gmail.com>
2009-03-10 15:36:13 +0000

    FLUID-2317: Fixed the TextboxSlider tests.
    svn path=/fluid/components/trunk/; revision=6701

--- src/webapp/tests/fluid-tests/js/TextboxSliderTests.js
+++ src/webapp/tests/fluid-tests/js/TextboxSliderTests.js
@@ -19,37 +19,45 @@ https://source.fluidproject.org/svn/LICENSE.txt
         var tests = new jqUnit.TestCase("TextboxSlider Tests");
         
         tests.test("Test Init", function () {
-            expect(1);
+            expect(2);
             fluid.textboxSlider(".fl-textbox-slider");
             jqUnit.assertEquals("Slider value is set to input value", 15, $(".fl-slider").slider("value"));
+            jqUnit.assertEquals("Textbox value is set", 15, $(".fl-textbox").val());
             
         });
 
+        var testSetting = function (valToTest, expected) {
+            var slider = $(".fl-slider");
+            var textbox = $(".fl-textbox");
+            
+            slider.slider("value", valToTest);
+            jqUnit.assertEquals("Slider value should be " + expected, expected, slider.slider("value"));
+            textbox.val(valToTest);
+            textbox.change();
+            jqUnit.assertEquals("Textbox value should be the " +expected, expected, textbox.val());            
+        };
+        
         tests.test("Test Min/Max Size", function () {
-    //        expect(2);
+            expect(12);
             var textboxSlider = fluid.textboxSlider(".fl-textbox-slider", {min: 5, max: 55});
             
-            $(".fl-slider").slider("value", 56);
-            jqUnit.assertEquals("Slider value should be the max", 55, $(".fl-slider").slider("value"));
-            $(".fl-textbox").val(56);
-            jqUnit.assertEquals("Textbox value should be the max", 55, $(".fl-textbox").val());            
-
-            $(".fl-slider").slider("value", 4);
-            jqUnit.assertEquals("Slider value should be the min", 5, $(".fl-slider").slider("value"));            
+            testSetting(56, 55);
+            testSetting(55, 55);
+            testSetting(4, 5);
+            testSetting(25, 25);
+            testSetting(-5, 5);
+            testSetting(5, 5);
         });
 
         tests.test("Test Negative Scale", function () {
-            expect(3);
+            expect(10);
             fluid.textboxSlider(".fl-textbox-slider", {min: -15, max: -5});
             
-            $(".fl-slider").slider("value", 56);
-            jqUnit.assertEquals("Slider value should be the max", -5, $(".fl-slider").slider("value"));
-
-            $(".fl-slider").slider("value", -10);
-            jqUnit.assertEquals("Slider value should be the value", -10, $(".fl-slider").slider("value"));
-
-            $(".fl-slider").slider("value", -16);
-            jqUnit.assertEquals("Slider value should be the min", -15, $(".fl-slider").slider("value"));
+            testSetting(56, -5);
+            testSetting(-10, -10);
+            testSetting(-16, -15);
+            testSetting(-15, -15);
+            testSetting(-5, -5);
             
         });
 
