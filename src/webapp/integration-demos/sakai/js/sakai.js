d3600c89bef7cb1668a7b4ad71f867d966d8f174
Jacob Farber <jacob.farber@utoronto.ca>
2009-05-22 19:07:22 +0000

    FLUID-2481, FLUID-2380: tweaked the jquery ui css to prevent a bug in IE where content would disappear when using the newly styled accordion for the first time. Markup has changed enough to prevent the layout from taking up too much space.
    svn path=/fluid/infusion/trunk/; revision=7214

--- src/webapp/integration-demos/sakai/js/sakai.js
+++ src/webapp/integration-demos/sakai/js/sakai.js
@@ -65,7 +65,7 @@ $(function () {
     dialog_container.dialog({
     	bgiframe: true,
         title: 'User Interface Options',
-    	width: '60em',
+    	width: '72em',
     	modal: true,
         autoOpen: false,
         draggable: true
