ebb693b2a414beb350e7a2deaf6bae5651d715fc
Jacob Farber <jacob.farber@utoronto.ca>
2009-09-22 19:12:12 +0000

    FLUID-3150: Fixes for the uploader demo, better code structure
    svn path=/fluid/infusion/trunk/; revision=8044

--- src/webapp/demos/uploader/js/uploader.js
+++ src/webapp/demos/uploader/js/uploader.js
@@ -1,21 +1,28 @@
+var demo = demo || {};
+
 (function () {
+    demo.initUploader = function () {
+        fluid.progressiveEnhanceableUploader(".flc-uploader", ".fl-progEnhance-basic", {
+            demo: true,                
+            uploadManager: {
+                type: "fluid.swfUploadManager",        
+                options: {
+                   // Set the uploadURL to the URL for posting files to your server.
+                   uploadURL: "http://myserver.com/uploadFiles",
     
-    var initUploader = function () {
-        var myUploader = fluid.progressiveEnhanceableUploader(".flc-uploader", ".fl-progEnhance-basic", {
-            demo: true,
-            uploadManager: "fluid.swfUploadManager"
-        });        
+                   // This option points to the location of the SWFUpload Flash object that ships with Fluid Infusion.
+                   flashURL: "../../../lib/swfupload/flash/swfupload.swf"
+                }
+            },
+            decorators: [{
+                type: "fluid.swfUploadSetupDecorator",
+                options: {
+                    // This option points to the location of the Browse Files button used with Flash 10 clients.
+                    flashButtonImageURL: "../../../components/uploader/images/browse.png"
+                }
+            }]
+        });    
     }
-    
-    $(document).ready(function () {        
-        $("button").bind("click", function () {
-
-            var urlSelector = "../../../components/uploader/html/Uploader.html";
-            $(".uploaderUI").load(urlSelector);
-        });
-    });
-
-
 })();
 
 
