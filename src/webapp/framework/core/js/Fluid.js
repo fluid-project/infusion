180ca5237c7c70c167e7861171c4cf90d86d658e
Colin Clark <colinbdclark@gmail.com>
2009-11-05 20:42:41 +0000

    FLUID-3363: Updating fluid.fail() for standard development.
    svn path=/fluid/infusion/trunk/; revision=8597

--- src/webapp/framework/core/js/Fluid.js
+++ src/webapp/framework/core/js/Fluid.js
@@ -29,8 +29,8 @@ var fluid = fluid || fluid_1_2;
     fluid.fail = function (message) {
         fluid.setLogging(true);
         fluid.log(message.message? message.message : message);
-        throw new Error(message);
-        //message.fail(); // Intentionally cause a browser error by invoking a nonexistent function.
+        //throw new Error(message);
+        message.fail(); // Intentionally cause a browser error by invoking a nonexistent function.
     };
     
     /**
