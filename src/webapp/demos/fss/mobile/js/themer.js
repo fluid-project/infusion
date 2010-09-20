c1524408a4827a64d9a5f64dad7887462ee77b61
Justin Obara <obarajustin@gmail.com>
2010-09-20 14:29:53 +0000

    FLUID-3649: Adding in the copyright information to the themer.js file.
    svn path=/fluid/infusion/trunk/; revision=10038

--- src/webapp/demos/fss/mobile/js/themer.js
+++ src/webapp/demos/fss/mobile/js/themer.js
@@ -1,4 +1,15 @@
-﻿/*global fluid, jQuery, window*/
+﻿/*
+Copyright 2009 University of Toronto
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://source.fluidproject.org/svn/LICENSE.txt
+*/
+
+/*global fluid, jQuery, window*/
 
 var fluid = fluid || {};
 
