f5cbdfd99930edc8b573271eda3132ac5bd6e81b
Justin Obara <obarajustin@gmail.com>
2010-10-07 14:55:32 +0000

    FLUID-3751: Merging the progress demo that Golam and others have been working on, into trunk. Golam has further patches, which will be applied after the code has been moved to trunk.
    The branch will be deleted after Jan has finished using it for a demo.
    
    svn path=/fluid/infusion/trunk/; revision=10131

--- src/webapp/demos/progress/js/progress.js
+++ src/webapp/demos/progress/js/progress.js
@@ -1,5 +1,6 @@
 /*
 Copyright 2009 University of California, Berkeley
+Copyright 2010 OCAD University
 
 Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
@@ -17,14 +18,15 @@ var demo = demo || {};
     var myProgress;
     
     /**
-     * Used to simulate an application that would call Progress.
-     * @param {Object} aProgress  the Progress object to call
-     * @param {Integer} percent   the starting percentage when first called and then the current percentage when called recursively
-     * @param {Integer} steps     the number of recursive steps to use for the simulated progress
-     *                            note: because of some randomness inserted into the simulation 
-     *                            for realism, the number of steps will actually be much less
+     * Used to simulate an application that would call Progress.    
+     * @param {Integer} percent         the starting percentage when first called and then the current percentage when called recursively
+     * @param {Integer} steps           the number of recursive steps to use for the simulated progress
+     *                                  note: because of some randomness inserted into the simulation 
+     *                                  for realism, the number of steps will actually be much less
+     * @param {Function} stepFunction   the stepFunction update the progress component
+     * @param {Function} finishFunction the finishFunction enables the submit button, hides the progress simulation and update the text                          
      */
-    var anIllusionOfProgress = function (aProgress, percent, steps) {
+    var simulateTime = function (percent, steps, stepFunction, finishFunction) {
         
         var invokeAfterRandomDelay = function (fn) {
             var delay = Math.floor(Math.random() * 1000 + 100);
@@ -40,27 +42,96 @@ var demo = demo || {};
         if (percent < 100) {
             // bump up the current percentage
             percent = Math.round(Math.min(percent + increment + aSmallRandomNumber(), 100));
-            // create a label for the progress
-            var currentProgressLabel = percent + "% Complete";
-            // tell progress to update
-            aProgress.update(percent, currentProgressLabel);
+ 
+            // update the progress component
+            stepFunction(percent);
+
             // after a random delay, do it all over again
             invokeAfterRandomDelay(function () {
-                anIllusionOfProgress(aProgress, percent, steps);
+                simulateTime(percent, steps, stepFunction, finishFunction);
             });
         } else {
-            myProgress.hide(2000);
+            finishFunction();
         }
         
     };
     
-    demo.initClickProgress = function () {
-        myProgress = fluid.progress("#main", {
+    var timeSimulator = function (percent, steps) {
+        var that = {};
+                
+        that.start = function (stepFunction, finishFunction) {
+            // simulate time
+            simulateTime(percent, steps, stepFunction, finishFunction);            
+        };
+
+        return that;
+    };
+
+    demo.initShoppingDemo = function (percent, steps) {
+        var submitButton = $("#submit-section button");
+        var statusText = $(".status-text");
+        var restartDemo = $(".restart-demo");
+        var liveRegion = $(".region");
+        var statusArea = $(".status-area");
+       
+        var timer = timeSimulator(percent, steps);         
+         
+        // set initial aria roles and aria-controls to some of the containers
+        statusArea.attr("role", "status");
+        submitButton.attr("aria-controls", "status-message");
+         
+        // hide link to the demo page
+        restartDemo.hide();        
+         
+        // initialize text on element with the class status-text
+        // TODO: Language strings should be external to code.
+        statusText.text("Confirm and submit your order.");
+                
+        // here's where we create the progress component
+        var myProgress = fluid.progress("#progress-container", {
             speed: 1000
         });
-        $("#showButton").click(function () {
-            anIllusionOfProgress(myProgress, 0, 200);
-            $("#showButton").blur();
+        
+        var setAriaLiveRegionAttr = function (liveRegion) {
+            // set the aria live region attributes
+            liveRegion.attr("aria-channel", "notify");
+            liveRegion.attr("aria-relevant", "all");
+            liveRegion.attr("aria-atomic", "false");
+            liveRegion.attr("aria-live", "assertive");
+            liveRegion.attr("role", "description");
+        };      
+        
+        var myFinishFunction = function () {
+            myProgress.container.fadeOut("slow", function () {
+                // change text on element with class status-text after  progress simulation is complete   
+                // TODO: More English strings in code
+                statusText.text("Order Submitted. Demo finished.").show();            
+                // set the live region attributes to the div element
+                setAriaLiveRegionAttr(liveRegion);            
+                // re-enable the link to restart the demo
+                restartDemo.show();        
+            });
+        };        
+        
+        // progress component specific
+        var stepFunction = function (percent) {
+             // create a label for the progress
+             // TODO: English string in code
+            var currentProgressLabel = percent + "% Complete";            
+            // tell progress to update
+            myProgress.update(percent, currentProgressLabel);
+        };
+        
+        submitButton.click(function () {
+            timer.start(stepFunction, myFinishFunction);
+            submitButton.blur();
+            
+            // disable the button
+            submitButton.removeClass("enabled");
+            submitButton.addClass("disabled");
+            submitButton.attr("disabled", "disabled");  
+                    
+            statusText.hide();             
         });   
     };
 
