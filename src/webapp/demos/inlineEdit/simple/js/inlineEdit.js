4fbffb6c775e74ad1c7613041f7b7c27133da26d
Justin Obara <obarajustin@gmail.com>
2010-10-08 17:58:08 +0000

    FLUID-3752: Committing Mike's patch ( inlineEditDemoCSSCleanup.txt ) which re-implements the inlineEdit demo based on the latest wireframes. Made one minor change which was to remove the surrounding <div> from the instruction text.
    svn path=/fluid/infusion/trunk/; revision=10135

--- src/webapp/demos/inlineEdit/simple/js/inlineEdit.js
+++ src/webapp/demos/inlineEdit/simple/js/inlineEdit.js
@@ -1,7 +1,5 @@
 /*
-Copyright 2008-2009 University of Cambridge
-Copyright 2008-2009 University of Toronto
-Copyright 2008-2009 University of California, Berkeley
+Copyright 2010 OCAD University
 
 Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
@@ -18,6 +16,17 @@ https://source.fluidproject.org/svn/LICENSE.txt
 var demo = demo || {};
 (function ($, fluid) {
     
+    var undoRenderer = function (that, targetContainer) {
+        var markup = 
+            "<span class='flc-undo'>" +
+            "<span class='undoContainer' role='button'><a href='#' class='undoControl'><img src='../images/undo.jpg' alt='Undo edit'></a></span>" +
+            "<span class='redoContainer' role='button'><a href='#' class='redoControl'><img src='../images/redo.jpg' alt='Redo edit'></a></span>" +
+            "</span>";
+        var markupNode = $(markup);
+        targetContainer.append(markupNode);
+        return markupNode;
+    };
+    
     /**
      * Initialize all simple inline edit components present on the inline-edit 
      * demo.
@@ -27,33 +36,59 @@ var demo = demo || {};
         /**
          * Simple inline edits example.
          */            
-        fluid.inlineEdit(".simpleEdit", {
+        fluid.inlineEdit(".simpleEditTitle", {
             componentDecorators: {
-                type: "fluid.undoDecorator"
+                type: "fluid.undoDecorator",
+                options: {
+                    selectors: demo.initInlineEdit.selectors,
+                    renderer: undoRenderer
+                }
             },
             urls: {
-                textEditButtonImage: "../../../../components/inlineEdit/images/edit_icon-01.png"
-            }
+                textEditButtonImage: "../images/pencil.jpg"
+            },
+            strings: {
+                editModeToolTip: demo.initInlineEdit.strings.editModeToolTip
+            },
+            styles: {
+                textEditButton: "fl-inlineEdit-button",
+                edit: "fl-inlineEdit-title-edit"
+            },
+            tooltipText: demo.initInlineEdit.strings.tooltipTextTitle
         });
         
-        /**
-         * Multiple inline text editors.
-         */
-        fluid.inlineEdits(".multipleEdit", {
-            selectors: {
-                text: ".editableText",
-                editables : "p"
-            },
+        fluid.inlineEdit(".simpleEditCaption", {
             componentDecorators: {
-                type: "fluid.undoDecorator"
+                type: "fluid.undoDecorator",
+                options: {
+                    selectors: demo.initInlineEdit.selectors,
+                    renderer: undoRenderer
+                }
             },
             urls: {
-                textEditButtonImage: "../../../../components/inlineEdit/images/edit_icon-01.png"
-            }
+                textEditButtonImage: "../images/pencil.jpg"
+            },
+            strings: {
+                editModeToolTip: demo.initInlineEdit.strings.editModeToolTip
+            },            
+            tooltipText: demo.initInlineEdit.strings.tooltipTextCaption
         });
     };
         
     demo.initInlineEdit = function () {        
         inlineSimpleEditSetup();
+    };  
+    
+    demo.initInlineEdit.strings = {
+        editModeToolTip : "Press Enter, Tab, or click elsewhere when done.",
+        tooltipTextCaption : "Edit caption.  Select or press Enter.", 
+        tooltipTextTitle : "Edit title.  Select or press Enter."
+    };
+    
+    demo.initInlineEdit.selectors = {
+        undoContainer: ".undoContainer",
+        undoControl: ".undoControl",
+        redoContainer: ".redoContainer",
+        redoControl: ".redoControl"
     };    
 })(jQuery, fluid);
