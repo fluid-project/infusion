a2393d71942b7130efc165770fc14b5b077ecebe
Antranig Basman <antranig.basman@colorado.edu>
2010-10-18 19:16:16 +0000

    FLUID-3767: Detection strategy for "focusin" event no longer works on IE using jQuery framework version of delegate. Removing detect since future versions of jQuery will be compliant.
    svn path=/fluid/infusion/trunk/; revision=10148

--- src/webapp/framework/core/js/jquery.keyboard-a11y.js
+++ src/webapp/framework/core/js/jquery.keyboard-a11y.js
@@ -88,23 +88,16 @@ var fluid = fluid || fluid_1_2;
             $.data(this, NAMESPACE_KEY, data);
         });
     };
-/** Global focus manager - makes use of jQuery delegate plugin if present,
- * detecting presence of "focusin" event.
+/** Global focus manager - makes use of "focusin" event supported in jquery 1.4.2 or later.
  */
 
-    var lastFocusedElement = "disabled";
+    var lastFocusedElement = null;
     
-    if ($.event.special["focusin"]) {
-        lastFocusedElement = null;
-        $(document).bind("focusin", function(event){
-            lastFocusedElement = event.target;
-        });
-    }
+    $(document).bind("focusin", function(event){
+        lastFocusedElement = event.target;
+    });
     
     fluid.getLastFocusedElement = function () {
-        if (lastFocusedElement === "disabled") {
-           fluid.fail("Focus manager not enabled - please include jquery.delegate.js or equivalent for support of 'focusin' event");
-        }
         return lastFocusedElement;
     }
 
