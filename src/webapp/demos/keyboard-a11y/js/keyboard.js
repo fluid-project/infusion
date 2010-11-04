2b3188f0abb9a0dc7354fa0b26f474a1a576e295
Anastasia Cheetham <ac@anastasiacheetham.ca>
2010-11-04 18:21:54 +0000

    FLUID-3799: Beginnings of the re-write of the Keyboard-A11y demo: Simplified the HTML for the image thumbnails a bit; Made the thumbnails navigable/activatable; Started making the rater accessible.
    svn path=/fluid/infusion/trunk/; revision=10183

--- src/webapp/demos/keyboard-a11y/js/keyboard.js
+++ src/webapp/demos/keyboard-a11y/js/keyboard.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2009 University of Toronto
+Copyright 2010 OCAD University
 
 Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
@@ -12,65 +12,40 @@ https://source.fluidproject.org/svn/LICENSE.txt
 /*global jQuery*/
 /*global fluid*/
 
-/*global demo*/
 var demo = demo || {};
 
 (function ($, fluid) {
     
-    demo.initKeyboardNav = function () {
-        
-        /************************ Menu ****************************/
-        var level1tabs = $(".level_1");
-        
-        level1tabs.fluid("selectable", {
-            selectableSelector : "> li",
-            autoSelectFirstItem : true,
+    var makeRaterNavigable = function () {
+        $(demo.initImageRater.selectors.rater).fluid("tabbable");
+        $(demo.initImageRater.selectors.rater).fluid("selectable", {
             direction: fluid.a11y.orientation.HORIZONTAL,
-            onUnselect: function(el){
-                $(el).removeClass("active revealSubMenu");
-            },
-            onSelect : function(el){
-                $(el).addClass("active");
-            }        
+            onSelect: function (star) {
+                console.log("star selected: " + star);
+            }
         });
+    };
 
-        var level1Activation = function (e) {
-            $(e.target).addClass("revealSubMenu");
-            $(".level_2 li:eq(0)", e.target).focus(); // hilight the first child;
-        };
-
-        fluid.activatable(level1tabs, level1Activation, {
-            additionalBindings: [{
-                modifier: null,
-                key: $.ui.keyCode.DOWN,
-                activateHandler: level1Activation
-            }]
+    var makeThumbnailsNavigable = function () {
+        var thumbs = $(demo.initImageRater.selectors.thumbnails);
+        thumbs.fluid("tabbable");
+        thumbs.fluid("selectable", {
+            direction: fluid.a11y.orientation.HORIZONTAL,
+            onSelect: function (thumbEl) {
+                var src = $("img", thumbEl).attr("src");
+                $(demo.initImageRater.selectors.image).attr("src", src);
+            }
         });
-        
-        
-        /************************ Sub Menu ****************************/
-        var level2tabs = $(".level_2");
-        
-        
-        $.each(level2tabs, function (i, submenu) {
-            submenu = $(submenu);
-            submenu.fluid("selectable", {
-                selectableSelector : "> li",
-                direction: fluid.a11y.orientation.VERTICAL,
-                onUnselect: function(el){
-                    $(el).removeClass("subActive");
-                },
-                onSelect : function(el){                    
-                    $(el).addClass("subActive");                    
-                }        
-            });            
-        });
+    };
 
-        var level2Activation = function (e) {            
-        };
-
-        fluid.activatable(level2tabs, level2Activation);        
-        
+    demo.initImageRater = function () {
+        makeThumbnailsNavigable();
+//        makeRaterNavigable();
     };
     
+    demo.initImageRater.selectors = {
+        thumbnails: ".demo-container-imageThumbnails",
+        rater: ".demo-container-fiveStar",
+        image: ".demo-image-mainImage"
+    };
 })(jQuery, fluid);
