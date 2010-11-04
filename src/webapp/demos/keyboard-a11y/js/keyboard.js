a177ef1af616906d4be854f0dc731c9a17f6d8ae
Anastasia Cheetham <ac@anastasiacheetham.ca>
2010-11-04 21:03:18 +0000

    FLUID-3799: Adjust keyboard interaction to match design (arrow keys select, but selection does not view image - activation does); Modify ranking html in prep for implementation.
    svn path=/fluid/infusion/trunk/; revision=10184

--- src/webapp/demos/keyboard-a11y/js/keyboard.js
+++ src/webapp/demos/keyboard-a11y/js/keyboard.js
@@ -16,36 +16,65 @@ var demo = demo || {};
 
 (function ($, fluid) {
     
-    var makeRaterNavigable = function () {
-        $(demo.initImageRater.selectors.rater).fluid("tabbable");
-        $(demo.initImageRater.selectors.rater).fluid("selectable", {
+    var makeRankerNavigable = function () {
+        var ranker = $(demo.initImageRanker.selectors.rater);
+        ranker.fluid("tabbable");
+        ranker.fluid("selectable", {
+            selectableSelector: "img",
             direction: fluid.a11y.orientation.HORIZONTAL,
             onSelect: function (star) {
-                console.log("star selected: " + star);
+                console.log("star selected: " + $(star).attr("class"));
             }
         });
     };
 
     var makeThumbnailsNavigable = function () {
-        var thumbs = $(demo.initImageRater.selectors.thumbnails);
-        thumbs.fluid("tabbable");
-        thumbs.fluid("selectable", {
+        var thumbContainer = $(demo.initImageRanker.selectors.thumbnails);
+        thumbContainer.fluid("tabbable");
+        thumbContainer.fluid("selectable", {
             direction: fluid.a11y.orientation.HORIZONTAL,
             onSelect: function (thumbEl) {
-                var src = $("img", thumbEl).attr("src");
-                $(demo.initImageRater.selectors.image).attr("src", src);
+                $(thumbEl).addClass(demo.initImageRanker.styles.selected);
+            },
+            onUnselect: function (thumbEl) {
+                $(thumbEl).removeClass(demo.initImageRanker.styles.selected);
             }
         });
     };
 
-    demo.initImageRater = function () {
+    var imageActivationHandler = function (evt) {
+        // remove the "activated" style from any other thumbnails
+        $("img", demo.initImageRanker.selectors.thumbnails).removeClass(demo.initImageRanker.styles.activated);
+        
+        // display the selected image in the main viewer
+        var thumb = $(evt.target);
+        var src = thumb.attr("src");
+        $(demo.initImageRanker.selectors.image).attr("src", src);
+
+        // add the "activated" class to the current thumbnail
+        thumb.addClass(demo.initImageRanker.styles.activated);
+    };
+    var makeThumbnailsActivatable = function () {
+        var thumbs = $("img", demo.initImageRanker.selectors.thumbnails);
+        thumbs.fluid("activatable", imageActivationHandler);
+
+        // add the same handler to the click event
+        thumbs.click(imageActivationHandler);
+    };
+
+    demo.initImageRanker = function () {
         makeThumbnailsNavigable();
-//        makeRaterNavigable();
+        makeThumbnailsActivatable();
     };
     
-    demo.initImageRater.selectors = {
+    demo.initImageRanker.selectors = {
         thumbnails: ".demo-container-imageThumbnails",
-        rater: ".demo-container-fiveStar",
+        rater: "#ranking",
         image: ".demo-image-mainImage"
+    };
+
+    demo.initImageRanker.styles = {
+        selected: "demo-selected",
+        activated: "demo-activated"
     };
 })(jQuery, fluid);
