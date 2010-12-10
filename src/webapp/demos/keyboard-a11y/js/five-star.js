a18578a61d3b5d4fb024c33d3adc63e7e20468b4
Jonathan Hung <jhung@ocad.ca>
2010-12-10 19:41:15 +0000

    FLUID-3757: Reverted a classname change to the fivestar widget that broke functionality.
    svn path=/fluid/infusion/trunk/; revision=10381

--- src/webapp/demos/keyboard-a11y/js/five-star.js
+++ src/webapp/demos/keyboard-a11y/js/five-star.js
@@ -19,7 +19,7 @@ var demo = demo || {};
 
     // This assumes the className is of the form "star-x" where x is the starNum
     var getStarNum = function (el) {
-        var className = $(el).attr("class").match("demo-star-[1-5]")[0];
+        var className = $(el).attr("class").match("star-[1-5]")[0];
         return parseInt(className.charAt(className.length - 1));
     };
 
