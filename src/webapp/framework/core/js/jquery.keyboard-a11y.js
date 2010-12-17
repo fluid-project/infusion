9cc59b0744fd2f67babadf80014d23fd4ea81026
Antranig Basman <antranig.basman@colorado.edu>
2010-12-17 21:09:07 +0000

    FLUID-3487: Commiting "j" version of FLUID-3487 patch which enables correct operation of DMB on a domain containing a fluid.selectable on IE8
    svn path=/fluid/infusion/trunk/; revision=10437

--- src/webapp/framework/core/js/jquery.keyboard-a11y.js
+++ src/webapp/framework/core/js/jquery.keyboard-a11y.js
@@ -297,7 +297,7 @@ var fluid = fluid || fluid_1_3;
             sc_that.activeItemIndex = elements.length - 1;
         }
         if (sc_that.activeItemIndex >= 0) {
-            $(elements[sc_that.activeItemIndex]).focus();
+            fluid.focus(elements[sc_that.activeItemIndex]);
         }
     };
 
@@ -305,7 +305,7 @@ var fluid = fluid || fluid_1_3;
         // FLUID-3590: FF 3.6 and Safari 4.x won't fire blur() when programmatically moving focus.
         var selElm = selectionContext.selectedElement();
         if (selElm) {
-            selElm.blur();
+            fluid.blur(selElm);
         }
 
         unselectElement(selectionContext.selectedElement(), selectionContext);
@@ -382,7 +382,7 @@ var fluid = fluid || fluid_1_3;
                 if (selectionContext.activeItemIndex === NO_SELECTION) {
                     selectionContext.activeItemIndex = 0;
                 }
-                $(selectionContext.selectables[selectionContext.activeItemIndex]).focus();
+                fluid.focus(selectionContext.selectables[selectionContext.activeItemIndex]);
             }
 
            // Force focus not to bubble on some browsers.
@@ -480,7 +480,7 @@ var fluid = fluid || fluid_1_3;
      * Selects the specified element.
      */
     fluid.selectable.select = function(target, toSelect) {
-        $(toSelect).focus();
+        fluid.focus(toSelect);
     };
 
     /**
