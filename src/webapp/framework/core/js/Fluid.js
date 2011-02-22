8017397209c17b757b6cab29936f178ee865b7fd
Anastasia Cheetham <acheetham@ocad.ca>
2011-02-22 11:12:18 -0500

    FLUID-4064: Updated copyright dates

--- src/webapp/framework/core/js/Fluid.js
+++ src/webapp/framework/core/js/Fluid.js
@@ -12,7 +12,8 @@
 Copyright 2007-2010 University of Cambridge
 Copyright 2007-2009 University of Toronto
 Copyright 2007-2009 University of California, Berkeley
-Copyright 2010 Lucendo Development Ltd.
+Copyright 2010-2011 OCAD University
+Copyright 2010-2011 Lucendo Development Ltd.
 
 Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
