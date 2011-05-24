f45be6c7669021cefe7468dc342adba1fe61e153
Michelle D'Souza <michelled33@gmail.com>
2011-05-24 12:23:10 -0400

    FLUID-4235: Modified the demos to use the pageEnhancer.

--- src/webapp/integration-demos/uportal/js/portal.js
+++ src/webapp/integration-demos/uportal/js/portal.js
@@ -106,7 +106,7 @@ var demo = demo || {};
      * Initialization script for dynamically changing skins
      */
     var initSkinChange = function () {
-        var uiEnhancer = fluid.uiEnhancer(document);
+        var uiEnhancer = fluid.pageEnhancer().uiEnhancer;
         
         jQuery("#hc-skin").click(function () {
             uiEnhancer.updateModel(demo.hcLargeSettings);
