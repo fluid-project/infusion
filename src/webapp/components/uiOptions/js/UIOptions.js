d052d4ccb4b172e5d109cc91b0f166aeb841bb05
Michelle D'Souza <michelled33@gmail.com>
2011-06-01 16:32:11 -0400

    FLUID-4265: Pulling the storage strategy for settings out of UIEnhancer. Moving defaultSiteSettings into uiOptions.store.

--- src/webapp/components/uiOptions/js/UIOptions.js
+++ src/webapp/components/uiOptions/js/UIOptions.js
@@ -224,7 +224,6 @@ var fluid_1_4 = fluid_1_4 || {};
             },
             settingsStore: "{uiEnhancer}.settingsStore"
         },
-        defaultSiteSettings: "{uiEnhancer}.defaultSiteSettings",
         savedSelections: "{uiEnhancer}.model",
         textSize: {
             min: 1,
@@ -280,7 +279,7 @@ var fluid_1_4 = fluid_1_4 || {};
          */
         that.reset = function () {
             that.events.onReset.fire();
-            that.updateModel(fluid.copy(that.options.defaultSiteSettings));
+            that.updateModel(fluid.copy(that.settingsStore.options.defaultSiteSettings));
             that.refreshControlsView();
         };
         
