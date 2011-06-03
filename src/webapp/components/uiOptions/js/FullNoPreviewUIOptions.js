e07151a7ab56b064b6ff9f43435a386e55c77a8a
heidi valles <hvalles@ocad.ca>
2011-06-03 13:34:28 -0400

    FLUID-4229: Added full no preview component and added to demo

---
+++ src/webapp/components/uiOptions/js/FullNoPreviewUIOptions.js
@@ -0,0 +1,94 @@
+/*
+Copyright 2008-2009 University of Cambridge
+Copyright 2008-2009 University of Toronto
+Copyright 2010-2011 OCAD University
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
+*/
+
+// Declare dependencies
+/*global fluid_1_4:true, jQuery*/
+
+// JSLint options 
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+var fluid_1_4 = fluid_1_4 || {};
+
+(function ($, fluid) {
+    /**********************
+     * Full No Preview UI Options *
+     *********************/
+     
+    fluid.defaults("fluid.fullNoPreviewUIOptions", {
+        gradeNames: ["fluid.viewComponent", "autoInit"],            
+        components: {
+            uiOptions: {
+                type: "fluid.uiOptions",
+                container: "{fullNoPreviewUIOptions}.container"
+            }                     
+        }
+    });       
+    
+    // Options for UIOptions in fat panel mode
+    fluid.demands("fluid.uiOptions", ["fluid.fullNoPreviewUIOptions"], {
+        options: {
+            components: {
+                preview: {
+                    type: "fluid.emptySubcomponent"
+                }            
+            },    
+            resources: {
+                template: {
+                    url: "../../../../components/uiOptions/html/FullNoPreviewUIOptions.html"
+                }
+            }
+        }
+    });      
+     
+    
+    // Supply the template URL of "text and display" panel on the user preferences interface
+    fluid.demands("fluid.uiOptions.textControls", ["fluid.uiOptions"], {
+        options: {
+            resources: {
+                template: {
+                    url: "../../../../components/uiOptions/html/UIOptionsTemplate-text.html"
+                }
+            }
+        }
+    });
+
+    // Supply the template URL of "layout and navigation" panel on the user preferences interface
+    fluid.demands("fluid.uiOptions.layoutControls", ["fluid.uiOptions"], {
+        options: {
+            resources: {
+                template: {
+                    url: "../../../../components/uiOptions/html/UIOptionsTemplate-layout.html"
+                }
+            }
+        }
+    });
+
+    // Supply the template URL of "layout and navigation" panel on the user preferences interface
+    fluid.demands("fluid.uiOptions.linksControls", ["fluid.uiOptions"], {
+        options: {
+            resources: {
+                template: {
+                    url: "../../../../components/uiOptions/html/UIOptionsTemplate-links.html"
+                }
+            }
+        }
+    });    
+    // Supply the table of contents' template URL
+    fluid.demands("fluid.tableOfContents", ["fluid.uiEnhancer"], {
+        options: {
+            templateUrl: "../../../../components/uiOptions/html/TableOfContents.html"
+        }
+    });     
+  
+        
+})(jQuery, fluid_1_4);
