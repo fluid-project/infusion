6f54bf0bbd32edff189b4d2b5e969dfac73dcdeb
Justin Obara <obarajustin@gmail.com>
2011-06-07 22:09:20 -0400

    FLUID-4209: Starting to add in unit tests
    Commented out all old unit tests, will remove once all have been re-written. They are here just for reference to make sure that all the old cases are adequately covered.
    
    Currently wrote tests for the modelbuilder and headingCalculator subcomponents.

--- src/webapp/tests/component-tests/tableOfContents/js/TableOfContentsTests.js
+++ src/webapp/tests/component-tests/tableOfContents/js/TableOfContentsTests.js
@@ -17,79 +17,226 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
 
 (function ($) {
 
-    $(document).ready(function () {
-        var toc;
-        var options = {
-            components: {
-                levels: {
-                    options: {
-                        resources: {
-                            template: {
-                                url: "../../../../components/tableOfContents/html/TableOfContents.html"
-                            }
-                        }
+    // $(document).ready(function () {
+    //     var toc;
+    //     var options = {
+    //         components: {
+    //             levels: {
+    //                 options: {
+    //                     resources: {
+    //                         template: {
+    //                             url: "../../../../components/tableOfContents/html/TableOfContents.html"
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     };
+    // 
+    //     var tests = jqUnit.testCase("Table of Contents Tests");
+    //      
+    //     var testAfterRender = function (testFn) {
+    //         var toc;
+    //         
+    //         options.listeners = {
+    //             afterRender: function () {
+    //                 testFn(toc);
+    //                 start();
+    //             }
+    //         };
+    //         
+    //         toc = fluid.tableOfContents("#main", options);
+    //     };
+    //     
+    //     var testTocItem = function (item, name) {
+    //         var a = $("a", item);
+    //         jqUnit.assertEquals(name + " has text", name, a.text());
+    //         jqUnit.assertTrue(name + " has href", a.attr("href").indexOf("#" + name) > -1);            
+    //     };
+    //     
+    //     tests.asyncTest("TOC Creation", function () {
+    //         testAfterRender(function (toc) {
+    //             expect(22);
+    // 
+    //             var tocEl = $("#main").children().eq(0);
+    //             jqUnit.isVisible("Table of contents should be visible", tocEl);
+    //         
+    //             var items = $("li", tocEl);
+    //             jqUnit.assertEquals("10 headings", 10, items.length);
+    //         
+    //             testTocItem(items[0], "Amphibians");
+    //             testTocItem(items[1], "Toads");
+    //             testTocItem(items[2], "Natterjack Toads");
+    //             testTocItem(items[3], "Salamander");
+    //             testTocItem(items[4], "Newt");
+    //             testTocItem(items[5], "Birds");
+    //             testTocItem(items[6], "Anseriformes");
+    //             testTocItem(items[7], "Ducks");
+    //             testTocItem(items[8], "Mammals");
+    //             testTocItem(items[9], "CATT");
+    //         });            
+    //     });
+    //            
+    //     tests.asyncTest("Anchor insertion", function () {
+    //         testAfterRender(function (toc) {
+    //             expect(5);
+    // 
+    //             var anchors = $("a", "#amphibians-div");
+    //             jqUnit.assertEquals("5 headings in the amphibians section", 5, anchors.length);
+    // 
+    //             var anchor = anchors.eq(0);
+    //             jqUnit.assertEquals("Name is Amphibians", "Amphibians", anchor.attr("name"));         
+    //             jqUnit.assertEquals("No text", "", anchor.text());
+    //             jqUnit.assertFalse("No href", !!anchor.attr("href"));
+    //             jqUnit.assertEquals("The next element in the DOM is the heading", "amphibians", anchor.next().prop("id"));
+    //         });
+    //     });
+    // });
+    
+    fluid.staticEnvironment.tocTests = fluid.typeTag("fluid.tableOfContents.unitTests");
+    
+    fluid.demands("fluid.tableOfContents.levels", "fluid.tableOfContents.unitTests", {
+        options: {
+            resources: {
+                    template: {
+                        url: "../../../../components/tableOfContents/html/TableOfContents.html"
                     }
                 }
             }
-        };
+    });
+    
+    var skippedHeadings = {
+        headingTags: ["h1", "h6"],
+        anchorInfo: [{url: "#h1"}, {url: "#h6"}],
+        headingInfo: [
+            {level: 1, text: "h1", url: "#h1"},
+            {level: 6, text: "h6", url: "#h6"}
+        ],
+        model: [{
+            level: 1, 
+            text: "h1", 
+            url: "#h1",
+            headings: [{
+                headings: [{
+                    headings: [{
+                        headings: [{
+                            headings: [{
+                                level: 6, 
+                                text: "h6", 
+                                url: "#h6"
+                            }]
+                        }]
+                    }]
+                }]
+            }]
+        }]
+    };
+    
+    var linearHeadings = {
+        headingTags: ["h1", "h2", "h3", "h4", "h5", "h6"],
+        anchorInfo: [{url: "#h1"}, {url: "#h2"}, {url: "#h3"}, {url: "#h4"}, {url: "#h5"}, {url: "#h6"}],
+        headingInfo: [
+            {level: 1, text: "h1", url: "#h1"},
+            {level: 2, text: "h2", url: "#h2"},
+            {level: 3, text: "h3", url: "#h3"},
+            {level: 4, text: "h4", url: "#h4"},
+            {level: 5, text: "h5", url: "#h5"},
+            {level: 6, text: "h6", url: "#h6"}
+        ],
+        model: [{
+            level: 1, 
+            text: "h1", 
+            url: "#h1",
+            headings: [{
+                level: 2,
+                text: "h2",
+                url: "#h2",
+                headings: [{
+                    level: 3,
+                    text: "h3",
+                    url: "#h3",
+                    headings: [{
+                        level: 4,
+                        text: "h4",
+                        url: "#h4",
+                        headings: [{
+                            level: 5,
+                            text: "h5",
+                            url: "#h5",
+                            headings: [{
+                                level: 6, 
+                                text: "h6", 
+                                url: "#h6"
+                            }]
+                        }]
+                    }]
+                }]
+            }]
+        }]
+    };
+    
+    var createElm = function (tagName) {
+        return fluid.unwrap($("<" + tagName + "/>", {text: tagName}));
+    }
+    
+    var createElms = function (tagArray) {
+        return fluid.transform(tagArray, createElm);
+    };
+    
+    var toModelTests = function (headingInfo, expectedModel) {
+        var model = fluid.tableOfContents.modelBuilder.toModel(headingInfo);
+        jqUnit.assertDeepEq("headingInfo converted to model correctly", expectedModel, model);
+    };
+    
+    var convertToHeadingObjectsTests = function (headings, anchorInfo, expectedHeadingInfo) {
+        var modelBuilder = fluid.tableOfContents.modelBuilder();
+        var headingInfo = modelBuilder.convertToHeadingObjects(headings, anchorInfo);
+        jqUnit.assertDeepEq("Heading objects created correctly", expectedHeadingInfo, headingInfo);
+    };
+    
+    var assembleModelTests = function (headings, anchorInfo, expectedModel) {
+        var modelBuilder = fluid.tableOfContents.modelBuilder();
+        var model = modelBuilder.assembleModel(headings, anchorInfo);
+        jqUnit.assertDeepEq("model assembled correctly", expectedModel, model);
+    };
+    
+    $(document).ready(function () {
 
-        var tests = jqUnit.testCase("Table of Contents Tests");
-         
-        var testAfterRender = function (testFn) {
-            var toc;
-            
-            options.listeners = {
-                afterRender: function () {
-                    testFn(toc);
-                    start();
-                }
-            };
-            
-            toc = fluid.tableOfContents("#main", options);
-        };
+        var tocHeadingCalc = jqUnit.testCase("Table of Contents: Heading Calculator Tests");
+        var tocMBTests = jqUnit.testCase("Table of Contents: Model Builder Tests");
+        var tocLevelsTests = jqUnit.testCase("Table of Contents: Levels Tests");
+        var tocTests = jqUnit.testCase("Table of Contents Tests");
+    
+        // "fluid.tableOfContents.modelBuilder.headingCalculator" tests
         
-        var testTocItem = function (item, name) {
-            var a = $("a", item);
-            jqUnit.assertEquals(name + " has text", name, a.text());
-            jqUnit.assertTrue(name + " has href", a.attr("href").indexOf("#" + name) > -1);            
-        };
+        tocHeadingCalc.test("getHeadingLevel", function () {
+            var headingCalc = fluid.tableOfContents.modelBuilder.headingCalculator();
+            
+            for (var i = 1; i <= 6; i++) {
+                var tagName = "h" + i;
+                var heading = createElm(tagName);
+                jqUnit.assertEquals(tagName + " level", i, headingCalc.getHeadingLevel(heading));
+            }
+        });
         
-        tests.asyncTest("TOC Creation", function () {
-            testAfterRender(function (toc) {
-                expect(22);
-
-                var tocEl = $("#main").children().eq(0);
-                jqUnit.isVisible("Table of contents should be visible", tocEl);
-            
-                var items = $("li", tocEl);
-                jqUnit.assertEquals("10 headings", 10, items.length);
-            
-                testTocItem(items[0], "Amphibians");
-                testTocItem(items[1], "Toads");
-                testTocItem(items[2], "Natterjack Toads");
-                testTocItem(items[3], "Salamander");
-                testTocItem(items[4], "Newt");
-                testTocItem(items[5], "Birds");
-                testTocItem(items[6], "Anseriformes");
-                testTocItem(items[7], "Ducks");
-                testTocItem(items[8], "Mammals");
-                testTocItem(items[9], "CATT");
-            });            
+        // "fluid.tableOfContents.modelBuilder" tests
+        
+        tocMBTests.test("toModel: linear headings", function () {toModelTests(linearHeadings.headingInfo, linearHeadings.model);});
+        tocMBTests.test("toModel: skipped headings", function () {toModelTests(skippedHeadings.headingInfo, skippedHeadings.model);});
+        
+        tocMBTests.test("convertToHeadingObjects: linear headings", function () {
+            convertToHeadingObjectsTests(createElms(linearHeadings.headingTags), linearHeadings.anchorInfo, linearHeadings.headingInfo);
         });
-               
-        tests.asyncTest("Anchor insertion", function () {
-            testAfterRender(function (toc) {
-                expect(5);
-
-                var anchors = $("a", "#amphibians-div");
-                jqUnit.assertEquals("5 headings in the amphibians section", 5, anchors.length);
-
-                var anchor = anchors.eq(0);
-                jqUnit.assertEquals("Name is Amphibians", "Amphibians", anchor.attr("name"));         
-                jqUnit.assertEquals("No text", "", anchor.text());
-                jqUnit.assertFalse("No href", !!anchor.attr("href"));
-                jqUnit.assertEquals("The next element in the DOM is the heading", "amphibians", anchor.next().prop("id"));
-            });
+        tocMBTests.test("convertToHeadingObjects: skipped headings", function () {
+            convertToHeadingObjectsTests(createElms(skippedHeadings.headingTags), skippedHeadings.anchorInfo, skippedHeadings.headingInfo);
         });
+        
+        tocMBTests.test("assembleModel: linear headings", function () {
+            assembleModelTests(createElms(linearHeadings.headingTags), linearHeadings.anchorInfo, linearHeadings.model);
+        });
+        tocMBTests.test("assembleModel: skipped headings", function () {
+            assembleModelTests(createElms(skippedHeadings.headingTags), skippedHeadings.anchorInfo, skippedHeadings.model);
+        });
+
     });
 })(jQuery);
