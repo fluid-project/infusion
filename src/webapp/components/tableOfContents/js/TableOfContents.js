632271073c53b5c4c559f3b631975991f102a1ba
Harris Wong <harris.wong@gmail.com>
2011-06-20 11:10:11 -0400

    FLUID-4209: Added Show/hide function into ToC.

--- src/webapp/components/tableOfContents/js/TableOfContents.js
+++ src/webapp/components/tableOfContents/js/TableOfContents.js
@@ -60,6 +60,19 @@ var fluid_1_4 = fluid_1_4 || {};
             return that.headingTextToAnchor(heading);
         });
         
+        // TODO: is it weird to have hide and show on a component?
+        that.hide = function () {
+            if (that.tocContainer) {
+                that.tocContainer.hide();
+            }
+        };
+        
+        that.show = function () {
+            if (that.tocContainer) {
+                that.tocContainer.show();
+            }
+        };
+        
         that.model = that.modelBuilder.assembleModel(headings, that.anchorInfo);
         that.events.onReady.fire();
     };
