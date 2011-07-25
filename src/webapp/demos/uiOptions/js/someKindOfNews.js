7f8c2db0514b61293489f9c1054a4ab5767eadfc
Anastasia Cheetham <acheetham@ocad.ca>
2011-07-25 16:34:49 -0400

    FLUID-4351: Start on new UIO demo(s).
    Working with a copy of the test page until we have a new design for the demo. Still not sure if we'll have an al-in-one demo (as currently implemented) or three separate demos, but the technical implementation so far will still be relevant enough.

---
+++ src/webapp/demos/uiOptions/js/someKindOfNews.js
@@ -0,0 +1,65 @@
+/*
+Copyright 2011 OCAD University
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
+*/
+
+// Declare dependencies
+/*global skon:true, fluid, jQuery*/
+
+// JSLint options 
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+var skon = skon || {};
+(function ($, fluid) {       
+
+    fluid.staticEnvironment.skonEnvironment = fluid.typeTag("skon.demo");
+
+    /* Our demo script */   
+    skon.slidingUIOptions = function (panel, uioptions) {
+        fluid.demands("fluid.uiOptions.templatePath", ["fluid.fatPanelUIOptions", "skon.demo"], {
+            options: {
+                value: "{fatPanelUIOptions}.options.prefix"
+            }
+        });
+
+        fluid.demands("fluid.renderIframe", ["skon.demo"], {
+            options: {
+                markupProps: {
+                    src: "../../../components/uiOptions/html/FatPanelUIOptionsFrame.html"
+                }
+            }
+        });
+    
+        // Supply the table of contents' template URL
+        fluid.demands("fluid.tableOfContents.levels", ["fluid.tableOfContents", "skon.demo"], {
+            options: {
+                resources: {
+                    template: {
+                        forceCache: true,
+                        url: "../../../components/tableOfContents/html/TableOfContents.html"
+                    }
+                }
+            }
+        });
+
+        fluid.pageEnhancer({
+            classnameMap: {
+                theme: {
+                    "default": "skon-theme-basic"
+                }
+            }
+        });
+        
+        // Next, start up UI Options
+        fluid.fatPanelUIOptions(".flc-uiOptions-fatPanel", {
+            prefix: "../../../../components/uiOptions/html/"
+        });            
+    };
+    
+})(jQuery, fluid);
