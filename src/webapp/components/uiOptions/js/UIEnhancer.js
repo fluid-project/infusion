1ccbf09be0a1ec604330757032f6b96e7dc87c8b
Justin Obara <obarajustin@gmail.com>
2011-08-18 11:52:13 -0400

    FLUID-4370: Increasing specificity to force themes
    Attempting to provide a means to force a theme through greater
    specificity. This is needed for the styled options in the "Colour &
    Contrast" theme select box in UIO.

--- src/webapp/components/uiOptions/js/UIEnhancer.js
+++ src/webapp/components/uiOptions/js/UIEnhancer.js
@@ -141,10 +141,10 @@ var fluid_1_4 = fluid_1_4 || {};
             },
             "theme": {
                 "default": "",
-                "bw": "fl-theme-uio-bw fl-theme-bw",
-                "wb": "fl-theme-uio-wb fl-theme-wb",
-                "by": "fl-theme-uio-by fl-theme-by",
-                "yb": "fl-theme-uio-yb fl-theme-yb"
+                "bw": "fl-theme-uio-bw fl-theme-bw fl-force-theme",
+                "wb": "fl-theme-uio-wb fl-theme-wb fl-force-theme",
+                "by": "fl-theme-uio-by fl-theme-by fl-force-theme",
+                "yb": "fl-theme-uio-yb fl-theme-yb fl-force-theme"
             },
             "layout": "fl-layout-linear",
             "links": "fl-text-underline fl-text-bold fl-text-larger", 
