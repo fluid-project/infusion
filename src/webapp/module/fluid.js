3ab793aa1bdbfbb099438015c13799dd32ce079a
Antranig Basman <antranig.basman@colorado.edu>
2012-02-16 22:50:43 -0700

    FLUID-4609: Added standard file header and linting for fluid.js module loader

--- src/webapp/module/fluid.js
+++ src/webapp/module/fluid.js
@@ -1,3 +1,21 @@
+/*
+Copyright 2009 University of Toronto
+Copyright 2010-2011 OCAD University
+Copyright 2011 Lucendo Development Ltd.
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
+*/
+/*global require, module, console, __dirname*/
+
+// JSLint options 
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+
 (function () {
     var fs = require("fs"),
         path = require("path"),
@@ -23,11 +41,11 @@
 
     var includes = fs.readFileSync(buildPath("includes.json"));
 
-    includes = JSON.parse(includes)
+    includes = JSON.parse(includes);
 
     for (var i = 0; i < includes.length; ++i) {
         loadInContext(includes[i]);
-    };
+    }
     
     var fluid = context.fluid;
 
