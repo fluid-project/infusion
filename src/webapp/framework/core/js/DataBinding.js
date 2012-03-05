ef7f237b0bce74ff91fb4150fd42d5597533d21f
Antranig Basman <antranig.basman@colorado.edu>
2012-03-05 10:28:14 -0700

    FLUID-4633: Incomplete work on the ChangeApplier

--- src/webapp/framework/core/js/DataBinding.js
+++ src/webapp/framework/core/js/DataBinding.js
@@ -446,6 +446,50 @@ var fluid_1_5 = fluid_1_5 || {};
         };
     }
     
+    
+    // TODO: put into framework
+    fluid.hasChangeSource = function (changes, source) {
+        return fluid.find(changes, function(change) {
+            if (change.source === source) {
+                return true;
+            }
+        });
+    };
+    
+    /** Add a listener to a ChangeApplier event that only acts in the case the event
+     * has not come from the specified source (typically ourself)
+     * @param modelEvent An model event held by a changeApplier (typically applier.modelChanged)
+     * @param path The path specification to listen to
+     * @param source The source value to exclude (direct equality used)
+     * @param func The listener to be notified of a change
+     */
+    fluid.addSourceGuardedListener = function(modelEvent, path, source, func) {
+        modelEvent.addListener(path, 
+            function(newModel, oldModel, changes) {
+                if (!fluid.hasChangeSource(changes, source)) {
+                    func.apply(null, arguments);
+            }
+        });
+    };
+
+    /** Convenience method to fire a change event to a specified applier, including
+     * a supplied "source" identified (perhaps for use with addSourceGuardedListener)
+     */ 
+    fluid.fireSourcedChange = function (applier, path, value, source) {
+        applier.fireChangeRequest({
+            path: path,
+            value: value,
+            source: source
+        });         
+    };
+    
+    /** Dispatches a list of changes to the supplied applier */
+    fluid.requestChanges = function (applier, changes) {
+        for (var i = 0; i < changes.length; ++ i) {
+            applier.fireChangeRequest(changes[i]);
+        }  
+    };
+    
   
     fluid.makeChangeApplier = function (model, options) {
         options = options || {};
@@ -455,6 +499,9 @@ var fluid_1_5 = fluid_1_5 || {};
             modelChanged: fluid.event.getEventFirer(false, false, "modelChanged event")
         };
         var that = {
+        // For now, we don't use "id" to avoid confusing component detection which uses
+        // a simple algorithm looking for that field
+            changeid: fluid.allocateGuid(),
             model: model
         };
         
@@ -690,6 +737,10 @@ var fluid_1_5 = fluid_1_5 || {};
             bindRequestChange(ation);
 
             return ation;
+        };
+        
+        that.hasChangeSource = function (source) {
+          
         };
         
         return that;
