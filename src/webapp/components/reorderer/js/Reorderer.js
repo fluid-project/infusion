febcb672b510e65e0c9c296234ef53830d7680c6
Antranig Basman <antranig.basman@colorado.edu>
2012-06-29 14:29:56 -0600

    FLUID-4693: Removing stray console.log statement

--- src/webapp/components/reorderer/js/Reorderer.js
+++ src/webapp/components/reorderer/js/Reorderer.js
@@ -814,7 +814,6 @@ var fluid_1_5 = fluid_1_5 || {};
                     });
                 },
                 onMove: function (item, newPosition) {
-                    console.log("labeller onMove: map is ", movedMap);
                     fluid.clear(movedMap); // if we somehow were fooled into missing a defocus, at least clear the map on a 2nd move
                     // This unbind is needed for FLUID-4693 with Chrome 18, which generates a focusOut when
                     // simply doing the DOM manipulation to move the element to a new position.   
