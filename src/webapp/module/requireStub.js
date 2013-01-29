d7d1b9f458b0324500ec521d70cbefc851a3b1b4
Yura Zenevich <yura.zenevich@gmail.com>
2013-01-29 10:58:11 -0500

    FLUID-4911: Updated requireStub.js to support import of modules that are cross node/browser.

--- src/webapp/module/requireStub.js
+++ src/webapp/module/requireStub.js
@@ -14,20 +14,52 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
 
 var fluid_1_5 = fluid_1_5 || {};
 
-(function (fluid) {
+(function (fluid, $) {
 
-    var requireStub = function (moduleName) {
-        if (moduleName !== "infusion") {
-            throw new Error("requireStub.js cannot be used to test modules other than Infusion, " +
-                "which is capable of running in both Node.js and a browser.");
-        }
-        return fluid;
-    };
-
-    window.require = requireStub;
     if (typeof (fluid) === "undefined") {
         throw new Error("Please include requireStub.js after Fluid Infusion in the document's <head>");
     }
+
+    var requireStub = function (moduleName) {
+        var module = requireStub.modules[moduleName];
+        if (!module) {
+            throw new Error("Module " + moduleName + " is not loaded. Please edit your requireModules.json.");
+        }
+        return module;
+    };
+
+    $.ajax({
+        url: "../js/requireModules.json",
+        async: false,
+        dataType: "json"
+    }).done(function (modules) {
+        requireStub.modules = {};
+        $.each(modules, function (moduleName, modulePath) {
+            var module = fluid.getGlobalValue(moduleName);
+            if (module) {
+                requireStub.modules[moduleName] = module;
+                return;
+            }
+            module = fluid.getGlobalValue(modulePath);
+            if (module) {
+                requireStub.modules[moduleName] = module;
+                return;
+            }
+            $.ajax({
+                async: false,
+                url: modulePath,
+                dataType:"script"
+            }).done(function () {
+                requireStub.modules[moduleName] = fluid.getGlobalValue(moduleName);
+            }).fail(function () {
+                throw new Error("Error loading script.");
+            });
+        })
+    }).fail(function () {
+        throw new Error("Please provide requireModules.json along with your test.");
+    });
+
+    window.require = requireStub;
     fluid.require = requireStub;
 
-}(fluid_1_5));
+}(fluid_1_5, jQuery));
