6156e5aa4a8f00c0e330cf718b1ef08eec34c520
Justin Obara <obarajustin@gmail.com>
2013-02-13 11:59:33 -0500

    FLUID-4794: refactored tests.
    Refactored the progressive enhancement tests to remove duplicate assertion code.

--- src/webapp/tests/framework-tests/enhancement/js/ProgressiveEnhancementTests.js
+++ src/webapp/tests/framework-tests/enhancement/js/ProgressiveEnhancementTests.js
@@ -81,6 +81,24 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
     
     fluid.registerNamespace("fluid.test");
     
+    var STATIC_ENV_NOT_SET = NOT_CHECKED = false;
+    var STATIC_ENV_SET = CHECKED = true;
+    
+    var assertEnvKey = function (env, key, shouldExist) {
+        jqUnit.expect(1);
+        var val = fluid.getGlobalValue(env)[key];
+        if (shouldExist) {
+            jqUnit.assertValue("The key '" + key + "' should exist in " + env, val);
+        } else {
+            jqUnit.assertNoValue("The key '" + key + "' should not exist in " + env, val);
+        }
+    };
+    
+    var assertKeyInEnvironments = function (key, staticShoulExist, checkedShouldExist) {
+        assertEnvKey("fluid.staticEnvironment", key, staticShoulExist);
+        assertEnvKey("fluid.enhance.checked", key, checkedShouldExist);
+    };
+    
     jqUnit.test("fluid.enhance.typeToKey", function () {
         jqUnit.expect(2);
         
@@ -93,7 +111,7 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
     });
     
     jqUnit.test("fluid.enhance.check", function () {
-        jqUnit.expect(9);
+        jqUnit.expect(5);
         fluid.test.setEnvironment = function () {
             jqUnit.assertTrue("The setEnvironment check was run", true);
             return true;
@@ -116,18 +134,18 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
         
         // Run the check and add keys to the static environment
         fluid.enhance.check(checkSet);
-        // Verify that the keys have been added to the static environment 
+        // Verify that the keys have been added to the static environment and the checked obj
         fluid.each(checkSet, function (val, key) {
             var staticKey = fluid.enhance.typeToKey(key);
-            jqUnit.assertValue("The key '" + staticKey + "', should exist in the static environment", fluid.staticEnvironment[staticKey]);
+            assertKeyInEnvironments(staticKey, STATIC_ENV_SET, CHECKED);
         });
         
         // Run the check but don't add keys to the static environment
         fluid.enhance.check(checksNotSet);
-        // Verify that the keys have not been added to the static environment
+        // Verify that the keys have not been added to the static environment, but have are in the checked obj
         fluid.each(checksNotSet, function (val, key) {
             var staticKey = fluid.enhance.typeToKey(key);
-            jqUnit.assertUndefined("The key '" + staticKey + "', should not exist in the static environment", fluid.staticEnvironment[staticKey]);
+            assertKeyInEnvironments(staticKey, STATIC_ENV_NOT_SET, CHECKED);
         });
 
         // Rerun a check that has been run before. It should not execute the check func.
@@ -138,27 +156,24 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
     }); 
     
     jqUnit.test("fluid.forget", function () {
-        jqUnit.expect(8);
+        jqUnit.expect(2);
         var checked = "checked.key";
         var unChecked = "static.unChecked";
         var neverAdded = "never.added";
         
-        // Add and verify that keys are in the static environment
+        // Add and verify that keys are in the static environment and checked
         var checkedKey = fluid.enhance.typeToKey(checked);
         fluid.staticEnvironment[checkedKey] = fluid.typeTag(checked);
         fluid.enhance.checked[checkedKey] = true;
-        jqUnit.assertValue("The key '" + checkedKey + "', should exist in the static environment", fluid.staticEnvironment[checkedKey]);
-        jqUnit.assertValue("The key '" + checkedKey + "', should exist in fluid.enhance.checked", fluid.enhance.checked[checkedKey]);
+        assertKeyInEnvironments(checkedKey, STATIC_ENV_SET, CHECKED);
         
         var unCheckedKey = fluid.enhance.typeToKey(unChecked);
         fluid.staticEnvironment[unCheckedKey] = fluid.typeTag(unChecked);
-        jqUnit.assertValue("The key '" + unCheckedKey + "', should exist in the static environment", fluid.staticEnvironment[unCheckedKey]);
-        jqUnit.assertNoValue("The key '" + unCheckedKey + "', should not exist in fluid.enhance.checked", fluid.enhance.checked[unCheckedKey]);
+        assertKeyInEnvironments(unCheckedKey, STATIC_ENV_SET, NOT_CHECKED);
         
         // forget keys
         fluid.enhance.forget(checked);
-        jqUnit.assertNoValue("The key '" + checkedKey + "', should not exist in the static environment", fluid.staticEnvironment[checkedKey]);
-        jqUnit.assertNoValue("The key '" + checkedKey + "', should not exist in fluid.enhance.checked", fluid.enhance.checked[checkedKey]);
+        assertKeyInEnvironments(checkedKey, STATIC_ENV_NOT_SET, NOT_CHECKED);
         
         var origSE = fluid.copy(fluid.staticEnvironment);
         var origChecked = fluid.copy(fluid.enhance.checked);
@@ -168,7 +183,6 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
     });
     
     jqUnit.test("fluid.enhance.forgetAll", function () {
-        jqUnit.expect(8);
         var typeNames = ["check.one", "check.two"];
         var keys = [];
         
@@ -178,8 +192,7 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
             keys.push(key);
             fluid.staticEnvironment[key] = fluid.typeTag(val);
             fluid.enhance.checked[key] = true;
-            jqUnit.assertValue("The key '" + key + "', should exist in the static environment", fluid.staticEnvironment[key]);
-            jqUnit.assertValue("The key '" + key + "', should exist in fluid.enhance.checked", fluid.enhance.checked[key]);
+            assertKeyInEnvironments(key, STATIC_ENV_SET, CHECKED);
         });
         
         // Remove all checked keys
@@ -187,8 +200,7 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
         
         // Verify that the checked keys have been removed
         fluid.each(keys, function (val) {
-            jqUnit.assertNoValue("The key '" + val + "', should not exist in the static environment", fluid.staticEnvironment[val]);
-            jqUnit.assertNoValue("The key '" + val + "', should not exist in fluid.enhance.checked", fluid.enhance.checked[val]);
+            assertKeyInEnvironments(val, STATIC_ENV_NOT_SET, NOT_CHECKED);
         });
     });
 
