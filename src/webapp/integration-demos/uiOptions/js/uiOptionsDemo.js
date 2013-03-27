9a43109a8ea335bed1b10437010c6f63aeeabd68
Anastasia Cheetham <ac@anastasiacheetham.ca>
2013-03-27 11:11:16 -0400

    FLUID-4323: Add simplified content as independent panel; convert TOC back to TOC-only

--- src/webapp/integration-demos/uiOptions/js/uiOptionsDemo.js
+++ src/webapp/integration-demos/uiOptions/js/uiOptionsDemo.js
@@ -5,10 +5,6 @@
     var pathToTocTemplate = "../../components/tableOfContents/html/TableOfContents.html";
     var pathToTemplates = "../../components/uiOptions/html/";
 
-    var extraSiteSettings = {
-        selfVoicing: false
-    };
-    
     demo.initPageEnhancer = function (customThemeName) {
         fluid.pageEnhancer({
             gradeNames: ["fluid.uiEnhancer.defaultActions", "fluid.uiEnhancer.extraActions"],
@@ -33,6 +29,7 @@
                 options: {
                     templates: {
                         uiOptions: "templates/FatPanelUIOptions.html",
+                        simplifiedContent: "templates/UIOptionsTemplate-simplifiedContent.html",
                         selfVoicing: "templates/UIOptionsTemplate-selfVoicing.html"
                      }
                 }
