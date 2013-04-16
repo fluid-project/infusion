d842b7467b67d05aaa823934d7a2dd99b028940f
Cindy Qi Li <cli@ocad.ca>
2013-04-16 17:01:25 -0400

    FLUID-4927: Grouped each integration tests.

--- src/webapp/tests/component-tests/uiOptions/js/FatPanelUIOptionsTests.js
+++ src/webapp/tests/component-tests/uiOptions/js/FatPanelUIOptionsTests.js
@@ -59,29 +59,6 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
         }
     });
 
-    fluid.defaults("fluid.tests.fatPanelMungingIntegration", {
-        gradeNames: ["fluid.test.testEnvironment", "autoInit"],
-        components: {
-            fatPanel: {
-                type: "fluid.uiOptions.fatPanel",
-                container: ".flc-uiOptions-fatPanel",
-                createOnEvent: "{mungingIntegrationTester}.events.onTestCaseStart",
-                options: fluid.merge(null, fluid.tests.uiOptions.mungingIntegrationOptions, {
-                    iframeRenderer: {
-                        options: {
-                            markupProps: {
-                                src: "./FatPanelUIOptionsFrame.html"
-                            }
-                        }
-                    }
-                })
-            },
-            mungingIntegrationTester: {
-                type: "fluid.tests.mungingIntegrationTester"
-            }
-        }
-    });
-
     fluid.tests.testComponent = function (uiOptionsLoader, uiOptions) {
         jqUnit.assertEquals("IFrame is invisible and keyboard inaccessible", false, uiOptions.iframeRenderer.iframe.is(":visible"));
 
@@ -121,14 +98,6 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
             fluid.tests.uiOptions.checkModelSelections("panelModel from defaults", defaultSiteSettings, panelModel);
             fluid.tests.uiOptions.checkModelSelections("pageModel from panelModel", pageModel, panelModel);  
         };
-    };
-
-    fluid.tests.testEnhancerTransit = function testEnhancerTransit(uiOptions) {
-        var cMap = fluid.tests.uiOptions.enhancerOptions.classnameMap;
-        jqUnit.assertEquals("classnameMap transferred to outer UIEnhancer", cMap.textFont["default"],
-             uiOptions.pageEnhancer.options.classnameMap.textFont["default"]);
-        jqUnit.assertEquals("classnameMap transferred to inner UIEnhancer", cMap.textFont["default"],
-             uiOptions.iframeRenderer.iframeEnhancer.options.classnameMap.textFont["default"]);
     };
 
     fluid.defaults("fluid.tests.fatPanelIntegrationTester", {
@@ -172,6 +141,41 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
             }]
         }]
     });
+
+    /*******************************************************************************
+     * UIOptions fatPanel options munging integration tests
+     *******************************************************************************/
+
+    fluid.defaults("fluid.tests.fatPanelMungingIntegration", {
+        gradeNames: ["fluid.test.testEnvironment", "autoInit"],
+        components: {
+            fatPanel: {
+                type: "fluid.uiOptions.fatPanel",
+                container: ".flc-uiOptions-fatPanel",
+                createOnEvent: "{mungingIntegrationTester}.events.onTestCaseStart",
+                options: fluid.merge(null, fluid.tests.uiOptions.mungingIntegrationOptions, {
+                    iframeRenderer: {
+                        options: {
+                            markupProps: {
+                                src: "./FatPanelUIOptionsFrame.html"
+                            }
+                        }
+                    }
+                })
+            },
+            mungingIntegrationTester: {
+                type: "fluid.tests.mungingIntegrationTester"
+            }
+        }
+    });
+
+    fluid.tests.testEnhancerTransit = function testEnhancerTransit(uiOptions) {
+        var cMap = fluid.tests.uiOptions.enhancerOptions.classnameMap;
+        jqUnit.assertEquals("classnameMap transferred to outer UIEnhancer", cMap.textFont["default"],
+             uiOptions.pageEnhancer.options.classnameMap.textFont["default"]);
+        jqUnit.assertEquals("classnameMap transferred to inner UIEnhancer", cMap.textFont["default"],
+             uiOptions.iframeRenderer.iframeEnhancer.options.classnameMap.textFont["default"]);
+    };
 
     fluid.defaults("fluid.tests.mungingIntegrationTester", {
         gradeNames: ["fluid.test.testCaseHolder", "autoInit"],
