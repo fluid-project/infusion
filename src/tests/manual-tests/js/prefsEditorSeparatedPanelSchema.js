37c666cb48ae723d163df24a6c74b39ecd3e52fe
Justin Obara <obarajustin@gmail.com>
2013-10-09 08:53:23 -0400

    FLUID-5161: separated panel w/ shema manual test
    Added the manual test for the separated panel prefs editor using the schema in preparation for replacing the current framework demo with a UI Options demo.

---
+++ src/tests/manual-tests/js/prefsEditorSeparatedPanelSchema.js
@@ -0,0 +1,38 @@
+/*
+Copyright 2013 OCAD University
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
+*/
+
+// Declare dependencies
+/*global demo:true, fluid, jQuery, window*/
+
+// JSLint options
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+var demo = demo || {};
+(function ($, fluid) {
+
+    /**
+     * Initialize Preference Editor. This version of Preferences Editor uses the
+     * page itself as a live preview.
+     *
+     * Makes use of a schema to configure Preferences Editor
+     */
+    demo.initPrefsEditor = function (container) {
+        var builder = fluid.prefs.builder({
+            gradeNames: ["fluid.prefs.auxSchema.starter"],
+            auxiliarySchema: {
+                "templatePrefix": "../../../framework/preferences/html/",
+                "messagePrefix": "../../../framework/preferences/messages/"
+            }
+        });
+        return fluid.invokeGlobalFunction(builder.options.assembledPrefsEditorGrade, [container]);
+    };
+
+})(jQuery, fluid);
