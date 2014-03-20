3c797337f2bcf0783fddcb55733e83c3ea01492d
jhung <jhung@ocadu.ca>
2014-03-20 10:54:50 -0400

    FLUID-5291: Renamed "demoMenu" classnames to "overviewPanel". Todo: rename files and directory.

--- src/components/demoMenu/js/DemoMenu.js
+++ src/components/demoMenu/js/DemoMenu.js
@@ -1,16 +1,16 @@
 var fluid_1_5 = fluid_1_5 || {};
 
 (function ($, fluid) {
-    fluid.defaults("fluid.demoMenu", {
+    fluid.defaults("fluid.overviewPanel", {
         gradeNames: ["fluid.rendererComponent", "fluid.modelComponent", "autoInit"],
         resources: {
             template: {
-                href: "../html/demoMenuTemplate.html"
+                href: "../html/overviewPanelTemplate.html"
             }
         },
         listeners: {
             "onCreate.showTemplate": {
-                "funcName": "fluid.demoMenu.showTemplate"
+                "funcName": "fluid.overviewPanel.showTemplate"
             },
             "afterRender.registerToggleListener": {
                 "this": "{that}.dom.toggleControl",
@@ -56,44 +56,44 @@ var fluid_1_5 = fluid_1_5 || {};
         },
         invokers: {
             setVisibility: {
-                funcName: "fluid.demoMenu.setVisibility",
+                funcName: "fluid.overviewPanel.setVisibility",
                 args: ["{that}", "{that}.model.showMenu"],
                 dynamic: true
             },
             toggleMenu: {
-                funcName: "fluid.demoMenu.toggleMenu",
+                funcName: "fluid.overviewPanel.toggleMenu",
                 args: ["{that}", "{that}.model.showMenu"],
                 dynamic: true
             },
             closeMenu: {
-                funcName: "fluid.demoMenu.closeMenu",
+                funcName: "fluid.overviewPanel.closeMenu",
                 args: "{that}"
             }
         },
         selectors: {
-            demoMenuRoot: ".flc-demoMenu-root",
-            toggleControl: ".flc-demoMenu-toggleControl",
-            titleBegin: ".flc-demoMenu-title-begin",
-            titleLinkText: ".flc-demoMenu-title-linkText",
-            titleEnd: ".flc-demoMenu-title-end",
-            componentName: ".flc-demoMenu-componentName",
-            componentVersion: ".flc-demoMenu-componentVersion",
-            description: ".flc-demoMenu-description",
-            instructionsHeading: ".flc-demoMenu-instructionsHeading",
-            instructions: ".flc-demoMenu-instructions",
-            codeLink: ".flc-demoMenu-codeLink",
-            codeLinkText: ".flc-demoMenu-codeLinkText",
-            apiLink: ".flc-demoMenu-apiLink",
-            apiLinkText: ".flc-demoMenu-apiLinkText",
-            designLink: ".flc-demoMenu-designLink",
-            designLinkText: ".flc-demoMenu-designLinkText",
-            feedbackText: ".flc-demoMenu-feedbackText",
-            feedbackLink: ".flc-demoMenu-feedbackLink",
-            feedbackLinkText: ".flc-demoMenu-feedbackLinkText",
-            closeControl: ".flc-demoMenu-closeControl",
-            closeText: ".flc-demoMenu-closeText"
+            overviewPanelRoot: ".flc-overviewPanel-root",
+            toggleControl: ".flc-overviewPanel-toggleControl",
+            titleBegin: ".flc-overviewPanel-title-begin",
+            titleLinkText: ".flc-overviewPanel-title-linkText",
+            titleEnd: ".flc-overviewPanel-title-end",
+            componentName: ".flc-overviewPanel-componentName",
+            componentVersion: ".flc-overviewPanel-componentVersion",
+            description: ".flc-overviewPanel-description",
+            instructionsHeading: ".flc-overviewPanel-instructionsHeading",
+            instructions: ".flc-overviewPanel-instructions",
+            codeLink: ".flc-overviewPanel-codeLink",
+            codeLinkText: ".flc-overviewPanel-codeLinkText",
+            apiLink: ".flc-overviewPanel-apiLink",
+            apiLinkText: ".flc-overviewPanel-apiLinkText",
+            designLink: ".flc-overviewPanel-designLink",
+            designLinkText: ".flc-overviewPanel-designLinkText",
+            feedbackText: ".flc-overviewPanel-feedbackText",
+            feedbackLink: ".flc-overviewPanel-feedbackLink",
+            feedbackLinkText: ".flc-overviewPanel-feedbackLinkText",
+            closeControl: ".flc-overviewPanel-closeControl",
+            closeText: ".flc-overviewPanel-closeText"
         },
-        selectorsToIgnore: ["demoMenuRoot", "toggleControl", "codeLink", "apiLink", "designLink", "feedbackLink", "closeControl"],
+        selectorsToIgnore: ["overviewPanelRoot", "toggleControl", "codeLink", "apiLink", "designLink", "feedbackLink", "closeControl"],
         protoTree: {
             titleBegin: {messagekey: "titleBegin"},
             titleLinkText: {messagekey: "titleLinkText"},
@@ -134,21 +134,21 @@ var fluid_1_5 = fluid_1_5 || {};
         }
     });
 
-    fluid.demoMenu.showTemplate = function (that) {
+    fluid.overviewPanel.showTemplate = function (that) {
         fluid.fetchResources(that.options.resources, function () {
             that.refreshView();
         });
     };
 
-    fluid.demoMenu.setVisibility = function (that, value) {
-        that.locate("demoMenuRoot").toggleClass("fl-demoMenu-hidden", !value);
+    fluid.overviewPanel.setVisibility = function (that, value) {
+        that.locate("overviewPanelRoot").toggleClass("fl-overviewPanel-hidden", !value);
     };
 
-    fluid.demoMenu.toggleMenu = function (that, value) {
+    fluid.overviewPanel.toggleMenu = function (that, value) {
         that.applier.requestChange("showMenu", !value);
     };
 
-    fluid.demoMenu.closeMenu = function (that) {
+    fluid.overviewPanel.closeMenu = function (that) {
         that.applier.requestChange("showMenu", false);
     };
 
