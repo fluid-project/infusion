8bf62359b1d5f46c62feaa2a2bf4ea19417c0df4
kaspermarkus <kasper@markus.dk>
2014-03-19 18:10:10 -0700

    FLUID-5251: Updated code to base condition not only on strictly true/false boolean values, but also anything that javascript interpretes as boolean -eg. String evaluates to true, undefined evaluates to false, etc

--- src/framework/core/js/ModelTransformationTransforms.js
+++ src/framework/core/js/ModelTransformationTransforms.js
@@ -14,7 +14,7 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
 // Declare dependencies
 /*global fluid:true, fluid_1_5:true, jQuery*/
 
-// JSLint options 
+// JSLint options
 /*jslint white: true, elsecatch: true, jslintok: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
 
 var fluid_1_5 = fluid_1_5 || {};
@@ -29,14 +29,14 @@ var fluid = fluid || fluid_1_5;
     /**********************************
      * Standard transformer functions *
      **********************************/
-        
-    fluid.defaults("fluid.transforms.value", { 
+
+    fluid.defaults("fluid.transforms.value", {
         gradeNames: "fluid.standardTransformFunction",
         invertConfiguration: "fluid.transforms.value.invert"
     });
 
     fluid.transforms.value = fluid.identity;
-    
+
     fluid.transforms.value.invert = function (transformSpec, transform) {
         var togo = fluid.copy(transformSpec);
         // TODO: this will not behave correctly in the face of compound "value" which contains
@@ -47,41 +47,41 @@ var fluid = fluid || fluid_1_5;
     };
 
 
-    fluid.defaults("fluid.transforms.literalValue", { 
+    fluid.defaults("fluid.transforms.literalValue", {
         gradeNames: "fluid.standardOutputTransformFunction"
     });
 
     fluid.transforms.literalValue = function (transformSpec) {
-        return transformSpec.value;  
+        return transformSpec.value;
     };
-    
 
-    fluid.defaults("fluid.transforms.arrayValue", { 
+
+    fluid.defaults("fluid.transforms.arrayValue", {
         gradeNames: "fluid.standardTransformFunction"
     });
-    
+
     fluid.transforms.arrayValue = fluid.makeArray;
 
 
-    fluid.defaults("fluid.transforms.count", { 
+    fluid.defaults("fluid.transforms.count", {
         gradeNames: "fluid.standardTransformFunction"
     });
-    
+
     fluid.transforms.count = function (value) {
         return fluid.makeArray(value).length;
     };
-    
-    
-    fluid.defaults("fluid.transforms.round", { 
+
+
+    fluid.defaults("fluid.transforms.round", {
         gradeNames: "fluid.standardTransformFunction"
     });
-    
+
     fluid.transforms.round = function (value) {
         return Math.round(value);
     };
-    
 
-    fluid.defaults("fluid.transforms.delete", { 
+
+    fluid.defaults("fluid.transforms.delete", {
         gradeNames: "fluid.transformFunction"
     });
 
@@ -90,30 +90,30 @@ var fluid = fluid || fluid_1_5;
         transform.applier.requestChange(outputPath, null, "DELETE");
     };
 
-    
-    fluid.defaults("fluid.transforms.firstValue", { 
+
+    fluid.defaults("fluid.transforms.firstValue", {
         gradeNames: "fluid.transformFunction"
     });
-    
+
     fluid.transforms.firstValue = function (transformSpec, transform) {
         if (!transformSpec.values || !transformSpec.values.length) {
             fluid.fail("firstValue transformer requires an array of values at path named \"values\", supplied", transformSpec);
         }
         for (var i = 0; i < transformSpec.values.length; i++) {
             var value = transformSpec.values[i];
-            // TODO: problem here - all of these transforms will have their side-effects (setValue) even if only one is chosen 
+            // TODO: problem here - all of these transforms will have their side-effects (setValue) even if only one is chosen
             var expanded = transform.expand(value);
             if (expanded !== undefined) {
                 return expanded;
             }
         }
     };
-    
+
     fluid.defaults("fluid.transforms.linearScale", {
         gradeNames: [ "fluid.multiInputTransformFunction", "fluid.standardOutputTransformFunction", "fluid.lens" ],
         invertConfiguration: "fluid.transforms.linearScale.invert",
         inputVariables: {
-            value: null, 
+            value: null,
             factor: 1,
             offset: 0
         }
@@ -146,7 +146,7 @@ var fluid = fluid || fluid_1_5;
         return togo;
     };
 
-    fluid.defaults("fluid.transforms.binaryOp", { 
+    fluid.defaults("fluid.transforms.binaryOp", {
         gradeNames: [ "fluid.multiInputTransformFunction", "fluid.standardOutputTransformFunction" ],
         inputVariables: {
             left: null,
@@ -177,7 +177,7 @@ var fluid = fluid || fluid_1_5;
         var operator = fluid.model.transform.getValue(undefined, transformSpec.operator, transform);
 
         var fun = fluid.transforms.binaryLookup[operator];
-        return (fun === undefined || left === undefined || right === undefined) ? 
+        return (fun === undefined || left === undefined || right === undefined) ?
             undefined : fun(left, right);
     };
 
@@ -196,7 +196,7 @@ var fluid = fluid || fluid_1_5;
             return undefined;
         }
 
-        return inputs[condition]();
+        return inputs[!!condition]();
     };
 
 
@@ -215,7 +215,7 @@ var fluid = fluid || fluid_1_5;
         if (o.length === 1) {
             return 0;
         }
-        var matchPower = []; 
+        var matchPower = [];
         for (var i = 0; i < o.length; ++i) {
             var option = o[i];
             var value = fluid.firstDefined(fluid.model.transform.getValue(option.inputPath, undefined, transform),
@@ -224,7 +224,7 @@ var fluid = fluid || fluid_1_5;
             matchPower[i] = {index: i, matchCount: matchCount};
         }
         matchPower.sort(fluid.model.transform.compareMatches);
-        return matchPower[0].matchCount === matchPower[1].matchCount ? -1 : matchPower[0].index; 
+        return matchPower[0].matchCount === matchPower[1].matchCount ? -1 : matchPower[0].index;
     };
 
     fluid.transforms.valueMapper = function (transformSpec, transform) {
@@ -232,15 +232,15 @@ var fluid = fluid || fluid_1_5;
             fluid.fail("demultiplexValue requires a list or hash of options at path named \"options\", supplied ", transformSpec);
         }
         var value = fluid.model.transform.getValue(transformSpec.inputPath, undefined, transform);
-        var deref = fluid.isArrayable(transformSpec.options) ? // long form with list of records    
+        var deref = fluid.isArrayable(transformSpec.options) ? // long form with list of records
             function (testVal) {
                 var index = fluid.model.transform.matchValueMapperFull(testVal, transformSpec, transform);
                 return index === -1 ? null : transformSpec.options[index];
-            } : 
+            } :
             function (testVal) {
                 return transformSpec.options[testVal];
             };
-      
+
         var indexed = deref(value);
         if (!indexed) {
             // if no branch matches, try again using this value - WARNING, this seriously
@@ -272,9 +272,9 @@ var fluid = fluid || fluid_1_5;
             outputValue = undefined;
         }
         transform.outputPrefixOp.pop();
-        return outputValue; 
+        return outputValue;
     };
-    
+
     fluid.transforms.valueMapper.invert = function (transformSpec, transform) {
         var options = [];
         var togo = {
@@ -320,7 +320,7 @@ var fluid = fluid || fluid_1_5;
         });
         return togo;
     };
-    
+
     fluid.transforms.valueMapper.collect = function (transformSpec, transform) {
         var togo = [];
         fluid.model.transform.accumulateInputPath(transformSpec.inputPath, transform, togo);
@@ -331,13 +331,13 @@ var fluid = fluid || fluid_1_5;
     };
 
     /* -------- arrayToSetMembership and setMembershipToArray ---------------- */
-    
-    fluid.defaults("fluid.transforms.arrayToSetMembership", { 
+
+    fluid.defaults("fluid.transforms.arrayToSetMembership", {
         gradeNames: ["fluid.standardInputTransformFunction", "fluid.lens"],
         invertConfiguration: "fluid.transforms.arrayToSetMembership.invert"
     });
 
- 
+
     fluid.transforms.arrayToSetMembership = function (value, transformSpec, transform) {
         var options = transformSpec.options;
 
@@ -351,7 +351,7 @@ var fluid = fluid || fluid_1_5;
         if (transformSpec.presentValue === undefined) {
             transformSpec.presentValue = true;
         }
-        
+
         if (transformSpec.missingValue === undefined) {
             transformSpec.missingValue = false;
         }
@@ -378,7 +378,7 @@ var fluid = fluid || fluid_1_5;
         return togo;
     };
 
-    fluid.defaults("fluid.transforms.setMembershipToArray", { 
+    fluid.defaults("fluid.transforms.setMembershipToArray", {
         gradeNames: ["fluid.standardOutputTransformFunction"]
     });
 
@@ -392,7 +392,7 @@ var fluid = fluid || fluid_1_5;
         if (transformSpec.presentValue === undefined) {
             transformSpec.presentValue = true;
         }
-        
+
         if (transformSpec.missingValue === undefined) {
             transformSpec.missingValue = false;
         }
@@ -405,10 +405,10 @@ var fluid = fluid || fluid_1_5;
             }
         });
         return outputArr;
-    };    
+    };
 
     /* -------- objectToArray and arrayToObject -------------------- */
-    
+
     /**
      * Transforms the given array to an object.
      * Uses the transformSpec.options.key values from each object within the array as new keys.
@@ -441,16 +441,16 @@ var fluid = fluid || fluid_1_5;
             if (operation === "push") {
                 pathOp.push(paths[i]);
             } else {
-                pathOp.pop();   
+                pathOp.pop();
             }
         }
     };
-    
+
     fluid.model.transform.expandInnerValues = function (inputPath, outputPath, transform, innerValues) {
         var inputPrefixOp = transform.inputPrefixOp;
         var outputPrefixOp = transform.outputPrefixOp;
         var apply = fluid.model.transform.applyPaths;
-        
+
         apply("push", inputPrefixOp, inputPath);
         apply("push", outputPrefixOp, outputPath);
         var expanded = {};
@@ -464,7 +464,7 @@ var fluid = fluid || fluid_1_5;
         });
         apply("pop", outputPrefixOp, outputPath);
         apply("pop", inputPrefixOp, inputPath);
-        
+
         return expanded;
     };
 
@@ -485,7 +485,7 @@ var fluid = fluid || fluid_1_5;
         var pivot = transformSpec.key;
 
         fluid.each(arr, function (v, k) {
-            // check that we have a pivot entry in the object and it's a valid type:            
+            // check that we have a pivot entry in the object and it's a valid type:
             var newKey = v[pivot];
             var keyType = typeof(newKey);
             if (keyType !== "string" && keyType !== "boolean" && keyType !== "number") {
@@ -496,7 +496,7 @@ var fluid = fluid || fluid_1_5;
             delete content[pivot];
             // fix sub Arrays if needed:
             if (transformSpec.innerValue) {
-                content = fluid.model.transform.expandInnerValues([transform.inputPrefix, transformSpec.inputPath, k.toString()], 
+                content = fluid.model.transform.expandInnerValues([transform.inputPrefix, transformSpec.inputPath, k.toString()],
                     [newKey], transform, transformSpec.innerValue);
             }
             newHash[newKey] = content;
@@ -517,11 +517,11 @@ var fluid = fluid || fluid_1_5;
             var innerValue = togo.innerValue;
             for (var i = 0; i < innerValue.length; ++i) {
                 innerValue[i] = fluid.model.transform.invertConfiguration(innerValue[i]);
-            }            
+            }
         }
         return togo;
     };
-    
+
 
     fluid.defaults("fluid.transforms.objectToArray", {
         gradeNames: "fluid.standardTransformFunction"
@@ -535,7 +535,7 @@ var fluid = fluid || fluid_1_5;
         if (transformSpec.key === undefined) {
             fluid.fail("objectToArray requires a 'key' option.", transformSpec);
         }
-        
+
         var newArray = [];
         var pivot = transformSpec.key;
 
@@ -543,7 +543,7 @@ var fluid = fluid || fluid_1_5;
             var content = {};
             content[pivot] = k;
             if (transformSpec.innerValue) {
-                v = fluid.model.transform.expandInnerValues([transformSpec.inputPath, k], [transformSpec.outputPath, newArray.length.toString()], 
+                v = fluid.model.transform.expandInnerValues([transformSpec.inputPath, k], [transformSpec.outputPath, newArray.length.toString()],
                     transform, transformSpec.innerValue);
             }
             $.extend(true, content, v);
@@ -551,11 +551,11 @@ var fluid = fluid || fluid_1_5;
         });
         return newArray;
     };
-    
+
     fluid.defaults("fluid.transforms.limitRange", {
-        gradeNames: "fluid.standardTransformFunction"  
+        gradeNames: "fluid.standardTransformFunction"
     });
-    
+
     fluid.transforms.limitRange = function (value, transformSpec, transform) {
         var min = transformSpec.min;
         if (min !== undefined) {
@@ -563,7 +563,7 @@ var fluid = fluid || fluid_1_5;
             min += excludeMin;
             if (value < min) {
                 value = min;
-            }  
+            }
         }
         var max = transformSpec.max;
         if (max !== undefined) {
@@ -571,18 +571,18 @@ var fluid = fluid || fluid_1_5;
             max -= excludeMax;
             if (value > max) {
                 value = max;
-            }  
+            }
         }
         return value;
     };
-    
+
     fluid.defaults("fluid.transforms.free", {
-        gradeNames: "fluid.transformFunction"  
+        gradeNames: "fluid.transformFunction"
     });
-    
+
     fluid.transforms.free = function (transformSpec, transform) {
         var args = fluid.makeArray(transformSpec.args);
         return fluid.invokeGlobalFunction(transformSpec.func, args);
     };
-    
+
 })(jQuery, fluid_1_5);
