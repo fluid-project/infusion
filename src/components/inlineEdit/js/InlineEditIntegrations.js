d165fb7d9eb71b7939f2144c306bc4e06f664a61
Justin Obara <obarajustin@gmail.com>
2014-03-21 14:33:40 -0400

    FLUID-52844: Modified globals declaration

--- src/components/inlineEdit/js/InlineEditIntegrations.js
+++ src/components/inlineEdit/js/InlineEditIntegrations.js
@@ -12,7 +12,7 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
 */
 
 // Declare dependencies
-/* global fluid, fluid_1_5:true, CKEDITOR, tinyMCE */
+/* global fluid_1_5:true, CKEDITOR, tinyMCE */
 
 // JSHint options
 /* jshint bitwise: true, camelcase: false, curly: true, eqeqeq: true, es3: true, forin: true, freeze: true, immed: true, indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonbsp: true, nonew: true, plusplus: false, quotmark: double, undef: true, unused: true, strict: true, trailing: true, maxerr: 1000, browser: true, jquery: true, worker: true */
