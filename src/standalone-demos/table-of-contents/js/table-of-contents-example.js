ce1669fb6f84f2cd92e5cca05a0f0c9fad96bcd2
Justin Obara <obarajustin@gmail.com>
2014-03-28 12:23:19 -0400

    FLUID-5284: Linted ToC standalone demo

--- src/standalone-demos/table-of-contents/js/table-of-contents-example.js
+++ src/standalone-demos/table-of-contents/js/table-of-contents-example.js
@@ -10,14 +10,13 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
 */
 
 // Declare dependencies
-/*global demo:true, fluid, jQuery*/
-
-// JSLint options
-/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+/*global fluid */
 
 var demo = demo || {};
 
 (function ($, fluid) {
+    "use strict";
+
     demo.initTableOfContent = function () {
         fluid.staticEnvironment.demo = fluid.typeTag("fluid.tableOfContentsDemo");
         fluid.demands("fluid.tableOfContents.levels", ["fluid.tableOfContents", "fluid.tableOfContentsDemo"], {
