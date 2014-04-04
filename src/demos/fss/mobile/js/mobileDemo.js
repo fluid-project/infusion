fb3e75b985431b5bb45912a4378905bc2c6e67ec
Justin Obara <obarajustin@gmail.com>
2014-04-04 13:12:26 -0400

    FLUID-5284: clean up globals comment

--- src/demos/fss/mobile/js/mobileDemo.js
+++ src/demos/fss/mobile/js/mobileDemo.js
@@ -10,7 +10,7 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
 */
 
 // Declare dependencies
-/*global fluid */
+/* global fluid */
 
 (function ($, fluid) {
     "use strict";
