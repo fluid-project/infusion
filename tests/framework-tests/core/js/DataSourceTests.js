80c1e6ea891be2f3bddebed587fb12064c94dde2
Justin Obara <obarajustin@gmail.com>
2014-11-07 10:40:11 -0500

    FLUID-5542: Interim commit before adding promises
    Includes the amalgamation of the requestQueue and the queuedDataSource into a single grade. Tests are also forming.

--- tests/framework-tests/core/js/DataSourceTests.js
+++ tests/framework-tests/core/js/DataSourceTests.js
@@ -36,18 +36,183 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
         };
 
         var expected = {
-            singleLevel: "<object a:|string A|b:|string B|c:<array 0:|string D|1:|string E|2:|string F|>g:|number 5|>",
-            nested: "<object a:|string A|b:<object c:|string C|d:<array 0:|string D|1:|string E|>>f:<array 0:|string F|1:|string G|>h:|number 6|>"
+            singleLevel: "<object a:|\"A\"|b:|\"B\"|c:<array 0:|\"D\"|1:|\"E\"|2:|\"F\"|>g:|5|>",
+            nested: "<object a:|\"A\"|b:<object c:|\"C\"|d:<array 0:|\"D\"|1:|\"E\"|>>f:<array 0:|\"F\"|1:|\"G\"|>h:|6|>"
         }
 
         jqUnit.assertEquals("The single level object should be converted properly", expected.singleLevel, fluid.toHashKey(singleLevel));
         jqUnit.assertEquals("The multi-level object should be converted properly", expected.nested, fluid.toHashKey(nested));
     });
 
-    fluid.defaults("fluid.tests.requestQueue", {
-        gradeNames: ["fluid.requestQueue", "autoInit"],
+    // fluid.defaults("fluid.tests.requestQueue", {
+    //     gradeNames: ["fluid.requestQueue", "autoInit"],
+    //     members: {
+    //         // Each record should contain a list of the request ID that was sent to the respective events.
+    //         fireRecord: {
+    //             enqueued: [],
+    //             dropped: [],
+    //             onRequestStart: [],
+    //             afterRequestComplete: []
+    //         }
+    //     },
+    //     listeners: {
+    //         "enqueued": {
+    //             func: "{that}.record",
+    //             args: ["enqueued", "{arguments}.0"]
+    //         },
+    //         "dropped": {
+    //             func: "{that}.record",
+    //             args: ["dropped", "{arguments}.0"]
+    //         },
+    //         "onRequestStart": {
+    //             func: "{that}.record",
+    //             args: ["onRequestStart", "{arguments}.0"]
+    //         },
+    //         "afterRequestComplete": {
+    //             func: "{that}.record",
+    //             args: ["afterRequestComplete", "{arguments}.0"]
+    //         },
+    //     },
+    //     invokers: {
+    //         record: {
+    //             funcName: "fluid.tests.requestQueue.record",
+    //             args: ["{that}", "{arguments}.0", "{arguments}.1"]
+    //         },
+    //         requestFn: {
+    //             funcName: "fluid.tests.requestQueue.requestFn",
+    //             args: ["{that}", "{arguments}.0", "{arguments}.1", "{arguments}.2"]
+    //         }
+    //     }
+    // });
+    //
+    // fluid.tests.requestQueue.record = function (that, recordName, request) {
+    //     var reqID = request.model.ID;
+    //     that.fireRecord[recordName].push(reqID);
+    // };
+    //
+    // fluid.tests.requestQueue.requestFn = function (that, directModel, model, callback) {
+    //     // use a setTimeout to simulate an asynchronous request
+    //     setTimeout(callback, 100);
+    // };
+    //
+    // fluid.tests.verifyRequestQueue = function (that, numRequests, expectedRecord, cleanup) {
+    //     cleanup = cleanup || jqUnit.start;
+    //     var previousCallNum = 0;
+    //
+    //     var assertRequest = function (callNum) {
+    //         return function () {
+    //             jqUnit.assertTrue("Set call " + callNum + " should be triggered in the correct order", callNum > previousCallNum);
+    //             previousCallNum = callNum;
+    //             if (callNum === numRequests) {
+    //                 jqUnit.assertDeepEq("The event record should have been updated appropriately.", expectedRecord, that.fireRecord);
+    //                 cleanup();
+    //             }
+    //         };
+    //     };
+    //
+    //     var triggerQueue = function (currentCycle) {
+    //         that.enqueue({
+    //             method: that.requestFn,
+    //             directModel: {path: ""},
+    //             model: {ID: currentCycle},
+    //             callback: assertRequest(currentCycle)
+    //         });
+    //     };
+    //
+    //     for (var i = 1; i < numRequests; i++) {
+    //         triggerQueue(i);
+    //     }
+    //
+    //     // add a final request that occurs after the first should have completed.
+    //     setTimeout(function () {
+    //         triggerQueue(numRequests);
+    //     }, 200);
+    // };
+    //
+    // fluid.defaults("fluid.tests.requestQueue.concurrencyLimited", {
+    //     gradeNames: ["fluid.requestQueue.concurrencyLimited", "fluid.tests.requestQueue", "autoInit"]
+    // });
+    //
+    // jqUnit.asyncTest("Request Queue: Concurrency Limited", function () {
+    //     var request
+    //
+    //     fluid.tests.requestQueue.concurrencyLimited({
+    //         listeners: {
+    //             "onCreate.verifyDebounceRequestQueue": {
+    //                 listener: "fluid.tests.verifyRequestQueue",
+    //                 args: ["{that}", 4, {
+    //                     enqueued: [1, 2, 3, 4],
+    //                     dropped: [2],
+    //                     onRequestStart: [1, 3, 4],
+    //                     afterRequestComplete: [1, 3, 4]
+    //                 }]
+    //             }
+    //         }
+    //     });
+    // });
+    //
+    // fluid.defaults("fluid.tests.queuedDataSource", {
+    //     gradeNames: ["fluid.queuedDataSource", "autoInit"],
+    //     components: {
+    //         wrappedDataSource: {
+    //             options: {
+    //                 invokers: {
+    //                     "get": "fluid.tests.queuedDataSource.request",
+    //                     "set": "fluid.tests.queuedDataSource.request",
+    //                     "delete": "fluid.tests.queuedDataSource.request"
+    //                 }
+    //             }
+    //         }
+    //     }
+    // });
+    //
+    // fluid.tests.queuedDataSource.request = function () {
+    //     var callback = arguments[arguments.length -1];
+    //     var directModel = arguments[0];
+    //     callback(directModel);
+    // };
+    //
+    // fluid.tests.queuedDataSource.assertRequest = function (directModel) {
+    //     jqUnit.assert("The " + directModel.path + " request should have been triggerd");
+    // };
+    //
+    // fluid.tests.queuedDataSource.assertRequestsQueue = function (that, expectedReadQueues, expectedWriteQueues) {
+    //     var actualReadQueues = fluid.keys(that.requests.read).sort();
+    //     var actualWriteQueues = fluid.keys(that.requests.write).sort();
+    //
+    //     jqUnit.assertDeepEq("The read queue should be populated correctly", expectedReadQueues.sort(), actualReadQueues);
+    //     jqUnit.assertDeepEq("The write queue should be populated correctly", expectedWriteQueues.sort(), actualWriteQueues);
+    // };
+    //
+    // jqUnit.test("Queued DataSource", function () {
+    //     jqUnit.expect(5);
+    //     // The expected queues contain a list of the keys for which queues are bound to in the requests object.
+    //     var expectedReadQueues = ["<object path:|string get|>"];
+    //     var expectedWriteQueues = ["<object path:|string set|>", "<object path:|string delete|>"];
+    //     fluid.tests.queuedDataSource({
+    //         listeners: {
+    //             "onCreate": [{
+    //                 listener: "{that}.get",
+    //                 args: [{path: "get"}, fluid.tests.queuedDataSource.assertRequest]
+    //             }, {
+    //                 listener: "{that}.set",
+    //                 args: [{path: "set"}, {key: "value"}, fluid.tests.queuedDataSource.assertRequest]
+    //             }, {
+    //                 listener: "{that}.delete",
+    //                 args: [{path: "delete"}, fluid.tests.queuedDataSource.assertRequest]
+    //             }, {
+    //                 listener: "fluid.tests.queuedDataSource.assertRequestsQueue",
+    //                 args: ["{that}", expectedReadQueues, expectedWriteQueues]
+    //             }]
+    //         }
+    //     });
+    // });
+
+    fluid.defaults("fluid.tests.queuedDataSource", {
+        gradeNames: ["fluid.queuedDataSource", "autoInit"],
         members: {
-            // Each record should contain a list of the request ID that was sent to the respective events.
+            // Each record should contain a list of the directModels from the requests
+            // that were sent to the respective events.
             fireRecord: {
                 enqueued: [],
                 dropped: [],
@@ -56,153 +221,75 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
             }
         },
         listeners: {
-            "enqueued": {
-                func: "{that}.record",
-                args: ["enqueued", "{arguments}.0"]
+            "enqueued.test": {
+                listener: "fluid.tests.queuedDataSource.record",
+                args: ["{that}.fireRecord.enqueued", "{arguments}.0"]
             },
-            "dropped": {
-                func: "{that}.record",
-                args: ["dropped", "{arguments}.0"]
+            "dropped.test": {
+                listener: "fluid.tests.queuedDataSource.record",
+                args: ["{that}.fireRecord.dropped", "{arguments}.0"]
             },
-            "onRequestStart": {
-                func: "{that}.record",
-                args: ["onRequestStart", "{arguments}.0"]
+            "onRequestStart.test": {
+                listener: "fluid.tests.queuedDataSource.record",
+                args: ["{that}.fireRecord.onRequestStart", "{arguments}.0"]
             },
-            "afterRequestComplete": {
-                func: "{that}.record",
-                args: ["afterRequestComplete", "{arguments}.0"]
-            },
+            "afterRequestComplete.test": {
+                listener: "fluid.tests.queuedDataSource.record",
+                args: ["{that}.fireRecord.afterRequestComplete", "{arguments}.0"]
+            }
         },
-        invokers: {
-            record: {
-                funcName: "fluid.tests.requestQueue.record",
-                args: ["{that}", "{arguments}.0", "{arguments}.1"]
-            },
-            requestFn: {
-                funcName: "fluid.tests.requestQueue.requestFn",
-                args: ["{that}", "{arguments}.0", "{arguments}.1", "{arguments}.2"]
-            }
-        }
-    });
-
-    fluid.tests.requestQueue.record = function (that, recordName, request) {
-        var reqID = request.model.ID;
-        that.fireRecord[recordName].push(reqID);
-    };
-
-    fluid.tests.requestQueue.requestFn = function (that, directModel, model, callback) {
-        // use a setTimeout to simulate an asynchronous request
-        setTimeout(callback, 100);
-    };
-
-    fluid.tests.verifyRequestQueue = function (that, numRequests, expectedRecord, cleanup) {
-        cleanup = cleanup || jqUnit.start;
-        var previousCallNum = 0;
-
-        var assertRequest = function (callNum) {
-            return function () {
-                jqUnit.assertTrue("Set call " + callNum + " should be triggered in the correct order", callNum > previousCallNum);
-                previousCallNum = callNum;
-                if (callNum === numRequests) {
-                    jqUnit.assertDeepEq("The event record should have been updated appropriately.", expectedRecord, that.fireRecord);
-                    cleanup();
-                }
-            };
-        };
-
-        var triggerQueue = function (currentCycle) {
-            that.enqueue({
-                method: that.requestFn,
-                directModel: {path: ""},
-                model: {ID: currentCycle},
-                callback: assertRequest(currentCycle)
-            });
-        };
-
-        for (var i = 1; i < numRequests; i++) {
-            triggerQueue(i);
-        }
-
-        // add a final request that occurs after the first should have completed.
-        setTimeout(function () {
-            triggerQueue(numRequests);
-        }, 200);
-    };
-
-    fluid.defaults("fluid.tests.requestQueue.concurrencyLimited", {
-        gradeNames: ["fluid.requestQueue.concurrencyLimited", "fluid.tests.requestQueue", "autoInit"]
-    });
-
-    jqUnit.asyncTest("Request Queue: Concurrency Limited", function () {
-        var request
-
-        fluid.tests.requestQueue.concurrencyLimited({
-            listeners: {
-                "onCreate.verifyDebounceRequestQueue": {
-                    listener: "fluid.tests.verifyRequestQueue",
-                    args: ["{that}", 4, {
-                        enqueued: [1, 2, 3, 4],
-                        dropped: [2],
-                        onRequestStart: [1, 3, 4],
-                        afterRequestComplete: [1, 3, 4]
-                    }]
-                }
-            }
-        });
-    });
-
-    fluid.defaults("fluid.tests.queuedDataSource", {
-        gradeNames: ["fluid.queuedDataSource", "autoInit"],
         components: {
             wrappedDataSource: {
                 options: {
                     invokers: {
-                        "get": "fluid.tests.queuedDataSource.request",
-                        "set": "fluid.tests.queuedDataSource.request",
-                        "delete": "fluid.tests.queuedDataSource.request"
+                        "get": "fluid.tests.queuedDataSource.requestFn",
+                        "set": "fluid.tests.queuedDataSource.requestSetFn",
+                        "delete": "fluid.tests.queuedDataSource.requestFn"
                     }
                 }
             }
         }
     });
 
-    fluid.tests.queuedDataSource.request = function () {
-        var callback = arguments[arguments.length -1];
-        var directModel = arguments[0];
+    fluid.tests.queuedDataSource.record = function (record, request) {
+        record.push(request.directModel);
+    };
+
+    fluid.tests.queuedDataSource.requestFn = function (directModel, callback) {
+        // use a setTimeout to simulate an asynchronous request
+        // setTimeout(callback, 100);
         callback(directModel);
     };
 
-    fluid.tests.queuedDataSource.assertRequest = function (directModel) {
-        jqUnit.assert("The " + directModel.path + " request should have been triggerd");
+    fluid.tests.queuedDataSource.requestSetFn = function (directModel, model, callback) {
+        // use a setTimeout to simulate an asynchronous request
+        // setTimeout(callback, 100);
+        callback(directModel, model);
     };
 
-    fluid.tests.queuedDataSource.assertRequestsQueue = function (that, expectedReadQueues, expectedWriteQueues) {
-        var actualReadQueues = fluid.keys(that.requests.read).sort();
-        var actualWriteQueues = fluid.keys(that.requests.write).sort();
-
-        jqUnit.assertDeepEq("The read queue should be populated correctly", expectedReadQueues.sort(), actualReadQueues);
-        jqUnit.assertDeepEq("The write queue should be populated correctly", expectedWriteQueues.sort(), actualWriteQueues);
+    fluid.tests.queuedDataSource.assertRequests = function (that, expectedRequests) {
+        jqUnit.assertDeepEq("The requests should have been processed through the queue corectly.", expectedRequests, that.fireRecord);
+        jqUnit.start();
     };
 
-    jqUnit.test("Queued DataSource", function () {
-        jqUnit.expect(5);
-        // The expected queues contain a list of the keys for which queues are bound to in the requests object.
-        var expectedReadQueues = ["<object path:|string get|>"];
-        var expectedWriteQueues = ["<object path:|string set|>", "<object path:|string delete|>"];
+    jqUnit.asyncTest("Queued DataSource", function () {
         fluid.tests.queuedDataSource({
             listeners: {
                 "onCreate": [{
                     listener: "{that}.get",
-                    args: [{path: "get"}, fluid.tests.queuedDataSource.assertRequest]
+                    args: [{path: "get"}, fluid.identity]
                 }, {
                     listener: "{that}.set",
-                    args: [{path: "set"}, {key: "value"}, fluid.tests.queuedDataSource.assertRequest]
+                    args: [{path: "set"}, {key: "value"}, fluid.identity]
                 }, {
                     listener: "{that}.delete",
-                    args: [{path: "delete"}, fluid.tests.queuedDataSource.assertRequest]
+                    args: [{path: "delete"}, fluid.identity]
                 }, {
-                    listener: "fluid.tests.queuedDataSource.assertRequestsQueue",
-                    args: ["{that}", expectedReadQueues, expectedWriteQueues]
+                //     listener: "fluid.tests.queuedDataSource.assertRequestsQueue",
+                //     args: ["{that}", expectedReadQueues, expectedWriteQueues]
+                // }, {
+                    listener: "fluid.tests.queuedDataSource.assertRequests",
+                    args: ["{that}", {}]
                 }]
             }
         });
