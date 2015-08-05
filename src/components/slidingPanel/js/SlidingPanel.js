3cf96f709c9f5a2fe881a49f3e309154972cf795
Kaye <kayexmao@gmail.com>
2015-08-05 17:38:19 -0400

    FLUID-5680: removed unneccesary msges

--- src/components/slidingPanel/js/SlidingPanel.js
+++ src/components/slidingPanel/js/SlidingPanel.js
@@ -25,10 +25,6 @@ var fluid_2_0 = fluid_2_0 || {};
             toggleButton: ".flc-slidingPanel-toggleButton",
             toggleButtonLabel: ".flc-slidingPanel-toggleButton"
         },
-        strings: {
-            showText: "show",
-            hideText: "hide"
-        },
         events: {
             onPanelHide: null,
             onPanelShow: null,
