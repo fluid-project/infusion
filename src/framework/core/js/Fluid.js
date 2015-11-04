3292dfc1a781d02c1edb05c337b40b7bf143a8c2
Antranig Basman <antranig.basman@colorado.edu>
2015-11-04 13:39:02 +0000

    FLUID-5807: Simplified implementation and browser tests

--- src/framework/core/js/Fluid.js
+++ src/framework/core/js/Fluid.js
@@ -175,9 +175,7 @@ var fluid = fluid || fluid_2_0_0;
         this.stack = togo.stack;
         return this;
     };
-    var ErrorInheritor = function () {};
-    ErrorInheritor.prototype = Error.prototype;
-    fluid.FluidError.prototype = new ErrorInheritor();
+    fluid.FluidError.prototype = Object.create(Error.prototype);
 
     // The framework's built-in "log" failure handler - this logs the supplied message as well as any framework activity in progress via fluid.log
     fluid.logFailure = function (args, activity) {
