f932be6f0a5e11d305bbcde2051f5f1e3eb36d54
Cindy Qi Li <cli@ocadu.ca>
2016-02-16 16:37:38 -0500

    FLUID-5864: Started to implement the export functionality.

--- src/framework/preferences/js/GpiiPanel.js
+++ src/framework/preferences/js/GpiiPanel.js
@@ -58,7 +58,8 @@ var fluid_2_0_0 = fluid_2_0_0 || {};
         },
         events: {
             onImport: null,
-            onExport: null
+            beforeExport: null,
+            afterExport: null
         },
         listeners: {
             "afterRender.bindImport": {
@@ -69,7 +70,13 @@ var fluid_2_0_0 = fluid_2_0_0 || {};
             "afterRender.bindExport": {
                 "this": "{that}.dom.exportButton",
                 method: "click",
-                args: ["{that}.events.onExport.fire"]
+                args: ["{that}.export"]
+            }
+        },
+        invokers: {
+            export: {
+                funcName: "fluid.prefs.panel.gpii.export",
+                args: ["{that}", "{prefsEditor}", "{arguments}.0"]
             }
         },
         components: {
@@ -95,5 +102,26 @@ var fluid_2_0_0 = fluid_2_0_0 || {};
             target: "{that fluid.tooltip}.options"
         }]
     });
+
+    fluid.prefs.panel.gpii.export = function (that, prefsEditor, event) {
+        // TODO:
+        // 1. get UIO preferences from GPII to compare
+        // 2. if the saved prefs is same as the local prefs or no saved preferences, do nothing.
+        // 3. if the saved prefs is different from the local prefs,
+        // (1) the local chang is different from the default prefs, show warning dialog
+        // (2) the local change is same as the default prefs, apply the gpii prefs.
+
+        that.events.beforeExport.fire(gpiiPrefs);
+        event.preventDefault();
+        var localPrefs = prefsEditor.model.preferences,
+            stats = {changes: 0, unchanged: 0, changeMap: {}},
+            gpiiPrefs = prefsEditor.model.preferences;  // retrieved GPII prefs
+        console.log("localPrefs", localPrefs, "gpiiPrefs", gpiiPrefs)
+
+        if (stats.changes > 0) {
+            // compare local prefs with default prefss
+        }
+        that.events.afterExport.fire(gpiiPrefs);
+    };
 
 })(jQuery, fluid_2_0_0);
