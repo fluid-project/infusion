be52360c48bb896ae8ff270b70119e0bb58394e4
Dinuka De Silva <dinuka.2013513@iit.ac.lk>
2016-03-18 09:58:53 +0530

    FLUID-5689: Updated unwanted modelListeners

--- src/components/pager/js/Pager.js
+++ src/components/pager/js/Pager.js
@@ -493,7 +493,7 @@ var fluid_2_0_0 = fluid_2_0_0 || {};
             }
         }],
         modelListeners: {
-            "{fluid.pager}.model": "{that}.events.onModelChange.fire({change}.value, {change}.oldValue, {that})"
+            "": "{that}.events.onModelChange.fire({change}.value, {change}.oldValue, {that})"
         },
         listeners: {
             onCreate: {
