f0849e6b0fe965eebf9b85adcb17fc2484df2b61
Justin Obara <obarajustin@gmail.com>
2016-06-03 12:14:25 -0400

    FLUID-5892: Added and refactored unit tests
    Added unit tests for a conditional panel in the separated panel test. Also performed some refactoring to opportunistically address some TODOs

--- tests/framework-tests/preferences/js/SeparatedPanelPrefsEditorTests.js
+++ tests/framework-tests/preferences/js/SeparatedPanelPrefsEditorTests.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2011-2015 OCAD University
+Copyright 2011-2016 OCAD University
 Copyright 2011 Lucendo Development Ltd.
 
 Licensed under the Educational Community License (ECL), Version 2.0 or the New
@@ -216,7 +216,8 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
                 type: "fluid.prefs.separatedPanel",
                 container: ".flc-prefsEditor-separatedPanel",
                 createOnEvent: "{mungingIntegrationTester}.events.onTestCaseStart",
-                options: fluid.merge(null, fluid.tests.prefs.mungingIntegrationOptions, { // TODO: Why on earth does this not use standard grade merging?
+                options: {
+                    gradeNames: ["fluid.tests.prefs.mungingIntegrationBase"],
                     iframeRenderer: {
                         markupProps: {
                             src: "./SeparatedPanelPrefsEditorFrame-nativeHTML.html"
@@ -239,7 +240,7 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
                             }
                         }
                     }
-                })
+                }
             },
             mungingIntegrationTester: {
                 type: "fluid.tests.mungingIntegrationTester"
@@ -285,6 +286,73 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
         }]
     });
 
+    /*******************************************************************************
+     * PrefsEditor separatedPanel conditional panel integration tests
+     *******************************************************************************/
+
+     fluid.defaults("fluid.tests.separatedPanelConditionalPanelIntegration", {
+         gradeNames: ["fluid.test.testEnvironment"],
+         components: {
+             prefsEditor: {
+                 type: "fluid.tests.composite.separatedPanel.prefsEditor",
+                 container: ".flc-prefsEditor-separatedPanel",
+                 createOnEvent: "{prefsTester}.events.onTestCaseStart"
+             },
+             prefsTester: {
+                 type: "fluid.tests.separatedPanelConditionalPanelIntegrationTester"
+             }
+         }
+     });
+
+     fluid.defaults("fluid.tests.separatedPanelConditionalPanelIntegrationTester", {
+         gradeNames: ["fluid.test.testCaseHolder"],
+         modules: [{
+             name: "Prefs editor with composite panel",
+             tests: [{
+                 name: "Rendering",
+                 expect: 7,
+                 sequence: [{
+                     listener: "jqUnit.assert",
+                     event: "{separatedPanelConditionalPanelIntegration prefsEditorLoader prefsEditor}.events.onReady",
+                     args: ["The onReady event should have fired"]
+                 }, {
+                     func: "{prefsEditor}.prefsEditorLoader.slidingPanel.showPanel"
+                 }, {
+                     func: "{prefsEditor}.prefsEditorLoader.prefsEditor.applier.change",
+                     args: ["preferences.fluid_tests_composite_pref_increaseSize", true]
+                 }, {
+                     listener: "fluid.tests.separatedPanelConditionalPanelIntegrationTester.assertConditionalComponents",
+                     event: "{prefsEditor}.prefsEditorLoader.prefsEditor.increasing.events.afterRender",
+                     args: ["{arguments}.0", ["fluid_tests_composite_pref_lineSpace", "fluid_tests_composite_pref_magnification"], true],
+                     priority: "last"
+                 }, {
+                     func: "{prefsEditor}.prefsEditorLoader.prefsEditor.applier.change",
+                     args: ["preferences.fluid_tests_composite_pref_increaseSize", false]
+                 }, {
+                     listener: "fluid.tests.separatedPanelConditionalPanelIntegrationTester.assertConditionalComponents",
+                     event: "{prefsEditor}.prefsEditorLoader.prefsEditor.increasing.events.afterRender",
+                     args: ["{arguments}.0", ["fluid_tests_composite_pref_lineSpace", "fluid_tests_composite_pref_magnification"], false],
+                     priority: "last"
+                 }]
+             }]
+         }]
+     });
+
+     fluid.tests.separatedPanelConditionalPanelIntegrationTester.assertConditionalComponents = function (parentPanel, memberNames, instantiated) {
+         if (instantiated) {
+             fluid.each(memberNames, function (memberName) {
+                 var comp = parentPanel[memberName];
+                 jqUnit.assertNotUndefined("The " + memberName + " should be instantiated", comp);
+                 console.log("locate bool:", comp.locate("bool"), "comp selectors:", comp.options.selectors);
+                 jqUnit.assertEquals("The " + memberName + " should be rendered", comp.model.value, comp.locate("bool").text());
+             });
+         } else {
+             fluid.each(memberNames, function (memberName) {
+                 jqUnit.assertUndefined("The " + memberName + " should not be instantiated", parentPanel[memberName]);
+             });
+         }
+     };
+
     $(document).ready(function () {
 
         fluid.tests.prefs.globalSettingsStore();
@@ -292,7 +360,8 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
 
         fluid.test.runTests([
             "fluid.tests.separatedPanelIntegration",
-            "fluid.tests.separatedPanelMungingIntegration"
+            "fluid.tests.separatedPanelMungingIntegration",
+            "fluid.tests.separatedPanelConditionalPanelIntegration"
         ]);
     });
 
