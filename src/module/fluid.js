ae17e4095b58360ef96f7328acb675a5651a1ede
Michelle D'Souza <michelled33@gmail.com>
2016-08-15 12:18:01 -0400

    FLUID-5940: Added a comment and simplified the check for whether the Infusion we found is the current Infusion.

--- src/module/fluid.js
+++ src/module/fluid.js
@@ -38,13 +38,15 @@ var upPath = path.resolve(__dirname, "../../../../..");
 var upInfusionPath = resolveModuleSync("infusion", upPath);
 if (upInfusionPath) {
     upInfusion = require(upInfusionPath);
-    if (upInfusion && Object.keys(upInfusion).length !== 0) {
-        upInfusion.log("Resolved infusion from path " + __dirname + " to " + upInfusion.module.modules.infusion.baseDir);
-        module.exports = upInfusion;
-        return;
-    } else {
-        console.log(upInfusionPath + " resolved to current Infusion");
-    }
+}
+
+// Fix for FLUID-5940, when Infusion is a dependency of a Node.js project that is located in the
+// root of a filesystem we were resolving to the current version of Infusion. Doing a 'require'
+// on the same version of Infusion results in an empty object.
+if (upInfusion && upInfusion.module) {
+    upInfusion.log("Resolved infusion from path " + __dirname + " to " + upInfusion.module.modules.infusion.baseDir);
+    module.exports = upInfusion;
+    return;
 } else {
     console.log("Infusion at path " + moduleBaseDir + " is at top level ");
 }
