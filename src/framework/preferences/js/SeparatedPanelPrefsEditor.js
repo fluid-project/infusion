27b9f501d5c174745a95074b2df6664eeacb6611
Justin Obara <obarajustin@gmail.com>
2016-09-22 10:39:20 -0400

    FLUID-5926: Added comments for lazyLoading

--- src/framework/preferences/js/SeparatedPanelPrefsEditor.js
+++ src/framework/preferences/js/SeparatedPanelPrefsEditor.js
@@ -342,6 +342,31 @@ var fluid_2_0_0 = fluid_2_0_0 || {};
         fluid.invokeLater(callback);
     };
 
+    /**
+     * FLUID-5926: Some of our users have asked for ways to improve the initial page load
+     * performance when using the separated panel prefs editor / UI Options. One option,
+     * provided here, is to implement a scheme for lazy loading the instantiation of the
+     * prefs editor, only instantiating enough of the workflow to allow display the
+     * sliding panel tab.
+     *
+     * fluid.prefs.separatedPanel.lazyLoad modifies the typical separatedPanel workflow
+     * by delaying the instantiation and loading of resources for the prefsEditor until
+     * the first time it is opened.
+     *
+     * Lazy Load Workflow:
+     *
+     * - On instantiation of the prefsEditorLoader only the messageLoader and slidingPanel are instantiated
+     * - On instantiation, the messageLoader only loads preLoadResources, these are the messages required by
+     *   the slidingPanel. The remained of the message bundles will not be loaded until the "onLazyLoad" event is fired.
+     * - After the preLoadResources have been loaded, the onPrefsEditorMessagesPreloaded event is fired, and triggers the
+     *   sliding panel to instantiate.
+     * - When a user opens the separated panel prefs editor / UI Options, it checks to see if the prefs editor has been
+     *   instantiated. If it hasn't, a listener is temporarily bound to the onReady event, which gets fired
+     *   after the prefs editor is ready. This is used to continue the process of opening the sliding panel for the first time.
+     *   Additionally the onLazyLoad event is fired, which kicks off the remainder of the instantiation process.
+     * - onLazyLoad triggers the templateLoader to fetch all of the templates and the messageLoader to fetch the remaining
+     *   message bundles. From here the standard instantiation workflow takes place.
+     */
     fluid.defaults("fluid.prefs.separatedPanel.lazyLoad", {
         events: {
             onLazyLoad: null,
@@ -405,6 +430,18 @@ var fluid_2_0_0 = fluid_2_0_0 || {};
 
     };
 
+    /**
+     * Used to override the standard "onCreate.loadResources" listener for fluid.resourceLoader component,
+     * allowing for pre-loading of a subset of resources. This is requried for the lazyLoading workflow
+     * for the "fluid.prefs.separatedPanel.lazyLoad".
+     *
+     * @param {Object} that - the component
+     * @param {Object} resource - all of the resourceSpecs to load, including preload and others.
+     *                            see: fluid.fetchResources
+     * @param {Array/String} toPreload - a String or an Array of Strings corresponding to the names
+     *                                   of the resources, supplied in the resource argument, that
+     *                                   should be loaded. Only these resources will be loaded.
+     */
     fluid.prefs.separatedPanel.lazyLoad.preloadResources = function (that, resources, toPreload) {
         toPreload = fluid.makeArray(toPreload);
         var preloadResources = {};
