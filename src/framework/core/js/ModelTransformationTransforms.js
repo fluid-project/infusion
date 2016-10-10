78f056a32585cdc698305a2fb010546ccfc226b9
Tony Atkins <tony@raisingthefloor.org>
2016-10-10 10:35:07 +0200

    NOJIRA:  Updated copyright information in update source file.

--- src/framework/core/js/ModelTransformationTransforms.js
+++ src/framework/core/js/ModelTransformationTransforms.js
@@ -1,7 +1,7 @@
 /*
 Copyright 2010 University of Toronto
 Copyright 2010-2011 OCAD University
-Copyright 2013 Raising the Floor - International
+Copyright 2013-2016 Raising the Floor - International
 
 Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
