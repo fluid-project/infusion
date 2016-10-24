d0604596ce9853dacfe2155c61db5b7ff0d7d7fd
Alan Harnum <waharnum@gmail.com>
2016-10-24 15:11:18 -0400

    FLUID-5986: restructuring TAP functionality to group them together.

--- tests/node-tests/basic-node-tests.js
+++ tests/node-tests/basic-node-tests.js
@@ -15,32 +15,10 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
 var fluid = require("../../src/module/fluid.js"),
     path = require("path");
 
-// For accumulating TAP messages for parsing by testem custom launcher
-var tapOutput = "";
 // Number of expected assertions
 var expectedAsserts = 20;
 // Number of expected test cases
 var expectedTestCases = 10;
-
-// Set boolean for the  presence of the --tap command line option to output a
-// TAP report for consumption by testem when used as a custom launcher
-var shouldOutputTAP = process.argv.findIndex(function (argument) {
-    return argument.indexOf("--tap") > -1;
-}) > -1;
-
-// Function to output a TAP  failure report
-// Assumed to be used as below with fluid.onUncaughtException.addListener
-var outputTAPFailure = function (message) {
-    if (!fluid.tests.expectFailure) {
-        console.log("not ok -  " + message);
-    }
-};
-
-// Register listener to output TAP failure report
-if (shouldOutputTAP) {
-    fluid.onUncaughtException.addListener(outputTAPFailure, "outputTAPFailure",
-        fluid.handlerPriorities.uncaughtException.log);
-}
 
 fluid.loadTestingSupport();
 
@@ -48,6 +26,8 @@ var QUnit = fluid.registerNamespace("QUnit");
 var jqUnit = fluid.registerNamespace("jqUnit");
 
 fluid.registerNamespace("fluid.tests");
+
+fluid.registerNamespace("fluid.tests.tapOutput");
 
 fluid.loadInContext("../../tests/test-core/testTests/js/IoCTestingTests.js");
 
@@ -75,12 +55,41 @@ QUnit.testDone(function (data) {
     fluid.log(getTestMessage(data));
 });
 
-// Additional handler for accumulating TAP output
-if (shouldOutputTAP) {
+// Set boolean for the  presence of the --tap command line option to output a
+// TAP report for consumption by testem when used as a custom launcher
+fluid.tests.tapOutput.shouldOutputTAP = process.argv.findIndex(function (argument) {
+    return argument.indexOf("--tap") > -1;
+}) > -1;
+
+// For accumulating TAP messages for parsing by testem custom launcher
+var tapOutput = "";
+
+// Function to output a TAP  failure report
+// Assumed to be used with fluid.onUncaughtException.addListener
+fluid.tests.tapOutput.outputTAPFailure = function (message) {
+    if (!fluid.tests.expectFailure) {
+        console.log("not ok -  " + message);
+    }
+};
+
+if (fluid.tests.tapOutput.shouldOutputTAP) {
+    // Register listener to output TAP failure report
+    fluid.onUncaughtException.addListener(fluid.tests.tapOutput.outputTAPFailure, "outputTAPFailure",
+        fluid.handlerPriorities.uncaughtException.log);
+
+    // Additional handler for accumulating TAP output
     QUnit.testDone(function (data) {
         var tapResult = data.failed === 0 ? "ok" : "not ok";
         var tapMessage = tapResult + " " + getTestMessage(data);
         tapOutput = tapOutput + tapMessage + "\n";
+    });
+
+    // Additional handler for outputting TAP
+    QUnit.done(function () {
+        // Output the TAP header
+        console.log("1.." + expectedTestCases);
+        // Output the TAP report
+        console.log(tapOutput);
     });
 }
 
@@ -96,16 +105,6 @@ QUnit.done(function (data) {
         process.exitCode = 1;
     }
 });
-
-// Additional handler for outputting TAP
-if (shouldOutputTAP) {
-    QUnit.done(function () {
-        // Output the TAP header
-        console.log("1.." + expectedTestCases);
-        // Output the TAP report
-        console.log(tapOutput);
-    });
-}
 
 QUnit.log(function (details) {
     if (details.source) { // "white-box" inspection of qunit.js shows that it sets this field on error
@@ -180,8 +179,8 @@ fluid.tests.onUncaughtException = function () {
         fluid.invokeLater(function () {
             fluid.onUncaughtException.removeListener("log"); // restore the original listener
             // Reregister listener to output TAP failure report
-            if (shouldOutputTAP) {
-                fluid.onUncaughtException.addListener(outputTAPFailure, "outputTAPFailure",
+            if (fluid.tests.tapOutput.shouldOutputTAP) {
+                fluid.onUncaughtException.addListener(fluid.tests.tapOutput.outputTAPFailure, "outputTAPFailure",
                     fluid.handlerPriorities.uncaughtException.log);
             }
             console.log("Restarting jqUnit in nested handler");
