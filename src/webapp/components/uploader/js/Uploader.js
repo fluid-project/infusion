4e3a34fc547d6d3ff212bfb67892de9d2f7cf6dd
Christian Murphy <cmurphy@unicon.net>
2016-11-22 14:00:20 -0700

    FLUID-6064: Add role application only to file queue

--- src/webapp/components/uploader/js/Uploader.js
+++ src/webapp/components/uploader/js/Uploader.js
@@ -344,6 +344,9 @@ var fluid_1_4 = fluid_1_4 || {};
         updateQueueSummaryText(that);
         that.statusUpdater();
         renderFileUploadLimit(that);
+
+        // Uploader file queue uses application-style keyboard conventions, so give it a suitable role.
+        that.locate("fileQueue").attr("role", "application");
     };
 
     /**
