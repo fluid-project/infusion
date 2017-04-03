5a75fa4ca5590b1f97eb00c83499832da04ab76f
Justin Obara <obarajustin@gmail.com>
2017-04-03 12:20:00 -0400

    FLUID-5536: Updating copyrights

--- tests/component-tests/textfieldControl/js/TextfieldSliderTests.js
+++ tests/component-tests/textfieldControl/js/TextfieldSliderTests.js
@@ -1,6 +1,6 @@
 /*
 Copyright 2009 University of Toronto
-Copyright 2011-2016 OCAD University
+Copyright 2011-2017 OCAD University
 
 Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
