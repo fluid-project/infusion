51d349d80aa11d8430c597a87442ed8ebab3e3ff
Tony Atkins <tony@raisingthefloor.org>
2017-08-02 17:03:06 +0200

    GPII-2518: Updated to fix broken gpii-testem instrumentation code.

--- tests/testem.js
+++ tests/testem.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 "use strict";
 var fluid = require("../src/module/fluid");
+fluid.setLogging(true);
 
 require("gpii-testem");
 
@@ -8,6 +9,7 @@ fluid.defaults("fluid.tests.testem", {
     gradeNames: ["gpii.testem.coverageDataOnly"],
     testPages:  "tests/all-tests.html",
     sourceDirs: ["src"],
+    packageName: "%infusion",
     coverageDir: "coverage",
     serveDirs:  ["src", "node_modules"]
 });
