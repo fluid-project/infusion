2d2d9866340432ad27e72e77dc4a0c1b0ec85b43
Antranig Basman <antranig.basman@colorado.edu>
2017-09-20 20:18:14 +0100

    FLUID-6192: Updating copyright years following review

--- src/framework/core/js/DataBinding.js
+++ src/framework/core/js/DataBinding.js
@@ -4,7 +4,7 @@ Copyright 2008-2009 University of Toronto
 Copyright 2010-2011 Lucendo Development Ltd.
 Copyright 2010-2014 OCAD University
 Copyright 2012-2014 Raising the Floor - US
-Copyright 2014-2016 Raising the Floor - International
+Copyright 2014-2017 Raising the Floor - International
 
 Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
