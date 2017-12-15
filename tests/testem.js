0fec501c486afb220cd171881523849bce0c033e
Tony Atkins <tony@raisingthefloor.org>
2017-12-15 20:07:57 +0100

    GPII-2773: Update coverage client and test fixtures to address inconsistent test reporting.

--- tests/testem.js
+++ tests/testem.js
@@ -7,22 +7,41 @@ require("gpii-testem");
 
 fluid.defaults("fluid.tests.testem", {
     gradeNames: ["gpii.testem.coverageDataOnly"],
-    testPages:  "tests/all-tests.html",
-    // Add a 150ms pause between when our fixtures are ready and when we let Testem know it's safe to start the test run.
+    // Add a 500ms pause between when our fixtures are ready and when we let Testem know it's safe to start the test run.
     // Seems to help with spurious browser test failures in Windows.
-    startupPause: 150,
-    invokers: {
-        pauseOnStartup: {
-            funcName: "setTimeout",
-            args:     ["{that}.handleTestemStart", "{that}.options.startupPause", "{arguments}.0", "{arguments}.1", "{arguments}.2"]
-        }
-    },
-    testemOptions: {
-        on_start: "{that}.pauseOnStartup"
-    },
-    sourceDirs: ["src"],
+    // startupPause: 500,
+    // invokers: {
+    //     pauseOnStartup: {
+    //         funcName: "setTimeout",
+    //         args:     ["{that}.handleTestemStart", "{that}.options.startupPause", "{arguments}.0", "{arguments}.1", "{arguments}.2"]
+    //     }
+    // },
+    // testemOptions: {
+    //     on_start: "{that}.pauseOnStartup"
+    // },
+    // listeners: {
+        // "onTestemStart.pause": {
+        //     priority: "after:instrument",
+        //     funcName: "{that}.events.constructFixtures.fire"
+        // },
+        // "onTestemStart.constructFixtures": {
+        //     priority: "after:pause",
+        //     func:     "{that}.events.constructFixtures.fire"
+        // },
+    // },
+    instrumentSource: false,
     coverageDir: "coverage",
-    serveDirs:  ["src", "node_modules"]
+    reportsDir: "reports",
+    testemOptions: {
+        skip: "PhantomJS,Opera,Safari",
+        disable_watching: true,
+        test_page:  "tests/all-tests.html",
+        tap_quiet_logs: true,
+        serve_files: ["instrumented", "tests", "node_modules"],
+        routes: {
+            "/src": "instrumented/src"
+        }
+    }
 });
 
 module.exports = fluid.tests.testem().getTestemOptions();
