720ca5c7ffafe4fde7310d57dfe7b66aefd925e7
Justin Obara <obarajustin@gmail.com>
2018-02-23 10:07:39 -0500

    FLUID-6204: Updating copyright statements.

--- src/framework/preferences/js/LetterSpaceEnactor.js
+++ src/framework/preferences/js/LetterSpaceEnactor.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 OCAD University
+Copyright 2017-2018 OCAD University
 
 Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
