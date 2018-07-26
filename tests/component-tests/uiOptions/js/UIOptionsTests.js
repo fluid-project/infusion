48a911d91ac89ee7ac40239e6943363f9840116f
BlueSlug <mossmonkey@gmail.com>
2018-07-26 18:48:35 -0400

    FLUID-6299: Updated UIO tests to use IoC testing framework

--- tests/component-tests/uiOptions/js/UIOptionsTests.js
+++ tests/component-tests/uiOptions/js/UIOptionsTests.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2011-2014 OCAD University
+Copyright 2011-2014, 2018 OCAD University
 
 Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
@@ -10,35 +10,66 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
 
  */
 
-/* global fluid, jqUnit */
+/* global fluid */
 
 (function ($) {
     "use strict";
 
-    $(document).ready(function () {
+    var customizedTocTemplate = "../../../../src/components/tableOfContents/html/TableOfContents.html";
 
-        jqUnit.module("UIOptions Tests");
+    fluid.defaults("fluid.uiOptions.testPrefsEditor", {
+        gradeNames: ["fluid.uiOptions.prefsEditor"],
+        tocTemplate: customizedTocTemplate,
+        terms: {
+            templatePrefix: "../../../../src/framework/preferences/html",
+            messagePrefix: "../../../../src/framework/preferences/messages"
+        }
+    });
 
-        jqUnit.asyncTest("Pass in customized toc template", function () {
-            jqUnit.expect(2);
-
-            var customizedTocTemplate = "../../../../src/components/tableOfContents/html/TableOfContents.html";
-
-            fluid.uiOptions.prefsEditor(".flc-prefsEditor-separatedPanel", {
-                tocTemplate: customizedTocTemplate,
-                listeners: {
-                    onReady: function (that) {
-                        jqUnit.assertEquals("The toc template is applied properly to the pageEnhancer", customizedTocTemplate, that.enhancer.uiEnhancer.fluid_prefs_enactor_tableOfContents.options.tocTemplate);
-                        jqUnit.assertEquals("FLUID-5474: The toc template is applied properly to iframeEnhancer", customizedTocTemplate, that.prefsEditorLoader.iframeRenderer.iframeEnhancer.fluid_prefs_enactor_tableOfContents.options.tocTemplate);
-                        jqUnit.start();
-                    }
+    fluid.defaults("fluid.uiOptions.prefsEditorTester", {
+        gradeNames: ["fluid.test.testCaseHolder"],
+        modules: [{
+            name: "UIOptions Tests",
+            tests: [{
+                name: "Pass in customized toc template",
+                expect: 2,
+                sequence: [{
+                    event: "{prefsEditorTest prefsEditor}.events.onCreate",
+                    listener: "jqUnit.assertEquals",
+                    args: ["The toc template is applied properly to the pageEnhancer", customizedTocTemplate, "{prefsEditor}.enhancer.uiEnhancer.fluid_prefs_enactor_tableOfContents.options.tocTemplate"]
                 },
-                terms: {
-                    templatePrefix: "../../../../src/framework/preferences/html",
-                    messagePrefix: "../../../../src/framework/preferences/messages"
-                }
-            });
-        });
+                {
+                    funcName: "fluid.identity"
+                },
+                {
+                    event: "{prefsEditor}.events.onReady",
+                    listener: "jqUnit.assertEquals",
+                    args: ["FLUID-5474: The toc template is applied properly to iframeEnhancer", customizedTocTemplate, "{prefsEditor}.prefsEditorLoader.iframeRenderer.iframeEnhancer.fluid_prefs_enactor_tableOfContents.options.tocTemplate"]
+                }]
+            }]
+        }]
+    });
 
+    fluid.defaults("fluid.uiOptions.prefsEditorTest", {
+        gradeNames: ["fluid.test.testEnvironment"],
+        components: {
+            prefsEditor: {
+                type: "fluid.uiOptions.testPrefsEditor",
+                container: ".flc-prefsEditor-separatedPanel",
+                createOnEvent: "{prefsEditorTester}.events.onTestCaseStart"
+            },
+            prefsEditorTester: {
+                type: "fluid.uiOptions.prefsEditorTester"
+            }
+        }
+    });
+
+
+
+
+    $(document).ready(function () {
+        fluid.test.runTests([
+            "fluid.uiOptions.prefsEditorTest",
+        ]);
     });
 })(jQuery);
