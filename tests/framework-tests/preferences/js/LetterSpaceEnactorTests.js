4af9f98f2418f2d3f7947a8da6d0227e5599afe9
Justin Obara <obarajustin@gmail.com>
2018-09-24 16:34:22 -0400

    FLUID-6333: Linting

--- tests/framework-tests/preferences/js/LetterSpaceEnactorTests.js
+++ tests/framework-tests/preferences/js/LetterSpaceEnactorTests.js
@@ -9,7 +9,7 @@ You may obtain a copy of the ECL 2.0 License and BSD License at
 https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
 */
 
-/* global fluid, jqUnit */
+/* global fluid */
 
 (function ($) {
     "use strict";
