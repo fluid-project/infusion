9ab30ff791dd22b663212574067806e58e7393c8
Justin Obara <obara.justin@gmail.com>
2020-01-31 10:18:54 -0500

    FLUID-6451: Use the fluid.prefs.tempStore
    Will only use cookie store when explicitly testing the that 
    implementation

--- tests/component-tests/uiOptions/js/UIOptionsTests.js
+++ tests/component-tests/uiOptions/js/UIOptionsTests.js
@@ -17,6 +17,21 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
 (function ($) {
     "use strict";
 
+    // use tempStore for tests
+    fluid.contextAware.makeChecks({"fluid.prefs.tests": true});
+
+    fluid.contextAware.makeAdaptation({
+        distributionName: "fluid.tests.prefs.tempStoreDistributor",
+        targetName: "fluid.prefs.store",
+        adaptationName: "strategy",
+        checkName: "test",
+        record: {
+            contextValue: "{fluid.prefs.tests}",
+            gradeNames: "fluid.prefs.tempStore",
+            priority: "after:user"
+        }
+    });
+
     /* Mixin grade for UIO test component */
     fluid.defaults("fluid.tests.uiOptions.testPrefsEditorBase", {
         gradeNames: ["fluid.uiOptions.prefsEditor"],
